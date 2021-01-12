56c02ae1ebfe19710755a8c69de8be4080f354e7
huilling110 <huiling2016@outlook.com>
2021-01-12 20:16:53 +0800

    small changes

--- makeIHEPjob.py
+++ makeIHEPjob.py
@@ -99,7 +99,7 @@ def prepareCshJob(inputFile,shFile):
     print >> subFile, "cd "+Jobsubmitpath
     #  print >> subFile, "cmsenv" # cmsenv not working in job
 #        print >> subFile, "root -l -b -q "+rootplizer+"\'(\""+inputFile+"\")\'"
-    print >> subFile, "root -l -b -q "+"\'"+rootplizer+"+(false,\""+inputFile+"\","+"\"/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v16_removeISOforE_change2SS2OS_invitializeAll/\")"+"\'"
+    print >> subFile, "root -l -b -q "+"\'"+rootplizer+"+(false,\""+inputFile+"\","+"\"/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v17_removeMuonISO/\")"+"\'"
     #  print >> subFile, "root -l -b -q "+"\'"+rootplizer+"+(false,\""+inputFile+"\","+"\"/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/forMVA/1tau0lTausT/\")"+"\'"
 #	subprocess.call("chmod 777 "+shFile, shell=True)
 
@@ -115,8 +115,8 @@ for k in sample:
     #  shFileName = "Jobsubmit_2/"+shFile
     inputFile = sampleName+".root"
     prepareCshJob(inputFile,shFileName)	
-    logFileName = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v16_removeISOforE_change2SS2OS_invitializeAll/log/"+"sampleName_"+sampleName+".log"
-    errorFileName = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v16_removeISOforE_change2SS2OS_invitializeAll/log/"+"sampleName_"+sampleName+".err"
+    logFileName = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v17_removeMuonISO/log/"+"sampleName_"+sampleName+".log"
+    errorFileName = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v17_removeMuonISO/log/"+"sampleName_"+sampleName+".err"
     #  logFileName = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/forMVA/1tau0lTausT/"+"sampleNumber_"+sampleNumber+".log"
     #  errorFileName = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/forMVA/1tau0lTausT/"+"sampleNumber_"+sampleNumber+".err"
     print >> allJobFile, "hep_sub "+ shFile + " -o "+logFileName+ " -e "+errorFileName	 
