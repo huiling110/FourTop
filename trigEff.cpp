02450069a96c27d512d820545838eae974b5899d
Fabio Iemmi <fabio.iemmi@cern.ch>
2021-01-29 21:42:52 +0800

    in trigEff.cpp: add efficiency for 1tau2L category

--- trigEff.cpp
+++ trigEff.cpp
@@ -54,11 +54,15 @@ evt->SetBranchAddress( "eleMVAT_pt", &myeleMVAT_pt );
 vector<double> * mymuonT_pt = 0;
 evt->SetBranchAddress( "muonT_pt", &mymuonT_pt );
 
+ // multijet triggers
+
  int myHLT_PFHT450_SixJet40_BTagCSV_p056 = 0;
 evt->SetBranchAddress("HLT_PFHT450_SixJet40_BTagCSV_p056", &myHLT_PFHT450_SixJet40_BTagCSV_p056);
 
  int myHLT_PFHT400_SixJet30_DoubleBTagCSV_p056 = 0;
 evt->SetBranchAddress("HLT_PFHT400_SixJet30_DoubleBTagCSV_p056", &myHLT_PFHT400_SixJet30_DoubleBTagCSV_p056);
+
+ // single electron triggers
 
  int myHLT_Ele25_eta2p1_WPTight_Gsf = 0;
  evt->SetBranchAddress("HLT_Ele25_eta2p1_WPTight_Gsf", &myHLT_Ele25_eta2p1_WPTight_Gsf);
@@ -69,6 +73,8 @@ evt->SetBranchAddress("HLT_PFHT400_SixJet30_DoubleBTagCSV_p056", &myHLT_PFHT400_
  int myHLT_Ele27_eta2p1_WPLoose_Gsf = 0;
  evt->SetBranchAddress("HLT_Ele27_eta2p1_WPLoose_Gsf", &myHLT_Ele27_eta2p1_WPLoose_Gsf);
 
+ // electron + tau cross triggers
+
  int myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20 = 0;
  evt->SetBranchAddress("HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20", &myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20);
 
@@ -77,6 +83,8 @@ evt->SetBranchAddress("HLT_PFHT400_SixJet30_DoubleBTagCSV_p056", &myHLT_PFHT400_
 
  int myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau30 = 0;
  evt->SetBranchAddress("HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau30", &myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau30);
+
+ // single muon triggers
 
  int myHLT_IsoMu22 = 0;
  evt->SetBranchAddress("HLT_IsoMu22", &myHLT_IsoMu22);
@@ -96,15 +104,56 @@ evt->SetBranchAddress("HLT_PFHT400_SixJet30_DoubleBTagCSV_p056", &myHLT_PFHT400_
  int myHLT_IsoTkMu24 = 0;
  evt->SetBranchAddress("HLT_IsoTkMu24", &myHLT_IsoTkMu24);
 
+ // muon + tau cross triggers
+
  int myHLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1 = 0;
  evt->SetBranchAddress("HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1", &myHLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1);
 
+ // double electron triggers
  
+ int myHLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ = 0;
+ evt->SetBranchAddress("HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", &myHLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ);
+
+ // double muon triggers
+
+ int myHLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL = 0;
+ evt->SetBranchAddress("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL", &myHLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL);
+
+ int myHLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ = 0;
+ evt->SetBranchAddress("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ", &myHLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ);
+
+ int myHLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL = 0;
+ evt->SetBranchAddress("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL", &myHLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL);
+
+ int myHLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ = 0;
+ evt->SetBranchAddress("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ", &myHLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ);
+
+ // electron plus muon triggers
+
+ int myHLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL = 0;
+ evt->SetBranchAddress("HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL", &myHLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL);
+
+ int myHLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ = 0;
+ evt->SetBranchAddress("HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", &myHLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ);
+
+ int myHLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL = 0;
+ evt->SetBranchAddress("HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL", &myHLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL);
+ 
+ int myHLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ = 0;
+ evt->SetBranchAddress("HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ", &myHLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ);
+
+ int myHLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ = 0;
+ evt->SetBranchAddress("HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", &myHLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ);
+
+ int myHLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL = 0;
+ evt->SetBranchAddress("HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", &myHLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL);
 
  double num_1tau0L = 0;
  double den_1tau0L = 0;
  double num_1tau1L = 0;
  double den_1tau1L = 0;
+ double num_1tau2L = 0;
+ double den_1tau2L = 0;
 
 Long64_t nevents = evt->GetEntries();
 
@@ -134,11 +183,19 @@ for ( Long64_t ievent = 0; ievent < nevents; ++ievent ) {
    ///////////////////// DEFINE TRIGGER CUTS ///////////////////////////
    /////////////////////////////////////////////////////////////////////
 
+ bool singleEle = (myHLT_Ele25_eta2p1_WPTight_Gsf == 1 || myHLT_Ele27_WPTight_Gsf == 1 || myHLT_Ele27_eta2p1_WPLoose_Gsf == 1);
+ bool singleMu = (myHLT_IsoMu22 == 1 || myHLT_IsoTkMu22 == 1 || myHLT_IsoMu22_eta2p1 == 1 || myHLT_IsoTkMu22_eta2p1 == 1 || myHLT_IsoMu24 == 1 ||  myHLT_IsoTkMu24 == 1);
+ bool elePlusTau = (myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20 == 1 || myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1 == 1 || myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau30 == 1);
+ bool muPlusTau = (myHLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1 == 1);
+ bool doubleEle = (myHLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ == 1);
+ bool doubleMu = (myHLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL == 1 || myHLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ == 1 || myHLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL == 1 || myHLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ == 1);
+ bool elePlusMu (myHLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL == 1 || myHLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ == 1 || myHLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL == 1 || myHLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ == 1 || myHLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ == 1 || myHLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL == 1);
 
  bool is1tau0Ltrig = (myHLT_PFHT450_SixJet40_BTagCSV_p056 == 1 || myHLT_PFHT400_SixJet30_DoubleBTagCSV_p056 == 1);
- bool is1tau1etrig = ( (myHLT_Ele25_eta2p1_WPTight_Gsf == 1 || myHLT_Ele27_WPTight_Gsf == 1 || myHLT_Ele27_eta2p1_WPLoose_Gsf == 1) || (myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20 == 1 || myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1 == 1 || myHLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau30 == 1) );
- bool is1tau1mutrig = ( (myHLT_IsoMu22 == 1 || myHLT_IsoTkMu22 == 1 || myHLT_IsoMu22_eta2p1 == 1 || myHLT_IsoTkMu22_eta2p1 == 1 || myHLT_IsoMu24 == 1 ||  myHLT_IsoTkMu24 == 1)  || (myHLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1 == 1) );
-
+ bool is1tau1etrig = (singleEle || elePlusTau);
+ bool is1tau1mutrig = (singleMu || muPlusTau);
+ bool is1tau2Ltrig = ( singleEle || singleMu || doubleEle || doubleMu || elePlusMu  );
+ 
  if (is1tau0L) {
 
    den_1tau0L += mygenEvtWeight;
@@ -153,12 +210,53 @@ for ( Long64_t ievent = 0; ievent < nevents; ++ievent ) {
 
    if (is1tau1etrig || is1tau1mutrig) num_1tau1L += mygenEvtWeight;
 
-}
+ }
+
+ if (is1tau2OSL || is1tau2SSL) {
+
+   if (myeleMVAT_pt->size() > 0 && mymuonT_pt->size() == 0) {
+     
+     if (myeleMVAT_pt->at(0) > 30) {
+
+       den_1tau2L += mygenEvtWeight;
+       
+       if (is1tau2Ltrig) num_1tau2L += mygenEvtWeight;
+
+     }
+
+   }
+
+   if (myeleMVAT_pt->size() == 0 && mymuonT_pt->size() > 0) {
+     
+     if (mymuonT_pt->at(0) > 30) {
+
+       den_1tau2L += mygenEvtWeight;
+       
+       if (is1tau2Ltrig) num_1tau2L += mygenEvtWeight;
+
+     }
+
+   }
+
+   if (myeleMVAT_pt->size() > 0 && mymuonT_pt->size() > 0) {
+     
+     if (myeleMVAT_pt->at(0) > 30 && mymuonT_pt->at(0) > 30) {
+
+       den_1tau2L += mygenEvtWeight;
+       
+       if (is1tau2Ltrig) num_1tau2L += mygenEvtWeight;
+
+     }
+
+   }
+
+ }
 
  }//end loop over events
  
  cout << "trigger efficiency for 1tau0L cat = " << num_1tau0L/den_1tau0L << endl;
  cout << "trigger efficiency for 1tau1L cat = " << num_1tau1L/den_1tau1L << endl;
+ cout << "trigger efficiency for 1tau2L cat = " << num_1tau2L/den_1tau2L << endl;
  
  inputfile->Close();
  delete inputfile;
