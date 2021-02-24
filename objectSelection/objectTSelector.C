a65c4b507eeec5b7215de60cc886afff91e7d3e3
huilling110 <huiling2016@outlook.com>
2021-02-24 20:22:20 +0800

    made inputDir and outputDir in run_objectTSelector.C

--- objectSelection/objectTSelector.C
+++ objectSelection/objectTSelector.C
@@ -79,13 +79,13 @@ void objectTSelector::SlaveBegin(TTree * /*tree*/)
 
    TString option = GetOption();
     
-   TString  outputDir = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/test_objectSelction/";
+   // TString  outputDir = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/test_objectSelction/";
    // TString  outputDir = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v26_objectSelection/";
    // TString inName = fChain->GetName();
    // cout<<inName<<endl;
-   // TString outFileName = "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_correctnPartonsInBorn.root";
    TString outFileName = option;
-   outputfile = new TFile( outputDir+outFileName, "RECREATE");
+   // outputfile = new TFile( outputDir+outFileName, "RECREATE");
+   outputfile = new TFile( outFileName, "RECREATE");
    
    h_genWeight = new TH1D( "h_genweight", "h_genweight", 1,-0.5, 0.5);
 
--- objectSelection/run_objectTSelector.C
+++ objectSelection/run_objectTSelector.C
@@ -1,9 +1,11 @@
 
-void run_objectTSelector(Bool_t istest = true){
-
+void run_objectTSelector(Bool_t istest = true,
+                        TString inputDir = "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_correctnPartonsInBorn/Legacy16V2_TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8addGenWeight/210201_023641/0000/",
+                        TString outputDir = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v27_objectSelection/")
+{
     gROOT->ProcessLine(".L Loader.C+");
 
-    TString inputDir = "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_correctnPartonsInBorn/Legacy16V2_TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8addGenWeight/210201_023641/0000/";
+    // TString inputDir = "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_correctnPartonsInBorn/Legacy16V2_TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8addGenWeight/210201_023641/0000/";
     TString inputFile; TString inputBase = "/publicfs/cms/data/TopQuark/FourTop_hua/v3/2016/";
     inputFile = inputBase + inputDir;
     TChain chain("TNT/BOOM");
@@ -12,8 +14,10 @@ void run_objectTSelector(Bool_t istest = true){
     TString outputFileName(inputDir( 0, inputDir.First("/") ));
     outputFileName = outputFileName + ".root";
     TString selection = "/publicfs/cms/user/huahuil/code/FourTopTau/CMSSW_10_2_20_UL/src/FourTop/objectSelection/objectTSelector.C";
-    // chain.Process( selection + "+", "", 1000);
-    if ( istest )chain.Process( selection + "+", outputFileName, 1000);
+    if ( istest ){
+        outputDir = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/test_objectSelction/";
+        chain.Process( selection + "+", outputDir + outputFileName, 1000);
+    }
     else chain.Process( selection + "+", outputFileName);
 
 
