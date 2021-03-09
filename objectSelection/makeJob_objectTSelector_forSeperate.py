310916336a303ae8287ab8cd8d66af295aba6df2
huilling110 <huiling2016@outlook.com>
2021-03-09 14:24:40 +0800

    sumbmitted some jobs for v31

--- objectSelection/makeJob_objectTSelector_forSeperate.py
+++ objectSelection/makeJob_objectTSelector_forSeperate.py
@@ -3,10 +3,14 @@ import os
 import glob
 import string
 import subprocess
+ #
 
+
+
+#all the parameters you need to change is in this part , better not change the rest of the code.
 #  isdata = True
 isdata = False
-outputDir = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v30_fixedChargeBranchBug/"
+outputDir = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v31_fixedLeptonBug/"
 #  outputDir = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/test_objectSelction/"
 inputDir = "/publicfs/cms/data/TopQuark/FourTop_hua/v3/2016/"
 #  inputDir = "/publicfs/cms/data/TopQuark/FourTop_hua/v2/mc/2016/"
@@ -18,7 +22,6 @@ if os.path.exists(subAllFile):
     os.popen('rm -fr '+subAllFile)
 if not os.path.exists(Jobsubmitpath+"/Jobsubmit_seperate"):
     os.mkdir('Jobsubmit_seperate/')
-
 
 sample = {
         #  "JetHT/Legacy16V2_JetHTBlockCHLTToptaggerAdded_EJetMetUpdated_oldEIDBack_v2/210107_060426/0000/": "C_0",
@@ -32,12 +35,12 @@ sample = {
         #  "JetHT/Legacy16V2_v2_JetHTBlockBHLTToptaggerAdded_EJetMetUpdated_oldEIDBack_v2/210107_060219/0000/": "B_0",
         #  "JetHT/Legacy16V2_v2_JetHTBlockBHLTToptaggerAdded_EJetMetUpdated_oldEIDBack_v2/210107_060219/0001/": "B_1",
 
-   #  "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_correctnPartonsInBorn/Legacy16V2_TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8addGenWeight/210201_023641/0000/":"0",
+   "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_correctnPartonsInBorn/Legacy16V2_TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8addGenWeight/210201_023641/0000/":"0",
   # {{{
 ##tt
-    #  "TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/Legacy16V2_TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8addGenWeight/210201_024446/0000/":"1_0",
-    #  "TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8/Legacy16V2_TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8addGenWeight/210201_024239/0000/":"1_1",
-    #  "TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8/Legacy16V2_TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8addGenWeight/210201_024040/0000/":"1_2",
+    "TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/Legacy16V2_TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8addGenWeight/210201_024446/0000/":"1_0",
+    "TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8/Legacy16V2_TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8addGenWeight/210201_024239/0000/":"1_1",
+    "TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8/Legacy16V2_TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8addGenWeight/210201_024040/0000/":"1_2",
 #
     "ttZJets_13TeV_madgraphMLM-pythia8/Legacy16V2_ttZJets_13TeV_madgraphMLM-pythia8addGenWeight/210201_025644/0000":"3",
     "ttWJets_13TeV_madgraphMLM/Legacy16V2_ttWJets_13TeV_madgraphMLMaddGenWeight/210201_030246/0000/":"4",
@@ -50,31 +53,31 @@ sample = {
     "WGGJets_TuneCUETP8M1_13TeV_madgraphMLM_pythia8/Legacy16V2_WGGJets_TuneCUETP8M1_13TeV_madgraphMLM_pythia8addGenWeight/210201_040503/0000/": "9",
     "ZGGJets_ZToHadOrNu_5f_LO_madgraph_pythia8/Legacy16V2_ZGGJets_ZToHadOrNu_5f_LO_madgraph_pythia8addGenWeight/210201_040904/0000/": "10",
 #
-    "QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_054840/0000/": "12",
-    "QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055050/0000/": "13",
-    "QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055306/0000/": "14",
-    "QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055508/0000/": "15",
-    "QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055716/0000/": "16",
-    "QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055930/0000/": "17",
-    "QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055930/0000/": "18",
-    "QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_060130/0000/": "19",
+    #  "QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_054840/0000/": "12",
+    #  "QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055050/0000/": "13",
+    #  "QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055306/0000/": "14",
+    #  "QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055508/0000/": "15",
+    #  "QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055716/0000/": "16",
+    #  "QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055930/0000/": "17",
+    #  "QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_055930/0000/": "18",
+    #  "QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/Legacy16V2_QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8addGenWeight/210201_060130/0000/": "19",
 
 #  WJets "20,
-    "DYJetsToTauTau_ForcedMuEleDecay_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext1/Legacy16V2_DYJetsToTauTau_ForcedMuEleDecay_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext1addGenWeight/210201_041715/0000/": "21",
-
-    "tZq_ll_4f_ckm_NLO_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/Legacy16V2_tZq_ll_4f_ckm_NLO_TuneCP5_PSweights_13TeV-amcatnlo-pythia8addGenWeight/210201_042138/0000/": "22",
-    "tZq_nunu_4f_13TeV-amcatnlo-pythia8_TuneCUETP8M1/Legacy16V2_tZq_nunu_4f_13TeV-amcatnlo-pythia8_TuneCUETP8M1addGenWeight/210201_042338/0000/": "23",
-    "ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4/Legacy16V2_ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4addGenWeight/210201_042748/0000/": "24",
-    "ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4/Legacy16V2_ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4addGenWeight/210201_042947/0000/": "25",
-    "TGJets_TuneCUETP8M1_13TeV_amcatnlo_madspin_pythia8/Legacy16V2_TGJets_TuneCUETP8M1_13TeV_amcatnlo_madspin_pythia8addGenWeight/210201_042537/0000/": "26",
-
-    "THW_ctcvcp_HIncl_M125_TuneCP5_13TeV-madgraph-pythia8/Legacy16V2_THW_ctcvcp_HIncl_M125_TuneCP5_13TeV-madgraph-pythia8addGenWeight/210201_053954/0000/": "27",
-    "THQ_ctcvcp_Hincl_13TeV-madgraph-pythia8_TuneCUETP8M1/Legacy16V2_THQ_ctcvcp_Hincl_13TeV-madgraph-pythia8_TuneCUETP8M1addGenWeight/210201_054208/0000/": "28",
+    #  "DYJetsToTauTau_ForcedMuEleDecay_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext1/Legacy16V2_DYJetsToTauTau_ForcedMuEleDecay_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext1addGenWeight/210201_041715/0000/": "21",
+#
+    #  "tZq_ll_4f_ckm_NLO_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/Legacy16V2_tZq_ll_4f_ckm_NLO_TuneCP5_PSweights_13TeV-amcatnlo-pythia8addGenWeight/210201_042138/0000/": "22",
+    #  "tZq_nunu_4f_13TeV-amcatnlo-pythia8_TuneCUETP8M1/Legacy16V2_tZq_nunu_4f_13TeV-amcatnlo-pythia8_TuneCUETP8M1addGenWeight/210201_042338/0000/": "23",
+    #  "ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4/Legacy16V2_ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4addGenWeight/210201_042748/0000/": "24",
+    #  "ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4/Legacy16V2_ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4addGenWeight/210201_042947/0000/": "25",
+    #  "TGJets_TuneCUETP8M1_13TeV_amcatnlo_madspin_pythia8/Legacy16V2_TGJets_TuneCUETP8M1_13TeV_amcatnlo_madspin_pythia8addGenWeight/210201_042537/0000/": "26",
+#
+    #  "THW_ctcvcp_HIncl_M125_TuneCP5_13TeV-madgraph-pythia8/Legacy16V2_THW_ctcvcp_HIncl_M125_TuneCP5_13TeV-madgraph-pythia8addGenWeight/210201_053954/0000/": "27",
+    #  "THQ_ctcvcp_Hincl_13TeV-madgraph-pythia8_TuneCUETP8M1/Legacy16V2_THQ_ctcvcp_Hincl_13TeV-madgraph-pythia8_TuneCUETP8M1addGenWeight/210201_054208/0000/": "28",
      #  /publicfs/cms/data/TopQuark/FourTop_hua/v3/2016/
 # }}}
 
 }
-
+#better not change code after this line
 
 def prepareCshJob(inputFile,shFile, singleFile):
     subFile  = file(shFile,"w")
