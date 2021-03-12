b55feb230500b9f5502cf6a33c0fdd1d21c8a5f9
Fabio Iemmi <fabio.iemmi@cern.ch>
2021-03-12 22:10:22 +0800

    change to new files

--- fabio/createHistos_chain.h
+++ fabio/createHistos_chain.h
@@ -13,18 +13,20 @@ map<string, string> file = { //MAP OF INPUT FILES
 {"tttt", dir + "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_correctnPartonsInBorn"},
 
 //ttbar background    
-{"tt", dir + "TT_TuneCUETP8M2T4_13TeV-powheg-pythia8"},
-/*    
+{"ttbar-FH", dir + "TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8"},
+{"ttbar-SL", dir + "TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8"},
+{"ttbar-DL", dir + "TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8"},
+
 //tt+X background
-{"ttG+jets", dir + "TTGJets_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8"},
+{"ttG-SL", dir + "TTGamma_SingleLept_TuneEE5C_13TeV-madgraph-herwigpp"},
+{"ttG-DL", dir + "TTGamma_Dilept_TuneEE5C_13TeV-madgraph-herwigpp"},
 {"ttW+jets", dir + "ttWJets_13TeV_madgraphMLM"},
 {"ttZ+jets", dir + "ttZJets_13TeV_madgraphMLM-pythia8"},
 {"ttH", dir + "ttH_4f_ctcvcp_TuneCP5_13TeV_madgraph_pythia8"},
 
 //diboson background
 {"WZ", dir + "WZ_TuneCUETP8M1_13TeV-pythia8"},
-{"WWTo2L2Nu", dir + "WWTo2L2Nu_DoubleScattering_13TeV-pythia8"},
-{"WpWpJJ", dir + "WpWpJJ_EWK-QCD_TuneCUETP8M1_13TeV-madgraph-pythia8"}, //WHAT'S THIS PROCESS?
+{"WW", dir + "WW_TuneCUETP8M1_13TeV-pythia8"},
 {"ZZ", dir + "ZZ_TuneCUETP8M1_13TeV-pythia8"},
 {"WG+jets", dir + "WGJets_MonoPhoton_PtG-40to130_TuneCUETP8M1_13TeV-madgraph"},
 {"ZG+jets", dir + "ZGJetsToLLG_EW_LO_13TeV-sherpa"},
@@ -32,7 +34,7 @@ map<string, string> file = { //MAP OF INPUT FILES
 //triboson background
 {"WWW", dir + "WWW_4F_TuneCUETP8M1_13TeV-amcatnlo-pythia8"},
 {"WWZ", dir + "WWZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8"},
-{"WWG", dir + "WWG_TuneCUETP8M1_13TeV-amcatnlo-pythia8"},
+//{"WWG", dir + "WWG_TuneCUETP8M1_13TeV-amcatnlo-pythia8"},
 {"ZZZ", dir + "ZZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8"},
 {"WZZ", dir + "WZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8"},
 {"WZG", dir + "WZG_TuneCUETP8M1_13TeV-amcatnlo-pythia8"},
@@ -40,7 +42,7 @@ map<string, string> file = { //MAP OF INPUT FILES
 {"ZGG+jets", dir + "ZGGJets_ZToHadOrNu_5f_LO_madgraph_pythia8"},
 
 //W+jets background
-{"W+jets", dir + "WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"},
+//{"W+jets", dir + "WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"},
 
 //DY background
 {"DY", dir + "DYJetsToTauTau_ForcedMuEleDecay_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext1"},
@@ -49,7 +51,7 @@ map<string, string> file = { //MAP OF INPUT FILES
 {"tZq", dir + "tZq_ll_4f_ckm_NLO_TuneCP5_PSweights_13TeV-amcatnlo-pythia8"},
 {"tW_antitop", dir + "ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4"},
 {"tW_top", dir + "ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4"},
-{"tG+jets", dir + "TGJets_TuneCUETP8M1_13TeV_amcatnlo_madspin_pythia8"},
+//{"tG+jets", dir + "TGJets_TuneCUETP8M1_13TeV_amcatnlo_madspin_pythia8"},
 {"tHW", dir + "THW_ctcvcp_HIncl_M125_TuneCP5_13TeV-madgraph-pythia8"},
 {"THq", dir + "THQ_ctcvcp_Hincl_13TeV-madgraph-pythia8_TuneCUETP8M1"},
 
@@ -67,7 +69,7 @@ map<string, string> file = { //MAP OF INPUT FILES
 {"VBFHTo2L2Nu", dir + "VBFHToWWTo2L2Nu_M125_13TeV_powheg_JHUgenv628_pythia8"},
 {"VBFHToMuMu", dir + "VBFHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8"},
 {"VBFHToGG", dir + "VBFHToGG_M125_13TeV_amcatnlo_pythia8_v2"},
-*/
+
 };
 
 
@@ -81,4 +83,4 @@ map<string, TH1F> y_2tau1e;
 map<string, TH1F> y_2tau1mu;
 map<string, TH1F> y_2tau2L;
 
-map<string, TH1F> total_weight;
+map<string, TH1D> total_weight;
