6eb69abfd9f383508ae10266cb6ac87e11eb4518
huilling110 <huiling2016@outlook.com>
2021-03-18 11:10:10 +0800

    just changed some code layout

--- hua/EYandSP_usingClass.C
+++ hua/EYandSP_usingClass.C
@@ -383,9 +383,9 @@ for (UInt_t  cha=0; cha<1; cha++){
             // allProcesses[j].getEventTree()->Project( hname, plot, weight*MetFilters);
             // allProcesses[j].getEventTree()->Project( hname, plot, weight*(MetFilters+trigger));
             // allProcesses[j].getEventTree()->Project( hname, plot, weight*(channelCut_step1[cha]+MetFilters+trigger));
-            // allProcesses[j].getEventTree()->Project( hname, plot, weight*(channelCut_step2[cha]+MetFilters+trigger));
+            allProcesses[j].getEventTree()->Project( hname, plot, weight*(channelCut_step2[cha]+MetFilters+trigger));
             // allProcesses[j].getEventTree()->Project( hname, plot, weight*(channelCut_step3[cha]+MetFilters+trigger));
-            allProcesses[j].getEventTree()->Project( hname, plot, weight*(channelCut[cha]+MetFilters+trigger));
+            // allProcesses[j].getEventTree()->Project( hname, plot, weight*(channelCut[cha]+MetFilters+trigger));
             // allHistos[j]->Print();
             if ( j ==0 || j==1){
                 cout<<allHistos[j]->GetName()<<":"<<endl;
