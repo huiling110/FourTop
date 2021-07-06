00ee9471bf9e0f847c450e68716fea76630b3ca2
huilling110 <huiling2016@outlook.com>
2021-07-06 20:48:54 +0800

    used functions to make code cleaner

--- hua/tmva/autoTraining_correlation/generateVariableList.py
+++ hua/tmva/autoTraining_correlation/generateVariableList.py
@@ -13,49 +13,10 @@ def main(  TMVAlog = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v1H
     
     #the first line identifies each piece of datain other words, the name of a data column
     
-    initialVariableList = [] #Buffer list
-
-    #  with open(TMVAlog) as csv_file:
-    with open(TMVAlog) as csv_file, open("variableList.csv", "w+") as to_file2:
-        csv_reader = csv.reader(csv_file, delimiter=':')
-        writer2 = csv.writer(to_file2, delimiter=":")
-        linecount = 0
-        SPline = 0
-        variable1 = ''
-        for irow in csv_reader: 
-            #Each row returned by the reader is a list of String elements containing the data found by removing the delimiters.
-            #  if linecount<300:
-                #  print(irow[0])
-            #  if irow[0] == "number of branches":
-                #  print(irow[1])
-
-            #get the SP part
-            if irow[0] == "IdTransformation         ":
-                #  print(irow[1])
-                SPline = linecount+2
-            if (linecount == SPline and not(linecount == 0) ):
-                #  print(irow)
-                if len(irow)>3:
-                    #  if not(irow[2] == ' -----------------------------------------------------') or not(irow[1]=='Factory                  '):
-                    if irow[2] != ' -----------------------------------------------------' and irow[1]!='Factory                  ' and irow[2]!=' Variable                     ':
-                        #  print(irow)
-                        #  print(irow[2])
-                        #  writer2.writerow(irow[2])
-                        variable1 = irow[2].replace( " ", "")
-                        #  print( 'variable1', variable1 )
-                        #  writer2.writerow([irow[2]])
-                        #  initialVariableList.append(irow[2])
-                        writer2.writerow( variable1)
-                        initialVariableList.append(variable1)
 
 
-                if not irow[0] == 'Factory                  ':
-                    SPline += 1
-                    #  print(irow[2])
-                    #  data.append(irow[2])
 
-            linecount +=1
-
+    initialVariableList = getInitList( TMVAlog )
 
     print( 'initialVariableList: ', len(initialVariableList), initialVariableList)
     leading50List = leadingNList( initialVariableList, 50 )
@@ -75,6 +36,43 @@ def main(  TMVAlog = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v1H
         #  writer = csv.writer(to_file, delimiter=":")
         #  for new_row in data:
             #  writer.writerow(new_row)
+
+
+def getInitList( TMVAlog ):
+    initialVariableList = []
+    with open(TMVAlog) as csv_file, open("variableList.csv", "w+") as to_file2:
+        csv_reader = csv.reader(csv_file, delimiter=':')
+        writer2 = csv.writer(to_file2, delimiter=":")
+        linecount = 0
+        SPline = 0
+        variable1 = ''
+        for irow in csv_reader: 
+            #Each row returned by the reader is a list of String elements containing the data found by removing the delimiters.
+            #get the SP part
+            if irow[0] == "IdTransformation         ":
+                #  print(irow[1])
+                SPline = linecount+2
+            if (linecount == SPline and not(linecount == 0) ):
+                #  print(irow)
+                if len(irow)>3:
+                    #  if not(irow[2] == ' -----------------------------------------------------') or not(irow[1]=='Factory                  '):
+                    if irow[2] != ' -----------------------------------------------------' and irow[1]!='Factory                  ' and irow[2]!=' Variable                     ':
+                        #  print(irow)
+                        #  print(irow[2])
+                        #  writer2.writerow(irow[2])
+                        variable1 = irow[2].replace( " ", "")
+                        #  print( 'variable1', variable1 )
+                        #  writer2.writerow([irow[2]])
+                        #  initialVariableList.append(irow[2])
+                        writer2.writerow( variable1)
+                        initialVariableList.append(variable1)
+
+                if not irow[0] == 'Factory                  ':
+                    SPline += 1
+                    #  print(irow[2])
+                    #  data.append(irow[2])
+            linecount +=1
+    return initialVariableList
 
 
 
