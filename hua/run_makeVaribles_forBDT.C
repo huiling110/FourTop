ef499b34f8d02b2ea653eca9e6d9667c3dec5693
huilling110 <huiling2016@outlook.com>
2021-07-15 11:04:29 +0800

    produced v45_v1

--- hua/run_makeVaribles_forBDT.C
+++ hua/run_makeVaribles_forBDT.C
@@ -16,8 +16,8 @@ void run_makeVaribles_forBDT(
     if ( !istest){
         ifMergeAllevent = true;
     }
-    // TString inputBase = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v40_fixedHLTBugWithPreselection/";
-    TString inputBase = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v42_addNonBjets/";
+    // TString inputBase = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/v42_addNonBjets/";
+    TString inputBase = "/publicfs/cms/user/fabioiemmi/TauOfTTTT/v45_yieldsCrosscheck_presel2jets_PFJet450required/";
     // TString inputBase = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/test_objectSelction/";
    
 
