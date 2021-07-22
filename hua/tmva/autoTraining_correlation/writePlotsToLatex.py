dbcbcf84719817b9794768f12181840734648f21
huilling110 <huiling2016@outlook.com>
2021-07-23 00:00:45 +0800

    modifies writePlotsToLatex.py a bit so it can deal with different channels

--- hua/tmva/autoTraining_correlation/writePlotsToLatex.py
+++ hua/tmva/autoTraining_correlation/writePlotsToLatex.py
@@ -4,7 +4,9 @@ import os
 
 
 def main():
-    resultsDir = '/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v2Resubmitv1/2tau1l_v1/results/'
+    #  resultsDir = '/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v2Resubmitv1/2tau1l_v1/results/'
+    resultsDir = '/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v2Resubmitv1/1tau2os_v1/results/'
+    channel = 2
      
     #  for entry in os.listdir( resultsDir ):
     allPlots = os.listdir( resultsDir )
@@ -30,24 +32,34 @@ def main():
     print(plotPairList)
 
 
-    filename = '/workfs2/cms/huahuil/slides/tauOfTTTT/plots/2021_4-6/HuilingReport_MVA20210722.tex'
-    fileout = '/workfs2/cms/huahuil/slides/tauOfTTTT/plots/2021_4-6/HuilingReport_MVA20210722_add.tex'
+    #  filename = '/workfs2/cms/huahuil/slides/tauOfTTTT/plots/2021_4-6/HuilingReport_MVA20210722.tex'
+    filename = '/workfs2/cms/huahuil/slides/tauOfTTTT/plots/2021_4-6/HuilingReport_MVA20210722_add.tex'
+    fileout = '/workfs2/cms/huahuil/slides/tauOfTTTT/plots/2021_4-6/HuilingReport_MVA20210722_add2.tex'
         #  with open(filename, 'a') as file:
     with open(filename, 'r+') as file:
         lines = file.readlines()
 
         index = 0
         for iline in lines:
-            if 'subsection' in iline and '2tau1l' in iline :
-                print( iline )
-                sectionIndex = index
+            if channel ==1:
+                if 'subsection' in iline and '1tau1l' in iline :
+                    print( iline )
+                    sectionIndex = index
+            if channel ==2:
+                if 'subsection' in iline and '1tau2os' in iline :
+                    print( iline )
+                    sectionIndex = index
+            if channel ==3:
+                if 'subsection' in iline and '2tau1l' in iline :
+                    print( iline )
+                    sectionIndex = index
             index = index + 1
 
     #  i=0
     for pair in plotPairList:
         #  if i == 0:
             #  i+=1
-        addLinesToList( pair, sectionIndex, lines )
+        addLinesToList( pair, sectionIndex, lines, resultsDir )
         sectionIndex = sectionIndex + 15
     #  print( lines )
 
@@ -56,19 +68,21 @@ def main():
 
 
 
-def addLinesToList( pair, sectionIndex, lines):
+def addLinesToList( pair, sectionIndex, lines, resultsDir ):
     lines.insert( sectionIndex+1, '\n')
     lines.insert( sectionIndex+2, '\\begin{frame}{}\n')
     lines.insert( sectionIndex+3, '  \\begin{columns}[b]\n')
     lines.insert( sectionIndex+4, '   \\column{.5\\textwidth}\n' )
     lines.insert( sectionIndex+5, '    \\centering\n' )
     lines.insert( sectionIndex+6, '    \\begin{figure}\n' )
-    lines.insert( sectionIndex+7, '        \\includegraphics[width=.99\\textwidth]{/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v2Resubmitv1/2tau1l_v1/results/'+pair[0] + '}\n' )
+    #  lines.insert( sectionIndex+7, '        \\includegraphics[width=.99\\textwidth]{/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v2Resubmitv1/2tau1l_v1/results/'+pair[0] + '}\n' )
+    lines.insert( sectionIndex+7, '        \\includegraphics[width=.99\\textwidth]{'+ resultsDir +pair[0] + '}\n' )
     lines.insert( sectionIndex+8, '    \\end{figure}\n' )
     lines.insert( sectionIndex+9, '    \\column{.5\\textwidth}\n' )
     lines.insert( sectionIndex+10, '    \\centering\n' )
     lines.insert( sectionIndex+11, '    \\begin{figure}\n' )
-    lines.insert( sectionIndex+12, '        \\includegraphics[width=.99\\textwidth]{/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v2Resubmitv1/2tau1l_v1/results/' + pair[1] + '}\n' )
+    #  lines.insert( sectionIndex+12, '        \\includegraphics[width=.99\\textwidth]{/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v2Resubmitv1/2tau1l_v1/results/' + pair[1] + '}\n' )
+    lines.insert( sectionIndex+7, '        \\includegraphics[width=.99\\textwidth]{'+ resultsDir +pair[1] + '}\n' )
     lines.insert( sectionIndex+13, '    \\end{figure}\n'  )
     lines.insert( sectionIndex+14, '  \\end{columns}\n'  )
     lines.insert( sectionIndex+15, '\\end{frame}\n'  )
