542b64a9fd547239f332af411900094eb3462155
Fabio Iemmi <fabio.iemmi@cern.ch>
2021-09-20 19:58:23 +0800

    add setup for electron and muon scale factors

--- fabio/scale_factors/scaledYields.cpp
+++ fabio/scale_factors/scaledYields.cpp
@@ -10,9 +10,12 @@
 #include <TROOT.h> //for gROOT
 #include <TSystem.h> // for gSystem
 #include "scaledYields.h"
+//tools for b tagging SFs implementation
 #include "BTagCalibrationStandalone.h"
 #include "BTagCalibrationStandalone.cpp"
 #include "evalEventSF.C"
+//tools for DeepTau SFs implementation
+#include "TauPOG/TauIDSFs/interface/TauIDSFTool.h"
 
 void scaledYields() {
 
@@ -22,7 +25,7 @@ void scaledYields() {
 	//////////////////////////////////////////////////
 	///////// SET UP B-TAGGING SCALE FACTORS /////////
 	//////////////////////////////////////////////////
-
+    
 	// set up calibration + reader
     cout << "Loading the .csv file..." << endl;
     
@@ -61,6 +64,35 @@ void scaledYields() {
 	//////////////////////////////////////////////////
 	//////////////////////////////////////////////////
 
+    //////////////////////////////////////////////////
+	////////// SET UP DeepTau SCALE FACTORS //////////
+	//////////////////////////////////////////////////
+    //comment out the deep tau part for now; we are not ready to implement it yet
+    /*
+
+    std::vector<std::string> years = {
+        
+        "2016Legacy",
+        //"2017ReReco",
+        //"2018ReReco",
+
+    };
+
+    std::vector<std::string> WPs   = {//
+
+        "Loose",
+        "Medium",
+        "Tight"
+
+    };
+
+    std::vector<std::string> IDs   = {
+
+        "DeepTau2017v2p1VSjet", //2017 here has nothing to do with the year of data taking
+        "DeepTau2017v2p1VSe",
+        "DeepTau2017v2p1VSmu",
+    };
+    */
 	//////////////////////////////////////////////////
 	////////// SET UP TRIGGER SCALE FACTORS //////////
 	//////////////////////////////////////////////////
@@ -72,6 +104,37 @@ void scaledYields() {
 	//////////////////////////////////////////////////
 	//////////////////////////////////////////////////
 	
+    //////////////////////////////////////////////////
+	////////// SET UP ELECTRON SCALE FACTORS /////////
+	//////////////////////////////////////////////////
+
+    TFile * input_EGammaSF = new TFile( "electronSFs/2016LegacyReReco_ElectronTight_Fall17V2.root", "READ" );
+	TH2F * EGammaSF = (TH2F*)input_EGammaSF->Get("EGamma_SF2D");
+    TFile * input_SUSYEleSF = new TFile( "electronSFs/ElectronScaleFactors_Run2016.root", "READ" );
+	TH2F * SUSYEleIDplusIPSF = (TH2F*)input_SUSYEleSF->Get("Run2016_MVATightTightIP2D3D");
+    TH2F * SUSYEleISOSF = (TH2F*)input_SUSYEleSF->Get("Run2016_MultiIsoT");
+
+    //////////////////////////////////////////////////
+	//////////////////////////////////////////////////
+	//////////////////////////////////////////////////
+	
+    //////////////////////////////////////////////////
+	//////////// SET UP MUON SCALE FACTORS ///////////
+	//////////////////////////////////////////////////
+
+    TFile* input_MuonIDSF = new TFile( "muonSFs/TnP_NUM_MediumID_DENOM_generalTracks_VAR_map_pt_eta.root" );
+    TH2F * MuonIDSF = (TH2F*)input_MuonIDSF->Get("SF");
+    TFile* input_MuonISOSF = new TFile( "muonSFs/TnP_NUM_MultiIsoMedium_DENOM_MediumID_VAR_map_pt_eta.root" );
+    TH2F * MuonISOSF = (TH2F*)input_MuonISOSF->Get("SF");
+    TFile* input_MuonIP2DSF = new TFile( "muonSFs/TnP_NUM_TightIP2D_DENOM_MediumID_VAR_map_pt_eta.root" );
+    TH2F * MuonIP2DSF = (TH2F*)input_MuonIP2DSF->Get("SF");
+    TFile* input_MuonIP3DSF = new TFile( "muonSFs/TnP_NUM_TightIP3D_DENOM_MediumID_VAR_map_pt_eta.root" );
+    TH2F * MuonIP3DSF = (TH2F*)input_MuonIP3DSF->Get("SF");
+
+    //////////////////////////////////////////////////
+	//////////////////////////////////////////////////
+	//////////////////////////////////////////////////
+
 	map<string, string>::iterator file_it = file.begin();
 
 	TFile *outputfile = new TFile( "scaledYields_output.root", "RECREATE" );
@@ -197,24 +260,128 @@ void scaledYields() {
 					if(is2tau0L) h_2tau0L_noW->Fill(myjetsL->size(), mygenEvtWeight);
 					if(is2tau1L) h_2tau1L_noW->Fill(myjetsL->size(), mygenEvtWeight);
 					if(is2tau2L) h_2tau2L_noW->Fill(myjetsL->size(), mygenEvtWeight);
-
+                    
+                    
 					double * mybtagWeight;
 					mybtagWeight = evalEventSF(myjetsL, myjets_flavor, myjets_btags, CSVreader);
-				
+				    
 					double mytriggerWeight = triggerSF->GetBinContent(triggerSF->FindBin(HT, myjetsL->size()));
 					if (mytriggerWeight == 0.0) mytriggerWeight = 1.0; //correct if HT, njets out of range
 					
-					if(is1tau0L) h_1tau0L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * mybtagWeight[0]);
-					if(is1tau0L_CR) h_1tau0L_CR->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * mybtagWeight[0]);
-					if(is1tau0L_VR) h_1tau0L_VR->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * mybtagWeight[0]);
-					if(is1tau1L) h_1tau1L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * mybtagWeight[0]);
-					if(is1tau2L) h_1tau2L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * mybtagWeight[0]);
-					if(is1tau3L) h_1tau3L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * mybtagWeight[0]);
-					if(is2tau0L) h_2tau0L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * mybtagWeight[0]);
-					if(is2tau1L) h_2tau1L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * mybtagWeight[0]);
-					if(is2tau2L) h_2tau2L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * mybtagWeight[0]);
+					if(is1tau0L) h_1tau0L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight  * mybtagWeight[0]);
+					if(is1tau0L_CR) h_1tau0L_CR->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight  * mybtagWeight[0]);
+					if(is1tau0L_VR) h_1tau0L_VR->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight  * mybtagWeight[0]);
+					if(is1tau1L) {
+
+                        float myEleSF = 1.0;
+                        for (int i = 0; i < myelesMVAT->size(); i++) {
+
+                            //if (ievent % 1000 == 0) cout << "Ele eta: " << myelesMVAT->at(i).Eta() << " Ele pt: " << myelesMVAT->at(i).Pt() << endl;
+                            myEleSF *= EGammaSF->GetBinContent(EGammaSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleIDplusIPSF->GetBinContent(SUSYEleIDplusIPSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleISOSF->GetBinContent(SUSYEleISOSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                        }
+                        if (myEleSF == 0) myEleSF = 1.0;
+                        float myMuSF = 1.0;
+                        for (int i = 0; i < mymuonsT->size(); i++) {
+                            //if (ievent % 1000 == 0) cout << "Mu eta: " << mymuonsT->at(i).Eta() << " Mu pt: " << mymuonsT->at(i).Pt() << endl;
+                            myMuSF *= MuonIDSF->GetBinContent(MuonIDSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonISOSF->GetBinContent(MuonISOSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonIP2DSF->GetBinContent(MuonIP2DSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonIP3DSF->GetBinContent(MuonIP3DSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            
+                        }
+                        if (myMuSF == 0) myMuSF = 1.0;
+                        
+                        //if (ievent % 1000 == 0) cout << "EleSF: " << myEleSF << " MuSF: " << myMuSF << endl;
+                        h_1tau1L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * myEleSF * myMuSF  * mybtagWeight[0]);
+					
+                    }
+                    if(is1tau2L){
+
+                        float myEleSF = 1.0;
+                        for (int i = 0; i < myelesMVAT->size(); i++) {
+
+                            myEleSF *= EGammaSF->GetBinContent(EGammaSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleIDplusIPSF->GetBinContent(SUSYEleIDplusIPSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleISOSF->GetBinContent(SUSYEleISOSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                        }
+                        if (myEleSF == 0) myEleSF = 1.0;
+                        float myMuSF = 1.0;
+                        for (int i = 0; i < mymuonsT->size(); i++) {
+
+                            myMuSF *= MuonIDSF->GetBinContent(MuonIDSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonISOSF->GetBinContent(MuonISOSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonIP2DSF->GetBinContent(MuonIP2DSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonIP3DSF->GetBinContent(MuonIP3DSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            
+                        }
+                        if (myMuSF == 0) myMuSF = 1.0;
+                        h_1tau2L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * myEleSF * myMuSF  * mybtagWeight[0]);
+                        
+                    } 
+					if(is1tau3L) {
+
+                        float myEleSF = 1.0;
+                        for (int i = 0; i < myelesMVAT->size(); i++) {
+
+                            myEleSF *= EGammaSF->GetBinContent(EGammaSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleIDplusIPSF->GetBinContent(SUSYEleIDplusIPSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleISOSF->GetBinContent(SUSYEleISOSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                        }
+                        if (myEleSF == 0) myEleSF = 1.0;
+                        h_1tau3L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * myEleSF  * mybtagWeight[0]);
+                    }
+
+					if(is2tau0L) h_2tau0L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight  * mybtagWeight[0]);
+					if(is2tau1L) {
+
+                        float myEleSF = 1.0;
+                        for (int i = 0; i < myelesMVAT->size(); i++) {
+
+                            myEleSF *= EGammaSF->GetBinContent(EGammaSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleIDplusIPSF->GetBinContent(SUSYEleIDplusIPSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleISOSF->GetBinContent(SUSYEleISOSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                        }
+                        if (myEleSF == 0) myEleSF = 1.0;
+                        float myMuSF = 1.0;
+                        for (int i = 0; i < mymuonsT->size(); i++) {
+
+                            myMuSF *= MuonIDSF->GetBinContent(MuonIDSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonISOSF->GetBinContent(MuonISOSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonIP2DSF->GetBinContent(MuonIP2DSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonIP3DSF->GetBinContent(MuonIP3DSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            
+                        }
+                        if (myMuSF == 0) myMuSF = 1.0;
+                        h_2tau1L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * myEleSF *myMuSF  * mybtagWeight[0]);
+
+                    }
+					if(is2tau2L) {
+
+                        float myEleSF = 1.0;
+                        for (int i = 0; i < myelesMVAT->size(); i++) {
+
+                            myEleSF *= EGammaSF->GetBinContent(EGammaSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleIDplusIPSF->GetBinContent(SUSYEleIDplusIPSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                            myEleSF *= SUSYEleISOSF->GetBinContent(SUSYEleISOSF->FindBin(myelesMVAT->at(i).Eta(),myelesMVAT->at(i).Pt()));
+                        }
+                        if (myEleSF == 0) myEleSF = 1.0;
+                        float myMuSF = 1.0;
+                        for (int i = 0; i < mymuonsT->size(); i++) {
+
+                            myMuSF *= MuonIDSF->GetBinContent(MuonIDSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonISOSF->GetBinContent(MuonISOSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonIP2DSF->GetBinContent(MuonIP2DSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            myMuSF *= MuonIP3DSF->GetBinContent(MuonIP3DSF->FindBin(mymuonsT->at(i).Pt(), fabs(mymuonsT->at(i).Eta())));
+                            
+                        }
+                        if (myMuSF == 0) myMuSF = 1.0;
+                        h_2tau2L->Fill(myjetsL->size(), mygenEvtWeight * myPUWeight * myprefireWeight * mytriggerWeight * myEleSF *myMuSF  * mybtagWeight[0]);
+
+                    }
 				
-					delete mybtagWeight;
+					//delete mybtagWeight;
 
 				}
 
@@ -292,6 +459,18 @@ void scaledYields() {
 	delete outputfile;
 	input_TriggerSF->Close();
 	delete input_TriggerSF;
+    input_EGammaSF->Close();
+    delete input_EGammaSF;
+    input_SUSYEleSF->Close();
+    delete input_SUSYEleSF;
+    input_MuonIDSF->Close();
+    delete input_MuonIDSF;
+    input_MuonISOSF->Close();
+    delete input_MuonISOSF;
+    input_MuonIP2DSF->Close();
+    delete input_MuonIP2DSF;
+    input_MuonIP3DSF->Close();
+    delete input_MuonIP3DSF;
 	
 	gBenchmark->Show("running time");
 
