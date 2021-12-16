94d585656f92e5dc640f36710745f62aeb5e9e57
huilling110 <huiling2016@outlook.com>
2021-12-17 05:23:37 +0800

    added drawHistDistribution.C

---
+++ hua/combine/drawHistDistribution.C
@@ -0,0 +1,17 @@
+
+void drawHistDistribution(
+    TString inputFile = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v3addBtagHLTweights/1tau0l_v1/11bins/dataDriven_1tau0l_forCombine.root"
+){
+    TFile* input = new TFile( inputFile, "Read");
+    TH1D* TTTT = (TH1D*)input->Get( "TTTT_HT");
+    TTTT->Scale(  1 / TTTT->Integral() );
+    TTTT->SetLineColor(kRed);
+    TTTT->SetMaximum(0.4);
+    TH1D *allBgs = (TH1D *)input->Get("allBgs_HT");
+    allBgs->Scale(1 / allBgs->Integral());
+
+    TCanvas *c1 = new TCanvas( "c1", "HT distribution");
+    TTTT->Draw();
+    allBgs->Draw("same");
+    c1->SaveAs("/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v3addBtagHLTweights/1tau0l_v1/11bins/histPlots/HT_distribution.png");
+}
