2200aed0b9c0effea3afcff6bc7ef46f54b3d071
huilling110 <huiling2016@outlook.com>
2022-01-24 01:22:46 +0800

    PS: producePileMC.C works fine

--- hua/producePileupMC.C
+++ hua/producePileupMC.C
@@ -6,10 +6,10 @@ void producePileupMC(
     TChain chain("Events");
     chain.Add( dir + "outTree*.root" );
 
-    TH1D * MCPileup = new TH1D( "MCPileup", "MCPileup", 0, 100, 100);
-    chain.Project( "MCPileup", "Pileup_nPU");
+    TH1D * MCPileup = new TH1D( "pileup", "Pileup_nTrueInt", 100, 0, 100);
+    chain.Project( "pileup", "Pileup_nTrueInt");
 
-    TFile* outFile = new TFile( "PUHistogram_mc2016_postAPV.root", "CREATE");
+    TFile* outFile = new TFile( "PUHistogram_mc2016_postAPV.root", "RECREATE");
     MCPileup->Write();
     outFile->Close();
 }
