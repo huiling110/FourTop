b6499d1007045829aabb36777091158dad8ccc93
huilling110 <huiling2016@outlook.com>
2022-01-28 17:47:30 +0800

    era not working for pre2016 because of different version of nanoAOD

--- objectSelection/run_objectTSelectorForNanoAOD.C
+++ objectSelection/run_objectTSelectorForNanoAOD.C
@@ -6,8 +6,8 @@
 void run_objectTSelectorForNanoAOD(
     Bool_t istest = true,
     // Bool_t istest = false,
-    TString inputDir = "/publicfs/cms/data/TopQuark/nanoAOD/2016/mc/tttt/",
-    // TString inputDir = "/publicfs/cms/data/TopQuark/nanoAOD/2016APV/mc/tttt/",
+    // TString inputDir = "/publicfs/cms/data/TopQuark/nanoAOD/2016/mc/tttt/",
+    TString inputDir = "/publicfs/cms/data/TopQuark/nanoAOD/2016APV/mc/tttt/",
     TString outputDir = "/publicfs/cms/user/fabioiemmi/TauOfTTTT/test_tobjectSelector/",
     TString singleFileName = "outTree_0.root",
     TString eventSelectionBit = "7", // 2 for MetFilters, 4 for HLTSelection, 1 for preselection. so 7 if all selection; 0 if no selection 
@@ -18,7 +18,6 @@ void run_objectTSelectorForNanoAOD(
     gROOT->ProcessLine(".L Loader.C+");
 
     //determine era from inputDir
-    // TString era = inputDir( inputDir.First("nanoAOD")+8, inputDir.First("mc") );
     TString era = inputDir( inputDir.Index("nanoAOD")+8, (inputDir.Index("mc")-inputDir.Index("nanoAOD")-9) );
     cout<<"era is: "<<era<<"\n";
     
