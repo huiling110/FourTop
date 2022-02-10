7301ba336398f502ad04c5d1ae75a4586f1b1b97
Fabio Iemmi <fabio.iemmi@cern.ch>
2022-02-10 22:58:39 +0800

    read the golden JSON and save good lumi sections

--- objectSelection/objectTSelectorForNanoAOD.C
+++ objectSelection/objectTSelectorForNanoAOD.C
@@ -252,6 +252,55 @@ void objectTSelectorForNanoAOD::SlaveBegin(TTree * /*tree*/)
    era = option2;
    std::cout << "era is: " << era << "\n";
 
+   //if (isdata) {// deal with Lumi JSONs only if reading data
+       
+       TString jsonInFile = GoldenJSONs[era];
+       if (jsonInFile){
+
+           std::cout << "Reading JSON file " << jsonInFile << std::endl;
+           std::ifstream cfgfile(jsonInFile);
+           if (!cfgfile.good()){
+               std::cout << "File " << jsonInFile << " is not a correct file - please check JSON file strings in configuration file.\nExiting." << std::endl;
+               exit(8);
+           }
+           //cout << "0" << endl;
+           string JSON = "";
+           string readLine;
+           bool firstline = true;
+           while (getline(cfgfile,readLine)) { // read all lines from JSON file
+
+               std::stringstream ss(readLine);
+               string line = ss.str();
+               if (!firstline && line!="}") line.erase(0,1); //erase first charater: in golden JSONs each line starts with two spaces, but we want just one separating good runs; also, save last curly bracket
+               JSON.append(line);
+               if (firstline) firstline = false;
+
+           } 
+           TString processString = JSON;
+           TString segment;
+           Ssiz_t from = 0;
+           while(processString.Tokenize(segment,from,", \"")){ 
+               // JSON file has a layout like this: "284029": [[1, 112]], "284035": [[1, 369]], etc
+               // So first tokenize with respect to the string ', "' which separates each run+lumi block
+               // You get tokens in the form 284029": [[1, 112]]
+               TObjArray *mapItem = segment.Tokenize("\"");
+               // Now tokenize with respect to the string '"'
+               // You get two-element tokens with 1st being '284029' and second being ': [[1, 112]]'
+               Int_t key = (((TObjString *)(mapItem->At(0)))->String()).Atoi();
+               TString value = ((TObjString *)(mapItem->At(1)))->String();
+               TObjArray *values = value.Tokenize("[], :}");
+               // Finally tokenize the second element to remove everything which is not an integer
+               // You get the good lumi sections
+               std::vector<Int_t> valueVec;
+               for (Int_t i = 0; i < values->GetEntries(); i++){
+                   valueVec.push_back( (((TObjString *)(values->At(i)))->String()).Atoi());
+               }
+               _goodLumis[key] = valueVec;
+           }
+       }// end if JSON file
+
+       //}  
+
    TString outFileName = option1;
    outputfile = new TFile( outFileName, "RECREATE");
    cout<<"outputFileName: "<<outputfile->GetName()<<endl;
@@ -306,7 +355,6 @@ Bool_t objectTSelectorForNanoAOD::Process(Long64_t entry)
    }
    
    allEvents->Fill();
-
 
    //MET filters
    if ( MetFilters ){ //recommendations from here: https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2#MET_Filter_Recommendations_for_R
--- objectSelection/objectTSelectorForNanoAOD.h
+++ objectSelection/objectTSelectorForNanoAOD.h
@@ -113,6 +113,17 @@ public :
        {"2018", 0.7665},
 
    };
+
+   map<TString, TString> GoldenJSONs = { //map of golden JSONs
+
+       {"2016postVFP", "../golden_JSONs/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"},
+       {"2016preVFP", "../golden_JSONs/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"},
+       {"2017", "../golden_JSONs/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt"},
+       {"2018", "../golden_JSONs/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt"},
+
+   };
+
+   map<Int_t,std::vector<Int_t> > _goodLumis;
    Bool_t MetFilters = true;
    // Bool_t MetFilters = false;
    Bool_t HLTSelection = true;
