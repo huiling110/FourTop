33c3ae2a649f03360f0743989cb3cfb603e9c477
huilling110 <huiling2016@outlook.com>
2022-02-18 06:34:44 +0800

    added Linedef.h an Makefile

---
+++ objectSelection/Linkdef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+// includes all headrefiles
+#include "objectTSelectorForNanoAOD.h"
+
+// All classes
+// #pragma link C++ class EventLoop+;
+#pragma link C++ class objectTSelectorForNanoAOD+;
+
+
+#endif
