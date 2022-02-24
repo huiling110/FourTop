d5aea364ae5eb6a6b336044cb300984991b30d98
Fabio Iemmi <fabio.iemmi@cern.ch>
2022-02-25 01:07:46 +0800

    add list of files for UL2016_preVFP

--- fabio/UltraLegacy/trigger_studies/trigEff.h
+++ fabio/UltraLegacy/trigger_studies/trigEff.h
@@ -24,6 +24,49 @@ map<string, string> BTVSF_files = { //MAP OF INPUT BTV SCALE FACTOR FILES
 
 map<string, map<string, string>> file = { //MAP OF INPUT FILES
 
+    {"UL2016_preVFP", 
+     {   //signal
+         
+         {"tttt", "tttt"}, //broken
+
+         //ttbar background    
+         {"ttbar-FH", "ttbar_0l"},//broken
+         {"ttbar-SL", "ttbar_1l"},
+         {"ttbar-DL", "ttbar_2l"},
+
+         //QCD background    
+         {"QCD_HT200to300", "qcd_200to300"},
+         {"QCD_HT300to500", "qcd_300to500"},
+         {"QCD_HT500to700", "qcd_500to700"},
+         {"QCD_HT700to1000", "qcd_700to1000"},
+         {"QCD_HT1000to1500", "qcd_1000to1500"},
+         {"QCD_HT1500to2000", "qcd_1500to2000"},
+         {"QCD_HT2000toInf", "qcd_2000toInf"},
+
+         //tt+X background   
+         {"ttW+jets", "ttW"}, //broken   
+         {"ttZ+jets", "ttZ"}, //broken   
+         {"ttG+jets", "ttG"}, //broken   
+         {"ttH_bb", "ttH_bb"}, //broken  
+         {"ttH_nonbb", "ttH_nonbb"}, //broken    
+
+         //single-top background 
+         {"st_tchan", "st_tchan"},
+         {"st_antitchan", "st_antitchan"},
+         {"st_tW_top", "st_tW_top"},
+         {"st_tW_antitop", "st_tW_antitop"},
+         {"st_tZq", "st_tZq"},
+
+         {"data_singleMuon_eraB_v1", "singleMu_2016B_v1"},
+         {"data_singleMuon_eraB_v2", "singleMu_2016B_v2"},
+         {"data_singleMuon_eraC", "singleMu_2016C"},    
+         {"data_singleMuon_eraD", "singleMu_2016D"},
+         {"data_singleMuon_eraE", "singleMu_2016E"},
+         {"data_singleMuon_eraF_hipm", "singleMu_2016F_hipm"},
+         
+     }
+    }, //UL2016_preVFP
+
     {"UL2016_postVFP", 
      {   //signal
          
@@ -60,46 +103,6 @@ map<string, map<string, string>> file = { //MAP OF INPUT FILES
          {"data_singleMuon_eraF", "singleMu_2016F"},    
          {"data_singleMuon_eraG", "singleMu_2016G"},    
          {"data_singleMuon_eraH", "singleMu_2016H"},    
-
-         /*  
-         //diboson background    
-         {"WZ", "WZ_TuneCUETP8M1_13TeV-pythia8"},    
-         {"WW", "WW_TuneCUETP8M1_13TeV-pythia8"},    
-         {"ZZ", "ZZ_TuneCUETP8M1_13TeV-pythia8"},    
-         {"WG+jets", "WGJets_MonoPhoton_PtG-40to130_TuneCUETP8M1_13TeV-madgraph"},   
-         {"ZG+jets", "ZGJetsToLLG_EW_LO_13TeV-sherpa"},  
-
-         //triboson background   
-         {"WWW", "WWW_4F_TuneCUETP8M1_13TeV-amcatnlo-pythia8"},  
-         {"WWZ", "WWZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8"}, 
-         //{"WWG", "WWG_TuneCUETP8M1_13TeV-amcatnlo-pythia8"},   
-         {"ZZZ", "ZZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8"}, 
-         {"WZZ", "WZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8"}, 
-         {"WZG", "WZG_TuneCUETP8M1_13TeV-amcatnlo-pythia8"}, 
-         {"WGG", dir +"WGG_5f_TuneCUETP8M1_13TeV-amcatnlo-pythia8"}, 
-         {"ZGG+jets", "ZGGJets_ZToHadOrNu_5f_LO_madgraph_pythia8"},      
-
-         //W+jets background 
-         //{"W+jets", "WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"},  
-
-         //DY background 
-         {"DY", "DYJetsToTauTau_ForcedMuEleDecay_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext1"},    
-
-         //Higgs background  
-         {"VHToNonbb", "VHToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8"}, 
-         {"ZHToTauTau", "ZHToTauTau_M125_13TeV_powheg_pythia8"}, 
-         {"ZHTobb", "ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8"}, 
-         {"GluGluHTo4L", "GluGluHToZZTo4L_M125_13TeV_powheg2_JHUgenV6_pythia8"},
-         {"GluGluHTobb", "GluGluHToBB_M125_13TeV_amcatnloFXFX_pythia8"}, 
-         {"GluGluHToGG", "GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8"}, 
-         {"GluGluHToMuMu", "GluGluHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8"},    
-         {"GluGluHToTauTau", "GluGluHToTauTau_M125_13TeV_powheg_pythia8"},   
-         {"GluGluHTo2L2Nu", "GluGluHToWWTo2L2Nu_M125_13TeV_powheg_JHUgen_pythia8"},  
-         {"GluGluHToLNuqq", "GluGluHToWWToLNuQQ_M125_13TeV_powheg_JHUGenV628_pythia8"},  
-         {"VBFHTo2L2Nu", "VBFHToWWTo2L2Nu_M125_13TeV_powheg_JHUgenv628_pythia8"},    
-         {"VBFHToMuMu", "VBFHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8"},  
-         {"VBFHToGG", "VBFHToGG_M125_13TeV_amcatnlo_pythia8_v2"},    
-         */
         
      }
     }, //UL2016_postVFP
