49d58ff753535b6649bd5a9d49e3d06335df1fdc
huilling110 <huiling2016@outlook.com>
2022-03-02 20:53:45 +0800

    submitted forMVA/2016_postVFP/v1

---
+++ hua/makeVariables/Jobsubmit_seperate/qcd_700to1000.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/bin/hostname
+cd /workfs2/cms/huahuil/4topCode/CMSSW_10_6_27/src/FourTop/hua/makeVariables
+root -l -b -q 'run_makeVaribles_forBDT.C(false,"/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/UL2016_postVFP/v7_preselectionAddingTausL/mc/","qcd_700to1000","/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016_postVFP/v1/")'
