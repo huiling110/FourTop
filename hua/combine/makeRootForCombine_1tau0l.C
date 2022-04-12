800ac08af04f9040055738b0e49e3757ae219bea
huilling110 <huiling2016@outlook.com>
2022-04-12 21:22:28 +0800

    makeRootForCombine_1tau0l.C fit for nano

--- hua/combine/makeRootForCombine_1tau0l.C
+++ hua/combine/makeRootForCombine_1tau0l.C
@@ -45,18 +45,22 @@ void makeRootForCombine_1tau0l(){
     TT->Scale(lumiMap[era_g]);
     TT->Print();
     // TT->Write();
-/*
-    // TH1D* TTX = addHistChannel( ES1tau0l, weight, "jets_HT", binNum, binMin, binMax, 4, 9, "TTX_HT" );
     TH1D* TTX = addHistChannel( ES1tau0l, basicWeight, "jets_HT", binNum, binMin, binMax, 4, 9, "TTX_HT" );
+    TTX->Scale(lumiMap[era_g]);
 
-    TH1D* VV = addHistChannel( ES1tau0l, weight, "jets_HT", binNum, binMin, binMax, 9, 12, "VV_HT" );
+    TH1D* VV = addHistChannel( ES1tau0l, basicWeight, "jets_HT", binNum, binMin, binMax, 9, 12, "VV_HT" );
+    VV->Scale(lumiMap[era_g]);
 
-    TH1D*  SingleTop = addHistChannel( ES1tau0l, weight, "jets_HT", binNum, binMin, binMax, 12, 15, "SingleTop_HT" );
+    TH1D*  SingleTop = addHistChannel( ES1tau0l, basicWeight, "jets_HT", binNum, binMin, binMax, 12, 15, "SingleTop_HT" );
+    SingleTop->Scale(lumiMap[era_g]);
 
-    TH1D*  TTTT = addHistChannel( ES1tau0l, weight, "jets_HT", binNum, binMin, binMax, 0, 1, "TTTT_HT" );
-    TH1D* data_obs = addHistChannel( ES1tau0l, weight, "jets_HT", binNum, binMin, binMax, 0, 24, "data_obs_HT" );
+    TH1D*  TTTT = addHistChannel( ES1tau0l, basicWeight, "jets_HT", binNum, binMin, binMax, 0, 1, "TTTT_HT" );
+    TTTT->Scale(lumiMap[era_g]);
+    TH1D* data_obs = addHistChannel( ES1tau0l, basicWeight, "jets_HT", binNum, binMin, binMax, 0, 24, "data_obs_HT" );
+    data_obs->Scale(lumiMap[era_g]);
     //QCD starts from index 30
-    TH1D *allBgs = addHistChannel(ES1tau0l, weight, "jets_HT", binNum, binMin, binMax, 1, 15, "allBgs_HT");
+    TH1D *allBgs = addHistChannel(ES1tau0l, basicWeight, "jets_HT", binNum, binMin, binMax, 1, 15, "allBgs_HT");
+    allBgs->Scale(lumiMap[era_g]);
     cout << "allBGs before QCD = " << allBgs->Integral();
     allBgs->SetDefaultSumw2();
     allBgs->Add(my_QCD_HT);
@@ -66,12 +70,16 @@ void makeRootForCombine_1tau0l(){
 
     TString outFolder = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v2Add2Variables_fromV9/1tau0l_HT/templatesPlots/";
     drawTemplates( TT, outFolder );
+    drawTemplates( TTX, outFolder );
+    drawTemplates( VV, outFolder );
+    drawTemplates( SingleTop, outFolder );
+    drawTemplates( TTTT, outFolder );
+    drawTemplates( my_QCD_HT, outFolder );
 
 
 
     output->Write();
     output->Close();
-*/
 
 
 }
