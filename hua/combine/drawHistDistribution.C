9532399acc3161f313081c85be6404d6ddec39e9
huilling110 <huiling2016@outlook.com>
2022-04-13 05:54:09 +0800

    added writeCombinationDatacard.py

--- hua/combine/drawHistDistribution.C
+++ hua/combine/drawHistDistribution.C
@@ -1,7 +1,8 @@
 
 void drawHistDistribution(
     // TString inputFile = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v3addBtagHLTweights/2tau0l_HT/11bins/dataDriven_2tau0l_forCombine.root"
-    TString inputFile = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v2Add2Variables_fromV9/1tau0l_HT/dataDriven_1tau0l_forCombine.root"
+    // TString inputFile = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v2Add2Variables_fromV9/1tau0l_HT/dataDriven_1tau0l_forCombine.root"
+    TString inputFile = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v2Add2Variables_fromV9/2tau1l_HT/HT_2tau1l_forCombine.root"
 ){
     TFile* input = new TFile( inputFile, "Read");
     TH1D* TTTT = (TH1D*)input->Get( "TTTT_HT");
