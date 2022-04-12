db5520addf98f11e7ae0f7853a7463542e04336c
huilling110 <huiling2016@outlook.com>
2022-04-12 23:45:04 +0800

    small modifications

--- hua/combine/drawHistDistribution.C
+++ hua/combine/drawHistDistribution.C
@@ -1,6 +1,7 @@
 
 void drawHistDistribution(
-    TString inputFile = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v3addBtagHLTweights/2tau0l_HT/11bins/dataDriven_2tau0l_forCombine.root"
+    // TString inputFile = "/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v3addBtagHLTweights/2tau0l_HT/11bins/dataDriven_2tau0l_forCombine.root"
+    TString inputFile = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v2Add2Variables_fromV9/1tau0l_HT/dataDriven_1tau0l_forCombine.root"
 ){
     TFile* input = new TFile( inputFile, "Read");
     TH1D* TTTT = (TH1D*)input->Get( "TTTT_HT");
@@ -13,5 +14,7 @@ void drawHistDistribution(
     TCanvas *c1 = new TCanvas( "c1", "HT distribution");
     TTTT->Draw();
     allBgs->Draw("same");
-    c1->SaveAs("/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v3addBtagHLTweights/2tau0l_HT/11bins/histPlots/HT_distribution.png");
+    // c1->SaveAs("/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v3addBtagHLTweights/2tau0l_HT/11bins/histPlots/HT_distribution.png");
+    TString outFileName = inputFile.ReplaceAll( ".root", "_HTshape.png");
+    c1->SaveAs( outFileName );
 }
