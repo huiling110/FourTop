71fe6b3f87369ad4600a0898449da288865c6b36
huilling110 <huiling2016@outlook.com>
2022-05-18 07:12:52 +0800

    removed job scripts

--- hua/makeVariables/Jobsubmit_seperate/wz.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/bin/hostname
-cd /workfs2/cms/huahuil/4topCode/CMSSW_10_6_27/src/FourTop/hua/makeVariables
-root -l -b -q 'run_makeVaribles_forBDT.C(false,"/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/UL2016_preVFP/v9_allSelection/mc/","wz","/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016_preVFP/v5addWeight_fromV9/")'
