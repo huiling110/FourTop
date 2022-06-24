0f0dd853f00934baaf83c6f552a7b9d2a050930d
huilling110 <huiling2016@outlook.com>
2022-06-24 19:38:00 +0800

    submitted v14_MetFilterHLTSelection 2016post

--- objectSelection/makeJob_objectTSelectorForNanoAOD.py
+++ objectSelection/makeJob_objectTSelectorForNanoAOD.py
@@ -23,13 +23,15 @@ def main():
     # jobVersionName = 'v10_JECandTESadded/'
     # jobVersionName = 'v11_JECandTESInObjectSelectinLevel/'
     # jobVersionName = 'v12_addedMissingBranch/'
-    jobVersionName = 'v13_addedMissingFilterBranches/'
+    # jobVersionName = 'v13_addedMissingFilterBranches/'
+    jobVersionName = 'v14_MetFilterHLTSelection/'
     onlyMC = True
     # onlyMC = False
     era = '2016'
     # era = '2016APV'
     # era = '2018'
-    eventSelection = '0'
+    # eventSelection = '0'
+    eventSelection = '3'
    # 1 for MetFilters, 2 for HLTSelection, 4 for preSelection. so 7 if all selection; 0 if no selection 
     isHuiling = True
     dataList = [ 'jetHT', 'singleMu'] 
