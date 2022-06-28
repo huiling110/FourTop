8af3e8da2c42593c9463bbf5051f8ffd4d7aa435
huilling110 <huiling2016@outlook.com>
2022-06-29 07:29:06 +0800

    makeJob_forWriteHist.py seems good

---
+++ hua/writeVariableHist/makeJob_forWriteHist.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import subprocess
+
+inputDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016/v0baselineSelection_fromV15/'
+jobDir = 'jobSH/'
+Jobsubmitpath = '/workfs2/cms/huahuil/4topCode/CMSSW_12_2_4/src/FourTop/hua/writeVariableHist/' 
+outputDir = inputDir + 'variableHists/'
+
+def main():
+
+    if not os.path.exists( jobDir ):
+        os.mkdir( jobDir )
+
+    subAllProcess = open( 'subAllProcess.sh', 'w')
+    subAllProcess.write('#!/bin/bash\n')
+    subAllProcess.write('cd '+ Jobsubmitpath +'\n')
+
+    for iFile in os.listdir( inputDir ):
+        if '.root' in iFile:
+            iProcess = iFile.split('.root')[0]
+            print(iProcess)
+            iJobFile = jobDir + iProcess +'.sh' 
+            makeIjob( iJobFile, iProcess )  
+
+        if not os.path.exists(outputDir +"/log/" ):
+            os.mkdir( outputDir  +"/log/")
+        logFile = outputDir +   "log/" + iProcess + ".log"
+        errFile = outputDir +  "log/" + iProcess +".err"
+        subAllProcess.write('hep_sub '+ iJobFile + ' -o ' + logFile + ' -e ' + errFile +'\n' )
+
+    subprocess.run('chmod 777 ' + Jobsubmitpath + jobDir +'*sh',  shell=True)
+    subprocess.run('chmod 777 ' + Jobsubmitpath + 'subAllProcess.sh', shell=True)
+
+
+
+
+
+def makeIjob( shFile, iProcess ):
+    subFile = open( shFile, "w" )
+    subFile.write('#!/bin/bash\n')
+    subFile.write('cd '+ Jobsubmitpath + '\n' )
+    subFile.write( 'root -q -b \'run_writeHist.C( \"{}\", \"{}\" )\' '.format( inputDir, iProcess ) )
+    print( 'done writing: ', shFile)
+
+
+if __name__=='__main__':
+    main()
