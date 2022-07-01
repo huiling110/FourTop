161fbb64fd8722d15f50bc5d2f8c6332fa63f905
huilling110 <huiling2016@outlook.com>
2022-07-01 16:55:40 +0800

    add2016MVAtrees.py

--- hua/add2016MVAtrees.py
+++ hua/add2016MVAtrees.py
@@ -12,36 +12,48 @@ import subprocess
 # pre_dir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016_preVFP/v4modifiedMinDeltaR_fromV9/'
 # post_dir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016_postVFP/v4modifiedMinDeltaR_fromV9/' 
 # merged_dir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016/v4modifiedMinDeltaR_fromV9/'
-forMVAFile = 'v0baseline_v16_HLTselection/'
+version = 'v0baseline_v16_HLTselection/'
+ifJustMC = True
 
 # pre_dir_base = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016_preVFP/'
 # post_dir_base = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016_postVFP/'
 pre_dir_base = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016preVFP/'
 post_dir_base = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016postVFP/'
 merged_dir_base = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016/'
-pre_dir = pre_dir_base + forMVAFile
-post_dir = post_dir_base + forMVAFile
-merged_dir = merged_dir_base + forMVAFile
+pre_dir = pre_dir_base + version
+post_dir = post_dir_base + version
+merged_dir = merged_dir_base + version
 
 
 if not os.path.exists( merged_dir ):
     os.mkdir( merged_dir )
 
+preDirDict = {}
+postDirDict = {}
+mergedDirDict = {}
+preDirDict['mc'] = pre_dir + 'mc/'
+postDirDict['mc'] = post_dir + 'mc/'
+mergedDirDict ['mc']= merged_dir + 'mc/'
 
-for i in os.listdir( pre_dir ):
 
-    if os.path.isdir( pre_dir + i ):
-        continue
+for ikey in preDirDict.keys():
+    print( ikey )
+    if not os.path.exists( mergedDirDict[ikey] ):
+        os.mkdir( mergedDirDict[ikey] )
+    for i in os.listdir( preDirDict[ikey] ):
 
-    ifile = pre_dir  + i
-    ifile_post = post_dir + i 
-    ifile_merged = merged_dir + i
+        if os.path.isdir( preDirDict[ikey] + i ):
+            continue
 
-    icommand = 'hadd {} {} {}'.format( ifile_merged, ifile, ifile_post )
-    print( icommand )
-    process = subprocess.run( icommand, shell=True )
-    output = process.stdout
-    print( output )
+        ifile = preDirDict[ikey]  + i
+        ifile_post = postDirDict[ikey] + i 
+        ifile_merged = mergedDirDict[ikey] + i
+
+        icommand = 'hadd {} {} {}'.format( ifile_merged, ifile, ifile_post )
+        print( icommand )
+        process = subprocess.run( icommand, shell=True )
+        output = process.stdout
+        print( output )
 
 
  
