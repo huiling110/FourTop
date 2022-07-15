a84659eebdd2352fab083efc37c6d5de3c3d7fff
huilling110 <huiling2016@outlook.com>
2022-07-15 20:32:28 +0800

    updated writeGenSumToCSV

--- objectSelection/checkInOutFile.py
+++ objectSelection/checkInOutFile.py
@@ -17,7 +17,37 @@ def main():
     # checkInOutFileNumber( inOutListMC )
     # checkInOutFileNumber( inOutListData )
     
-    # writeGenSumToCSV( inOutListMC[1] )
+    writeGenSumToCSV( inOutListMC[1] )
+
+    # genSumDic = getGenSumDic( 'genWeightCSV/genSum_2016postVFP.csv' )
+    # writeHistsOneFileOneProcess( inOutListMC[1] )
+
+
+    # calCutFlow()
+
+def getGenSumDic( inputCsv ):
+    df = pd.read_csv( inputCsv )
+    genSumDic = df.to_dict()
+    print( genSumDic )
+    return genSumDic
+
+
+# de/ calCutFlow( mcDir, dataDir ):
+
+def writeHistsOneFileOneProcess( indir ):
+    for iPro in os.listdir( indir ):
+        print( iPro )
+        iHist = ROOT.TH1D( 'initial_' + iPro + '_' + 'onlygenWeight' )
+        for ifile in os.listdir( indir+iPro ):
+            if 'log' in ifile: continue
+            # print( ifile )
+            iRoot = ROOT.TFile( indir+iPro+ '/'+ ifile, 'READ' )
+            ihist_initial = iRoot.Get( 'h_initial' )
+            iHist.Add( ihist_initial )
+            iRoot.Close()
+        iHist.Scale(  )
+
+
 
 def writeGenSumToCSV( outDir ):
     processList = []
@@ -43,9 +73,10 @@ def writeGenSumToCSV( outDir ):
     # series = pd.Series( genWeightDic, name='2016postVFP')
 
     df = pd.DataFrame.from_dict( genWeightDic, orient='index' , columns=['2016postVFP'])
+    df['process'] = df.index
+    print( df )
     df.to_csv( 'genWeightCSV/' + 'genSum_2016postVFP.csv'  )
 
-    print( df )
 
 
 
