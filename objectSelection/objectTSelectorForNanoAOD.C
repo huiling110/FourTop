d7cba8d6cfff3dde887a08d452c73514c42c0165
huilling110 <huiling2016@outlook.com>
2022-08-25 23:18:35 +0800

    added back sorting for taus and jets

--- objectSelection/objectTSelectorForNanoAOD.C
+++ objectSelection/objectTSelectorForNanoAOD.C
@@ -115,7 +115,6 @@ Bool_t objectTSelectorForNanoAOD::Process(Long64_t entry)
         genWeight_allEvents = *Generator_weight;
     }
 
-    //!!!branch variable intialization to prevent them from get values from last event
 
     // good lumi selection
     //???not finding twiki for this
@@ -187,6 +186,7 @@ Bool_t objectTSelectorForNanoAOD::Process(Long64_t entry)
 
     //!!!very important to give value to default values to variables for each event!!!
     initializeBrancheValues();
+    //!!!branch variable intialization to prevent them from get values from last event
 
     run_ = *run;
     copyHLT_new(isdata, dataSet);
@@ -247,7 +247,11 @@ Bool_t objectTSelectorForNanoAOD::Process(Long64_t entry)
     SelectTaus(tausT, tausT_index, tausT_decayMode, tausT_genPartFlav, 3, leptonsMVAL, tauTES);
     SelectTaus(tausT_TESup, tausT_index_TESup, tausT_decayMode_TESup, tausT_genPartFlav_TESup, 3, leptonsMVAL, 0);
     SelectTaus(tausT_TESdown, tausT_index_TESdown, tausT_decayMode_TESdown, tausT_genPartFlav_TESdown, 3, leptonsMVAL, 0);
-    // sort( tausT.begin(), tausT.end(), compEle);
+    sort( tausT.begin(), tausT.end(), compEle);
+    sort( tausF.begin(), tausF.end(), compEle);
+    sort( tausL.begin(), tausL.end(), compEle);
+    sort( tausT_TESup.begin(), tausT_TESup.end(), compEle);
+    sort( tausT_TESdown.begin(), tausT_TESdown.end(), compEle);
     //  //???does here imply we need at least 1 leptons
     tausT_total = tausT_total + tausT.size();
     tausF_total = tausF_total + tausF.size();
@@ -266,16 +270,21 @@ Bool_t objectTSelectorForNanoAOD::Process(Long64_t entry)
     SelectJets(ifJER, 0, deepJet, jets, jets_btags, jets_index, jets_flavour, leptonsMVAL, tausL, 0);
     SelectJets(ifJER, 0, deepJet, jets_JECup, jets_btags_JECup, jets_index_JECup, jets_flavour_JECup, leptonsMVAL, tausL, 1);
     SelectJets(ifJER, 0, deepJet, jets_JECdown, jets_btags_JECdown, jets_index_JECdown, jets_flavour_JECdown, leptonsMVAL, tausL, 2);
+    sort( jets.begin(), jets.end(), compEle);
+    sort( jets_JECup.begin(), jets_JECup.end(), compEle);
+    sort( jets_JECdown.begin(), jets_JECdown.end(), compEle);
     // std::cout << "jets size" << jets.size() << "\n";
     // printElements( jets_btags, jets );
     // std::cout<<"jets_JECup:  "; printElements( jets_btags_JECup, jets_JECup );
     // std::cout<<"jets_JECdown:  "; printElements( jets_btags_JECdown, jets_JECdown );
     // pt are sorted in MINIAOD
-    // sort( jets.begin(), jets.end(), compEle);
     SelectJets(ifJER, 11, deepJet, bjetsL, bjetsL_btags, bjetsL_index, bjetsL_flavour, leptonsMVAL, tausL, 0);
     SelectJets(ifJER, 12, deepJet, bjetsM, bjetsM_btags, bjetsM_index, bjetsM_flavour, leptonsMVAL, tausL, 0);
     SelectJets(ifJER, 13, deepJet, bjetsT, bjetsT_btags, bjetsT_index, bjetsT_flavour, leptonsMVAL, tausL, 0);
     SelectJets(ifJER, 2, deepJet, forwardJets, forwardJets_btags, forwardJets_index, forwardJets_flavour, leptonsMVAL, tausL, 0);
+    sort( bjetsL.begin(), bjetsL.end(), compEle);
+    sort( bjetsM.begin(), bjetsM.end(), compEle);
+    sort( bjetsT.begin(), bjetsT.end(), compEle);
 
     jetsSubstructBjets(nonbjetsL, jets, bjetsL);
     jetsSubstructBjets(nonbjetsM, jets, bjetsM);
