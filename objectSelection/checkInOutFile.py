99fd6c3105e18978b42123109a1e8334bee3aaec
huilling110 <huiling2016@outlook.com>
2022-08-25 21:22:50 +0800

    moves some code to writeHistsToOneFile.py

--- objectSelection/checkInOutFile.py
+++ objectSelection/checkInOutFile.py
@@ -26,12 +26,6 @@ def main():
     
     # writeGenSumToCSV( inOutListMC[1] )
 
-    # genSumDic = getGenSumDic( 'genWeightCSV/genSum_2016postVFP.csv' )
-    # lumi = lumiMap[iera]
-    # fileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016postVFP/objectSelectionResults/' + jobVersionName
-    # uf.checkMakeDir( fileDir )
-    # writeHistsOneFileOneProcess( inOutListMC[1], genSumDic, samplesCrossSection, lumi, fileDir+'mc/' )
-    # writeHistsOneFileOneProcess( inOutListData[1], genSumDic, samplesCrossSection, lumi, fileDir+'data/', True )
     
 
     variableList = ['forEventCount']
@@ -51,52 +45,10 @@ def main():
     writeHistsToCSV( sumProcessPerVar,  inputDir['mc']+'results/', 'cutFlowRawEntries_objectSelection.csv', True)
 
 
-#copied to 
-def getGenSumDic( inputCsv ):
-    df = pd.read_csv( inputCsv )
-    # genSumDic = df.to_dict()
-    genSumDic = pd.Series( df['2016postVFP'].values, index=df['process'] ).to_dict()
-    print( genSumDic )
-    return genSumDic
 
 
 # de/ calCutFlow( mcDir, dataDir ):
 
-def writeHistsOneFileOneProcess( indir, genSumDic, samplesCrossSection, lumi, outDir, isData=False ):
-    uf.checkMakeDir( outDir )
-    for iPro in os.listdir( indir ):
-        print( iPro )
-        iRootFile = ROOT.TFile( outDir+iPro+'.root', "RECREATE")
-        # iRootFile = ROOT.TFile( outDir+iPro+'_variableHists.root', "RECREATE")
-        iHist = ROOT.TH1D( 'OBinitial_' + iPro + '_' + 'forEventCount', 'initial_' + iPro + '_' + 'onlygenWeight', 2, -1, 1 )
-        iHistHLT = ROOT.TH1D( 'OBHLT_' + iPro + '_' + 'forEventCount', 'HLT_' + iPro + '_' + 'onlygenWeight', 2, -1, 1 )
-        iHistPre = ROOT.TH1D( 'OBpreSelection_' + iPro + '_' + 'forEventCount', 'preSelection_' + iPro + '_' + 'onlygenWeight', 2, -1, 1 )
-        iHist.SetDirectory(iRootFile)
-        for ifile in os.listdir( indir+iPro ):
-            if 'log' in ifile: continue
-            # print( ifile )
-            iRoot = ROOT.TFile( indir+iPro+ '/'+ ifile, 'READ' )
-            ihist_initial = iRoot.Get( 'h_initial' )
-            ihist_HLT = iRoot.Get( 'h_afterHLT' )
-            ihist_pre = iRoot.Get( 'h_afterpreSelection')
-            iHist.Add( ihist_initial )
-            iHistHLT.Add( ihist_HLT )
-            iHistPre.Add( ihist_pre)
-            # iHist.SetDirectory(iRootFile)
-            iRoot.Close()
-
-        if isData: 
-            iProScale = 1.0
-        else: 
-            iProScale = lumi*samplesCrossSection[iPro]/genSumDic[iPro]
-        iHist.Scale( iProScale )
-        iHistHLT.Scale( iProScale )
-        iHistPre.Scale( iProScale )
-        iHist.Print()
-
-        iRootFile.Write()
-        print( 'written: ', iRootFile.GetName() )
-        iRootFile.Close()
 
 
 
---
+++ objectSelection/writeHistsToOneFile.py
@@ -0,0 +1,74 @@
+
+import pandas as pd
+import os
+import ROOT
+import usefulFunc as uf
+from ttttGlobleQuantity import (histoGramPerSample, lumiMap, samples,
+                                samplesCrossSection, summedProcessList)
+
+from makeJob_objectTSelectorForNanoAOD import (era, eraDic, inputBase,
+                                               jobVersionName, outputBase)
+
+
+def main():
+
+    iera = '2016postVFP'
+    inOutListMC = [ inputBase + era +'/' + 'mc/', outputBase + eraDic[era] + '/' +jobVersionName  + 'mc/' ]
+    inOutListData = [ inputBase + era +'/' + 'data/',outputBase + eraDic[era] + '/' +jobVersionName  + 'data/' ]
+
+
+    genSumDic = getGenSumDic( 'genWeightCSV/genSum_2016postVFP.csv' )
+    lumi = lumiMap[iera]
+    fileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016postVFP/objectSelectionResults/' + jobVersionName
+    uf.checkMakeDir( fileDir )
+    writeHistsOneFileOneProcess( inOutListMC[1], genSumDic, samplesCrossSection, lumi, fileDir+'mc/' )
+    writeHistsOneFileOneProcess( inOutListData[1], genSumDic, samplesCrossSection, lumi, fileDir+'data/', True )
+
+
+
+#copied to 
+def getGenSumDic( inputCsv ):
+    df = pd.read_csv( inputCsv )
+    # genSumDic = df.to_dict()
+    genSumDic = pd.Series( df['2016postVFP'].values, index=df['process'] ).to_dict()
+    print( genSumDic )
+    return genSumDic
+
+def writeHistsOneFileOneProcess( indir, genSumDic, samplesCrossSection, lumi, outDir, isData=False ):
+    uf.checkMakeDir( outDir )
+    for iPro in os.listdir( indir ):
+        print( iPro )
+        iRootFile = ROOT.TFile( outDir+iPro+'.root', "RECREATE")
+        # iRootFile = ROOT.TFile( outDir+iPro+'_variableHists.root', "RECREATE")
+        iHist = ROOT.TH1D( 'OBinitial_' + iPro + '_' + 'forEventCount', 'initial_' + iPro + '_' + 'onlygenWeight', 2, -1, 1 )
+        iHistHLT = ROOT.TH1D( 'OBHLT_' + iPro + '_' + 'forEventCount', 'HLT_' + iPro + '_' + 'onlygenWeight', 2, -1, 1 )
+        iHistPre = ROOT.TH1D( 'OBpreSelection_' + iPro + '_' + 'forEventCount', 'preSelection_' + iPro + '_' + 'onlygenWeight', 2, -1, 1 )
+        iHist.SetDirectory(iRootFile)
+        for ifile in os.listdir( indir+iPro ):
+            if 'log' in ifile: continue
+            # print( ifile )
+            iRoot = ROOT.TFile( indir+iPro+ '/'+ ifile, 'READ' )
+            ihist_initial = iRoot.Get( 'h_initial' )
+            ihist_HLT = iRoot.Get( 'h_afterHLT' )
+            ihist_pre = iRoot.Get( 'h_afterpreSelection')
+            iHist.Add( ihist_initial )
+            iHistHLT.Add( ihist_HLT )
+            iHistPre.Add( ihist_pre)
+            # iHist.SetDirectory(iRootFile)
+            iRoot.Close()
+
+        if isData: 
+            iProScale = 1.0
+        else: 
+            iProScale = lumi*samplesCrossSection[iPro]/genSumDic[iPro]
+        iHist.Scale( iProScale )
+        iHistHLT.Scale( iProScale )
+        iHistPre.Scale( iProScale )
+        iHist.Print()
+
+        iRootFile.Write()
+        print( 'written: ', iRootFile.GetName() )
+        iRootFile.Close()
+
+if __name__=='__main__':
+    main()
