1d39ca1c13fb8cae5e1e6ffcb38f595c02508f23
huilling110 <huiling2016@outlook.com>
2022-09-08 17:24:30 +0800

    submittd v0noBaseline_v29LorentzProblemSolvedNoJERnoTES for 2016preVFP

--- hua/makeVariables/makeJob_makeVaribles_forBDT.py
+++ hua/makeVariables/makeJob_makeVaribles_forBDT.py
@@ -46,8 +46,8 @@ def main():
 
     subAllofAll = open( 'subAllofAll.sh', 'w')
     for iera in inOutDirMap.keys():
-        if iera=='2016preVFP' : continue
-        # if iera=='2016postVFP' : continue#???
+        # if iera=='2016preVFP' : continue
+        if iera=='2016postVFP' : continue#???
         print(iera)
         if not os.path.exists( outputBase + iera +'/' ):
             os.mkdir( outputBase + iera +'/'  )
