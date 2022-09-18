c156eb1585793de2323b3bdec1f91915c5fb78c5
huilling110 <huiling2016@outlook.com>
2022-09-18 18:07:27 +0800

    added setupEnv.sh to be free of cmsenv

---
+++ setupEnv.sh
@@ -0,0 +1,8 @@
+
+export PATH=$PATH:/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/
+export PATH=/cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/gcc/9.3.0/bin/:$PATH
+export ROOTSYS=/cvmfs/cms.cern.ch/slc7_amd64_gcc900/lcg/root/6.22.08-8d9ab2b279c3f35e6100d909611c3c2f
+export PATH=$ROOTSYS/bin/:$PATH
+export LD_LIBRARY_PATH=$ROOTSYS/lib/:/cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/gcc/9.3.0/lib64/:/cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/gcc/9.3.0/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/cvmfs/cms.cern.ch/slc7_amd64_gcc900/cms/cmssw/CMSSW_12_2_4/external/slc7_amd64_gcc900/lib/:$LD_LIBRARY_PATH
+export PATH=/cvmfs/cms.cern.ch/slc7_amd64_gcc900/cms/cmssw/CMSSW_12_2_4/external/slc7_amd64_gcc900/bin/:$PATH
