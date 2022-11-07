98b995bf0532df329c67b601e85bb463e4e58f18
huilling110 <huiling2016@outlook.com>
2022-11-07 23:03:19 +0800

    submit v41addVertexSelection for 2016 pre too

--- objectSelection/makeJob_objectTSelectorForNanoAOD.py
+++ objectSelection/makeJob_objectTSelectorForNanoAOD.py
@@ -46,8 +46,8 @@ outputBase = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/'
 # jobVersionName = 'v40addTauJetEtau/'
 jobVersionName = 'v41addVertexSelection/'
 onlyMC = False
-era = '2016'
-# era = '2016APV'
+# era = '2016'
+era = '2016APV'
 # era = '2018'
 # era = '2017'
 def main():
