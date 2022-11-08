d0922df7b832e5681b2ab5b11fac7b33cadff4dc
huilling110 <huiling2016@outlook.com>
2022-11-08 17:27:54 +0800

    plotInputVariables works fine

--- hua/tmva/autoTraining_correlation/plotAllinDir.py
+++ hua/tmva/autoTraining_correlation/plotAllinDir.py
@@ -14,16 +14,12 @@ import plotAUC
 
 
 def main( ):
-    # TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v2Add2Variables_fromV9/1tau1l_v3/'
-    # TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v3correctBjetsvariable_fromV9/1tau1l_v1/'
-    # TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v3correctBjetsvariable_fromV9/1tau1l_v1/variableList_check/'
-    # TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v3correctBjetsvariable_fromV9/1tau1l_v2/'
     # TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v4modifiedMinDeltaR_fromV9/1tau1l_v1/'
     # TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v4modifiedMinDeltaR_fromV9/1tau1l_v3/'
     # TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v4modifiedMinDeltaR_fromV9/1tau1l_v4/'
     # TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v4modifiedMinDeltaR_fromV9/1tau2l_v1/'
     # TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v4modifiedMinDeltaR_fromV9/1tau2l_v2/'
-    TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016//v1fixedTauVariables_v40addTauJetEtau/1tau1l_v1/'
+    TMVAFileDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v3extra1tau1lCut_v41addVertexSelection/1tau1l_v0/'
     
     print( 'TMVAFileDir: ', TMVAFileDir )
     #  plotSigOnly = True
@@ -32,7 +28,6 @@ def main( ):
     channel = TMVAFileDir[-10:-4]
     print('channel: ', channel)
     # fileForVaribles = channel + 'varibleList_50.root'
-    # fileForVaribles = channel + 'varibleList_35.root'
     fileForVaribles = channel + 'varibleList_40.root'
     plotInputVariables( TMVAFileDir , fileForVaribles )
 
@@ -42,10 +37,26 @@ def main( ):
         plotSigOnlyC = 'false'
 
 
+
+    # variableNum_BDT, sig_BDT =  plotAll( TMVAFileDir )
+    # print( len(sig_BDT), sig_BDT )
+
+                    
+                        
+
+
+
+    # #plot sig
+    # logDir = TMVAFileDir + 'log/'
+    # plotAUC.plotAUC(  variableNum_BDT, sig_BDT, logDir, True, True )
+    # # plotAUC.plotAUC(  variableNum_BDTG, sig_BDTG, logDir, False, True )
+
+    # #plot AUC
+    # if not plotSigOnly:
+    #     plotAUC.getAUCToTGragh( logDir )
+        
+def plotAll( TMVAFileDir, plotSigOnlyC ):
     variableNum_BDT, sig_BDT = [], []
-    variableNum_BDTG, sig_BDTG = [], []
-
-
     for entry in os.listdir( TMVAFileDir ):
         #  print( entry )
         if os.path.isfile( TMVAFileDir+entry ):
@@ -72,45 +83,30 @@ def main( ):
                         BDTSig = float( line_Sig )
                         #  print( BDTSig )
                         sig_BDT.append( BDTSig )
-                    if 'BDTG_sigMax' in line:
-                        #  print( line )
-                        line_Sig = line[13:]
-                        BDTGSig = float( line_Sig )
-                        sig_BDTG.append( BDTGSig )
+                    # if 'BDTG_sigMax' in line:
+                    #     #  print( line )
+                    #     line_Sig = line[13:]
+                    #     BDTGSig = float( line_Sig )
                     if 'inputNum' in line and findNum%2==1:
                         findNum +=1
                         line = line[11:]
                         num = int(line)
                         variableNum_BDT.append( num )
-                        variableNum_BDTG.append( num )
-                    
-                        
+    
 
-    print( len(sig_BDT), sig_BDT )
-    print( len(sig_BDTG), sig_BDTG )
-
-
-    #plot sig
-    logDir = TMVAFileDir + 'log/'
-    plotAUC.plotAUC(  variableNum_BDT, sig_BDT, logDir, True, True )
-    # plotAUC.plotAUC(  variableNum_BDTG, sig_BDTG, logDir, False, True )
-
-    #plot AUC
-    if not plotSigOnly:
-        plotAUC.getAUCToTGragh( logDir )
-        
     
 
 def plotInputVariables( TMVAFileDir, fileForVariables ):
+    print( 'start to plot input variable  distribution')
     outForVariables = TMVAFileDir + 'results/variableDistribution/'
     if not os.path.exists( outForVariables ):
         os.mkdir( outForVariables )
-    # command = 'root -q -b \'../variables.C( \"{}\",  \"{}\", \"TMVA Input Variables\", kFALSE, kTRUE      )\'  '.format( TMVAFileDir+fileForVariables, outForVariables ) 
     codeDir = '/workfs2/cms/huahuil/4topCode/CMSSW_12_2_4/src/FourTop/hua/tmva/'
     command = 'root -q -b \'{}variables.C( \"{}\",  \"{}\", \"TMVA Input Variables\", kFALSE, kTRUE      )\'  '.format( codeDir, TMVAFileDir+fileForVariables, outForVariables ) 
     print( command )
     process = subprocess.run( command, shell=True)   
     print( process.stdout )
+    print( 'done input variable distribution')
     
     
 
