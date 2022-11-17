f52a1e4c4a3c78cd5ad65afcc1fd0839ad81d475
huilling110 <huiling2016@outlook.com>
2022-11-17 22:57:47 +0800

    use getEraFromDir from usefulFunc for plotCombineResult.py

--- hua/combine/plotCombineResults.py
+++ hua/combine/plotCombineResults.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import ROOT
+import usefulFunc as uf
 
 sys.path.insert(1, '/workfs2/cms/huahuil/4topCode/CMSSW_10_2_20_UL/src/FourTop/hua/tmva/autoTraining_correlation/')
 import plotAUC
@@ -10,16 +11,17 @@ import plotAUC
 def main(
     # resultDir = '/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v3addBtagHLTweights/1tau1l_v1/AppResults/datacard/sumDC/combineResults/'
     # resultDir = '/publicfs/cms/user/huahuil/TauOfTTTT/2016v1/TMVAOutput/v46_v3addBtagHLTweights/1tau1l_v1/AppResults_11bins/datacard/sumDC/combineResults/'
-    resultDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v3extra1tau1lCut_v41addVertexSelection/1tau1l_v0/AppResults_30bins/datacard/sumDC_10/combineResults/'
+    # resultDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2016/v3extra1tau1lCut_v41addVertexSelection/1tau1l_v0/AppResults_30bins/datacard/sumDC_10/combineResults/'
+    resultDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/TMVAoutput/2017/v3extra1tau1lCut_v41addVertexSelection/1tau1l_v0/AppResults_30bins/datacard/sumDC_10/combineResults/'
         ):
-    # variable_nums, mediumLimits = getMediumLimit( resultDir, False )
+    variable_nums, mediumLimits = getMediumLimit( resultDir, False )
     variable_numsSig, mediumSigs = getMediumLimit( resultDir, True )
 
+    era = uf.getEraFromDir(resultDir)
     outDir = resultDir + 'combineResults/'
-    if not os.path.exists(outDir):
-        os.mkdir( outDir )
-    # plotAUC.plot2D( variable_nums, mediumLimits, 'expectedLimit_withError' , outDir, '2016')
-    plotAUC.plot2D( variable_numsSig, mediumSigs, 'expectedSigs', outDir, '2016')
+    uf.checkMakeDir(outDir)
+    plotAUC.plot2D( variable_nums, mediumLimits, 'expectedLimit_withError' , outDir, era)
+    plotAUC.plot2D( variable_numsSig, mediumSigs, 'expectedSigs', outDir, era)
 
 
 
