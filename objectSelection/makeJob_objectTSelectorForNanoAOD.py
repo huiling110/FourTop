f28f854a978dd516b9cc67b1bfc8db049b8925fd
huilling110 <huiling2016@outlook.com>
2022-11-17 17:36:44 +0800

    submitted v41addVertexSelection for 2018

--- objectSelection/makeJob_objectTSelectorForNanoAOD.py
+++ objectSelection/makeJob_objectTSelectorForNanoAOD.py
@@ -47,8 +47,8 @@ jobVersionName = 'v41addVertexSelection/'
 onlyMC = False
 # era = '2016'
 # era = '2016APV'
-# era = '2018'
-era = '2017'
+era = '2018'
+# era = '2017'
 def main():
     # onlyMC = True
     # eventSelection = '3'
