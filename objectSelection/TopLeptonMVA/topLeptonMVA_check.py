a3607f1acc25418701dc2fc867e1a79ce58731f0
huilling110 <huiling2016@outlook.com>
2023-02-01 20:43:58 +0800

    can correctly save eventNumber

--- objectSelection/TopLeptonMVA/topLeptonMVA_check.py
+++ objectSelection/TopLeptonMVA/topLeptonMVA_check.py
@@ -1,11 +1,13 @@
-
+import os
 import ROOT
 from array import array
 # import uproot
 # import pandas as pd
+
 import sys
 sys.path.append("/workfs2/cms/huahuil/4topCode/CMSSW_10_2_20_UL/src/FourTop/hua/codeFromOthers/")
 import mvaTOPreader as tl
+import usefulFunc as uf
 
 def main():
     #xrootd for accessing data
@@ -13,26 +15,31 @@ def main():
     # nanoFile = '/store/mc/RunIISummer20UL18NanoAODv9/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/120000/B8F4ECF8-1363-C14F-B6F4-D292106546A0.root'
     # inputFile = ROOT.TFile.Open("root://rootd-cms.infn.it//" + nanoFile )#???not working
     inputFile = ROOT.TFile("/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/nanoAOD/B8F4ECF8-1363-C14F-B6F4-D292106546A0.root", "READ") #UL2018
-    # inputFile = ROOT.TFile("/scratchfs/cms/huahuil/tauOfTTTT_NanoAOD/nanoAOD/2018/mc/tttt/outTree_1.root", "READ")
     
     tree = inputFile.Get("Events") 
     # tree.Print()
     # tree.SetBranchStatus("*", 0)
     # tree.SetBranchStatus("Electron_pt", 1)#???
-    
-    # Electron_pt = array('f', [0.])
-    # tree.SetBranchAddress('Electron_mvaFall17V2noIso',Electron_mvaFall17V2noIso )
+        
+    outDir = os.path.dirname(os.path.realpath(__file__)) + '/output/'
+    uf.checkMakeDir(outDir) 
+    outFile = ROOT.TFile( outDir + 'syncWithSS.root', "RECREATE")
+    outTree = ROOT.TTree('events', 'event tree for sync')
+    event = array('L', [0])
+    # event  = 0
+    outTree.Branch('eventNumber', event, 'event/l')
     
     # for i in range(tree.GetEntries()):
     for i in range( 20):
         tree.GetEntry(i)
-        pt = tree.Electron_pt #Electron_pt#???
-            
+        event[0] = tree.event
+        print('eventNumber: ', tree.event)
+                
         for ele in range(tree.nElectron):
             lep = {} 
             lep['pdgId'] = 11
             lep["pt"] = tree.Electron_pt[ele]
-            lep["eta"] = tree.Electron_eta[ele]
+            lep["eta"] = tree.Electron_eta[ele] #absolute or not?
             lep["jetNDauCharged"] = tree.Electron_jetNDauCharged[ele]
             lep["miniPFRelIso_chg"] = tree.Electron_miniPFRelIso_chg[ele]
             lep["miniPFRelIso_all"] = tree.Electron_miniPFRelIso_all[ele]
@@ -44,17 +51,19 @@ def main():
             lep["dxy"] = tree.Electron_dxy[ele]
             lep["dz"] = tree.Electron_dz[ele]
             lep["mvaFall17V2noIso"] = tree.Electron_mvaFall17V2noIso[ele]
-            print(lep)
+            # print(lep)
         
             top = tl.mvaTOPreader('UL2018')
             print(top.getmvaTOPScore(lep))
         
+        outTree.Fill() 
+        
     inputFile.Close()   
     
-    # with uprvoot.open('/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/nanoAOD/B8F4ECF8-1363-C14F-B6F4-D292106546A0.root') as f:
-    #     tree = f["Events"]
-    #     events = tree.arrays(["Electron_pt"],library="pd") 
-    #     print(events)
+    outFile.Write()
+    print('outFile here: ', outFile.GetName())
+    outFile.Close()
+    
 
         
 
