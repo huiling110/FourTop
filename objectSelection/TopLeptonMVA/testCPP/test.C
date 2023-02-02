82878c578c3837d0757617a3e7ba5aaf99fc1075
huilling110 <huiling2016@outlook.com>
2023-02-03 01:08:30 +0800

    test.C is giving 0 results too

--- objectSelection/TopLeptonMVA/testCPP/test.C
+++ objectSelection/TopLeptonMVA/testCPP/test.C
@@ -1,9 +1,12 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <map>
+#include <iostream>
+#include <cmath>
 
-#include <xgboost/c_api.h>
-#include <xgboost/data.h>
+#include </cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/xgboost/1.3.3/include/xgboost/c_api.h>
+// #include </cvmfs/cms.cern.ch/slc7_amd64_gcc900/external/xgboost/1.3.3/include/xgboost/data.h>
 
 // #include "../inputMap.h"
 
@@ -19,10 +22,11 @@ using namespace std;
         }                                                                                              \
     }
 
-double TopLeptonEvaluate(std::map<string, float> &inputFeatures, string era, bool isMuon)
+double TopLeptonEvaluate(std::map<std::string, float> &inputFeatures, bool isMuon)
 {
+    BoosterHandle booster;
     const char *muonWeight = "/workfs2/cms/huahuil/4topCode/CMSSW_10_2_20_UL/src/FourTop/objectSelection/TopLeptonMVA/mvaWeights_new/el_TOPUL18_XGB.weights.bin";
-    std::cout << "leptonWeight: " << muonWeight << "\n";
+    // std::cout << "leptonWeight: " << muonWeight << "\n";
 
     //
 
@@ -39,8 +43,8 @@ double TopLeptonEvaluate(std::map<string, float> &inputFeatures, string era, boo
     boosterVars[0][7] = inputFeatures["pfRelIso03_all"];
     boosterVars[0][8] = inputFeatures["jetBTag"];
     boosterVars[0][9] = inputFeatures["sip3d"];
-    boosterVars[0][10] = TMath::Log(TMath::Abs(inputFeatures["dxy"]));
-    boosterVars[0][11] = TMath::Log(TMath::Abs(inputFeatures["dz"]));
+    boosterVars[0][10] = std::log(std::abs(inputFeatures["dxy"]));
+    boosterVars[0][11] = std::log(std::abs(inputFeatures["dz"]));
     boosterVars[0][12] = inputFeatures["mvaFall17V2noIso"];
 
     for (int i = 0; i < 13; i++)
@@ -48,7 +52,6 @@ double TopLeptonEvaluate(std::map<string, float> &inputFeatures, string era, boo
         std::cout << "inputFeatures: " << boosterVars[0][i] << "\n";
     }
 
-    BoosterHandle booster;
     safe_xgboost(XGBoosterCreate(NULL, 0, &booster));
     safe_xgboost(XGBoosterLoadModel(booster, muonWeight));
 
@@ -61,11 +64,11 @@ double TopLeptonEvaluate(std::map<string, float> &inputFeatures, string era, boo
     XGBoosterPredict(booster, dtest, 0, 0, 0, &out_len, &f);
 
     XGDMatrixFree(dtest);
-    XGBoosterFree(booster[0]);
+    XGBoosterFree(booster);
     std::cout << "Top lepton score = " << f[0] << "\n";
-    Double_t score = f[0];
+    double score = f[0];
     // Double_t score = (exp(f[0] * 2 * log(M_E)) - 1) / (exp(2 * log(M_E)) - 1) * 2 - 1;
-    // std::cout << "Top lepton score = " << score << "\n";
+    std::cout << "Top lepton score = " << score << "\n";
     delete f;
 
     return score;
@@ -73,20 +76,7 @@ double TopLeptonEvaluate(std::map<string, float> &inputFeatures, string era, boo
 
 int main()
 {
-    std::map<TString, Float_t> inputFeatures = {
-        // {"pt", Electron_pt[j]},
-        // {"eta", Electron_eta[j]},
-        // {"etNDauCharged", Electron_jetNDauCharged.At(j)},
-        // {"miniPFRelIso_chg", Electron_miniPFRelIso_chg[j]},
-        // {"miniPFRelIso_all", Electron_miniPFRelIso_all[j]},
-        // {"jetPtRelv2", Electron_jetPtRelv2[j]},
-        // {"jetPtRatio", jetPtRatio},
-        // {"pfRelIso03_all", Electron_pfRelIso03_all[j]},
-        // {"jetBTag", jetBTag},
-        // {"sip3d", Electron_sip3d[j]},
-        // {"dxy", Electron_dxy[j]},
-        // {"dz", Electron_dz[j]},
-        // {"mvaFall17V2noIso", Electron_mvaFall17V2noIso[j]}};
+    std::map<std::string, float> inputFeatures = {
         {"pt", 50},
         {"eta", 1.5},
         {"jetNDauCharged", 5},
@@ -101,5 +91,5 @@ int main()
         {"dz", 0.005},
         {"mvaFall17V2noIso", 0.8}};
 
-    topMVAScore = TopLeptonEvaluate(inputFeatures, '2018', false);
+    TopLeptonEvaluate(inputFeatures, false);
 }
