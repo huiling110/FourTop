7a1f4b3455dc0445e8aa8ec7124b4b0a081784f2
huilling110 <huiling2016@outlook.com>
2023-02-21 06:18:49 +0800

    P: PUWeight is wieldly 0

--- objectSelection/run_objectTSelectorForNanoAOD.C
+++ objectSelection/run_objectTSelectorForNanoAOD.C
@@ -24,8 +24,8 @@ void run_objectTSelectorForNanoAOD(
     // TString inputDir = "/publicfs/cms/data/TopQuark/nanoAOD/2018/data/jetHT_2018a/",
     // TString outputDir = "/publicfs/cms/user/fabioiemmi/TauOfTTTT/test_tobjectSelector/",
     TString outputDir = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/UL2016_postVFP/crossCheck_noOverlap/tttt/",
-    TString singleFileName = "outTree_11.root",
-    // TString singleFileName = "outTree_1.root",
+    // TString singleFileName = "outTree_11.root",
+    TString singleFileName = "outTree_1.root",
     // 1 for MetFilters, 2 for HLTSelection, 4 for preSelection. so 7 if all selection; 0 if no selection
     // TString eventSelectionBit = "3"
     TString eventSelectionBit = "7")
@@ -82,8 +82,8 @@ void run_objectTSelectorForNanoAOD(
     {
         outputDir = "output/";
         // eventNum = 100;
-        // eventNum = 1000;
-        eventNum = 10000;
+        eventNum = 1000;
+        // eventNum = 10000;
     }
     TString outputFile;
     outputFile = outputDir + singleFileName;
