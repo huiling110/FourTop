9ce848b8ffd8cca53a127f5600ab1f5a4ddfa381
huilling110 <huiling2016@outlook.com>
2023-02-21 19:18:58 +0800

    submit v1AddTOPLepVariables_v49FixedPileUpNoJERTES

--- hua/makeVariables/run_makeVaribles_forBDT.C
+++ hua/makeVariables/run_makeVaribles_forBDT.C
@@ -23,7 +23,8 @@ void run_makeVaribles_forBDT(
     // TString inputBase = "/scratchfs/cms/huahuil/tauOfTTTT_NanoAOD/UL2016_postVFP/v40addTauJetEtau/mc/",
     // TString inputBase = "/scratchfs/cms/huahuil/tauOfTTTT_NanoAOD/UL2017/v42fixedChargeType/mc/",
     // TString inputBase = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/UL2016_preVFP/v45newLepBugFixed/mc/",
-    TString inputBase = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/UL2016_preVFP/v46addPOGIDL/mc/",
+    // TString inputBase = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/UL2016_preVFP/v46addPOGIDL/mc/",
+    TString inputBase = "/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/UL2016_postVFP/v49FixedPileUpNoJERTES/mc/",
 
     TString inputDir = "tttt",
     // TString inputDir = "qcd_100to200",
