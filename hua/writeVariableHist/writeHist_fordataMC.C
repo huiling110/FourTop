5541b7de9d44eb82ddca3f3d886c28fdf7db9050
huilling110 <huiling2016@outlook.com>
2023-02-21 19:47:29 +0800

    changed CR3 definition

--- hua/writeVariableHist/writeHist_fordataMC.C
+++ hua/writeVariableHist/writeHist_fordataMC.C
@@ -279,7 +279,8 @@ Bool_t writeHist_fordataMC::Process(Long64_t entry)
     Bool_t is1tau1lCR0 = *tausT_number == 1 && lepNum == 1 && *jets_number >= 7 && *bjetsM_num == 1;
     Bool_t is1tau1lCR1 = *tausT_number == 1 && lepNum == 1 && *jets_number >= 7 && *bjetsM_num == 0;
     Bool_t is1tau1lCR2 = *tausT_number == 1 && lepNum == 1 && *jets_number == 6 && *bjetsM_num >= 2;
-    Bool_t is1tau1lCR3 = *tausT_number == 1 && lepNum == 1 && *jets_number == 6 && *bjetsM_num < 2;
+    // Bool_t is1tau1lCR3 = *tausT_number == 1 && lepNum == 1 && *jets_number == 6 && *bjetsM_num < 2;
+    Bool_t is1tau1lCR3 = *tausT_number == 1 &&lepNum == 1 &&*jets_number == 6 &&*bjetsM_num = 1;
     fillHistsVectorMyclass(is1tau1lCR3, 10, basicWeight);
     fillHistsVectorMyclass(is1tau1lCR0, 7, basicWeight);
     fillHistsVectorMyclass(is1tau1lCR1, 8, basicWeight);
