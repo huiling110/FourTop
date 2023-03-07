02c6623d50c142368135773c85cef551eb1d64de
huilling110 <huiling2016@outlook.com>
2023-03-08 00:07:49 +0800

    can save plots now

--- hua/plotting/plotSignalShape.py
+++ hua/plotting/plotSignalShape.py
@@ -1,11 +1,14 @@
 import ROOT
-
+import usefulFunc as uf
 
 def main():
     input = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2016postVFP/v0noCut_v52noHLTSelection/mc/variableHists_test/tttt.root' 
     region = 'noSelection'
     
-    
+    inputDir = input[ 0: input.rfind('/')+1 ]
+    print(inputDir)
+    outDir = inputDir + 'results/'
+    uf.checkMakeDir(outDir)
     
     file = ROOT.TFile(input, 'READ') 
     
@@ -13,7 +16,21 @@ def main():
         # Get the histogram object
         hist = key.ReadObj()
         histName = hist.GetName()
+        if not region in histName: continue
         print('histName: ', histName) 
+        plotShape( hist, outDir  )
+        
+def plotShape( hist, outDir):
+    # outFile = TFile
+   canvas = ROOT.TCanvas("canvas", "My Canvas", 800, 600) 
+   
+   hist.Draw()
+   
+   
+   outName = outDir+ hist.GetName()+'.png'
+   canvas.SaveAs(outName)
+   print('plot saved here: ', outName)
+    
      
     
 if __name__=='__main__':
