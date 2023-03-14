23cb3d5d6d53ae69035866b2ccc1c919eb0eb73f
huilling110 <huiling2016@outlook.com>
2023-03-14 18:11:36 +0800

    plotEffHLT can plot for tttt too

--- hua/plotting/plotHLTEfficiency.py
+++ hua/plotting/plotHLTEfficiency.py
@@ -9,16 +9,16 @@ def main():
     variableList = ['jets_HT', 'bjetsM_num', 'jets_6pt']
     regionList = ['baseline1Muon', 'baseline1MuonAndHLT', 'baseline', 'baselineAndHLT']
     
-    # variableDic = {
-    #     'jets_HT': np.array( [500., 550, 600, 650, 750, 850, 950, 1050, 1250, 1450, 1650, 1950, 2250, 2500] ),
-    # }
+    variableDic = {
+        'jets_HT': np.array( [500., 550, 600, 650, 750, 850, 950, 1050, 1250, 1450, 1650, 1950, 2250, 2500] ),
+    }
     # variableDic = {
     #     'bjetsM_num': np.array([-0.5, 0.5, 1.5, 2.5, 3.5, 5.5, 7.5]),
     # }
     
-    variableDic = {
-        'jets_6pt': np.array([25.0, 40, 55, 70, 85, 115, 145])
-    } 
+    # variableDic = {
+    #     'jets_6pt': np.array([25.0, 40, 55, 70, 85, 115, 145])
+    # } 
     
     
     
@@ -35,14 +35,9 @@ def main():
     
     # plotEffHLT(variableDic, 'baseline', 'baselineAndHLT', sumProcessPerVar, 'MCTruthEff', plotDir)
     # plotEffHLT(variableDic, 'baseline1Muon', 'baseline1MuonAndHLT', sumProcessPerVar,  'MCRefEff', plotDir)
-    plotEffHLT(variableDic, 'baseline1Muon', 'baseline1MuonAndHLT', sumProcessPerVar, 'dataRefEff', plotDir, ifData=True)
+    # plotEffHLT(variableDic, 'baseline1Muon', 'baseline1MuonAndHLT', sumProcessPerVar, 'dataRefEff', plotDir, ifData=1)
+    plotEffHLT(variableDic, 'baseline', 'baselineAndHLT', sumProcessPerVar, 'ttttHLTEff', plotDir, ifData=2)
     
-    #MC reference efficiency
-    # ptBins = np.array( [500., 550, 600, 650, 750, 850, 950, 1050, 1250, 1450, 1650, 1950, 2250, 2500] )
-    # variableDic = {
-    #     'bjetsM_num': [],
-    # }
-   
    
    
     
@@ -50,15 +45,21 @@ def main():
     #overlay of MC truth efficiency, MC reference efficiency and data reference efficiency
 
 
-def plotEffHLT(variableDic,  regionDe, regionNu, sumProcessPerVar, plotName, plotDir, ifData=False): 
-    if not ifData:
+def plotEffHLT(variableDic,  regionDe, regionNu, sumProcessPerVar, plotName, plotDir, ifData=0): 
+    if  ifData==0:
         MCTrueth_de = uf.addBGHist(sumProcessPerVar[list(variableDic.keys())[0]], list(variableDic.keys())[0], regionDe, includeQCD=True)
         MCTrueth_nu = uf.addBGHist(sumProcessPerVar[list(variableDic.keys())[0]], list(variableDic.keys())[0], regionNu, includeQCD=True)
-    else:
+    elif ifData==1:
         MCTrueth_de = sumProcessPerVar[list(variableDic.keys())[0]][regionDe]['singleMu'].Clone()
         MCTrueth_nu = sumProcessPerVar[list(variableDic.keys())[0]][regionNu]['singleMu'].Clone()
         MCTrueth_de.SetName(regionDe)
         MCTrueth_nu.SetName(regionNu)
+    elif ifData==2:
+        MCTrueth_de = sumProcessPerVar[list(variableDic.keys())[0]][regionDe]['tttt'].Clone()
+        MCTrueth_nu = sumProcessPerVar[list(variableDic.keys())[0]][regionNu]['tttt'].Clone()
+        MCTrueth_de.SetName(regionDe)
+        MCTrueth_nu.SetName(regionNu)
+        
     if len( variableDic[list(variableDic.keys())[0]] ) >0:
         binLowEges = variableDic[list(variableDic.keys())[0]]
         MCTrueth_de = MCTrueth_de.Rebin(len(binLowEges)-1, '', binLowEges)
