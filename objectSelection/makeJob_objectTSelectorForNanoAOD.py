77922c4bc5e4887c9efe3e7edc60dc1464e76a57
huilling110 <huiling2016@outlook.com>
2023-03-24 03:50:19 +0800

    submit v52noHLTButPreSelection for 2018

--- objectSelection/makeJob_objectTSelectorForNanoAOD.py
+++ objectSelection/makeJob_objectTSelectorForNanoAOD.py
@@ -48,8 +48,8 @@ jobVersionName = 'v52noHLTButPreSelection/'
 onlyMC = False
 # era = '2016'
 # era = '2016APV'
-era = '2017'
-# era = '2018'
+# era = '2017'
+era = '2018'
 def main():
     # onlyMC = True
     # eventSelection = '3'
