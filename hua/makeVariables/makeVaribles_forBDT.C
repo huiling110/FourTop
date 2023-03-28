6224706e4c366d4010402efce71c984e340a313b
huilling110 <huiling2016@outlook.com>
2023-03-28 17:30:39 +0800

    MV: fixed selection bit conversion

--- hua/makeVariables/makeVaribles_forBDT.C
+++ hua/makeVariables/makeVaribles_forBDT.C
@@ -72,40 +72,40 @@ void makeVaribles_forBDT::SlaveBegin(TTree * /*tree*/)
 
     // initialize selection level
     //???should make this an function and then a library to be easily used by other codes
-    switch (selectionBitInt)
+    if (selectionBitInt == 1)
     {
-    case 0:
-        m_MetFilters = 0;
-        m_HLTSelection = 0;
-        m_baselineSelection = 0;
-        break;
-    case 1:
         m_MetFilters = 1;
-        break;
-    case 2:
-        m_HLTSelection = 1;
-        break;
-    case 3:
-        m_MetFilters = 1;
-        m_HLTSelection = 1;
-        break;
-    case 4:
-        m_baselineSelection = 1;
-        break;
-    case 5:
-        m_MetFilters = 1;
-        m_baselineSelection = 1;
-        break;
-    case 6:
-        m_HLTSelection = 1;
-        m_baselineSelection = 1;
-    case 7:
-        m_MetFilters = 1;
-        m_HLTSelection = 1;
-        m_baselineSelection = 1;
-    default:
-        break;
     }
+    else if (selectionBitInt == 2)
+    {
+        m_HLTSelection = 1;
+    }
+    else if (selectionBitInt == 3)
+    {
+        m_MetFilters = 1;
+        m_HLTSelection = 1;
+    }
+    else if (selectionBitInt == 4)
+    {
+        m_baselineSelection = 1;
+    }
+    else if (selectionBitInt == 5)
+    {
+        m_MetFilters = 1;
+        m_baselineSelection = 1;
+    }
+    else if (selectionBitInt == 6)
+    {
+        m_HLTSelection = 1;
+        m_baselineSelection = 1;
+    }
+    else if (selectionBitInt == 7)
+    {
+        m_MetFilters = 1;
+        m_HLTSelection = 1;
+        m_baselineSelection = 1;
+    }
+
     std::cout << "m_MetFilters:m_HLTSelection:m_baselineSelection: " << m_MetFilters << m_HLTSelection << m_baselineSelection << "\n";
 
     outputfile = new TFile(outDir + m_processName + ".root", "RECREATE");
