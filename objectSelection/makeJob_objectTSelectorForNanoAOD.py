8e48f78a4e4651a8c69687f363d734be17ce93a0
huilling110 <huiling2016@outlook.com>
2023-03-28 15:13:14 +0800

    submit v54noHLTButPre for 2017

--- objectSelection/makeJob_objectTSelectorForNanoAOD.py
+++ objectSelection/makeJob_objectTSelectorForNanoAOD.py
@@ -45,19 +45,18 @@ outputBase = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/'
 # jobVersionName = 'v52noHLTSelection/'
 # jobVersionName = 'v52noHLTButPreSelection/'
 # jobVersionName = 'v53HLTAndPre/'
-# jobVersionName = 'v54noHLTButPre/'
-jobVersionName = 'v55HLTAndPre/'
+jobVersionName = 'v54noHLTButPre/'
+# jobVersionName = 'v55HLTAndPre/'
 
 onlyMC = False
 # era = '2016'
 # era = '2016APV'
-# era = '2017'
-era = '2018'
+era = '2017'
+# era = '2018'
 def main():
     # onlyMC = True
-    # eventSelection = '3'
-    eventSelection = '7'
-    # eventSelection = '5'
+    # eventSelection = '7'
+    eventSelection = '5'
     # eventSelection = '1'
    # 1 for MetFilters, 2 for HLTSelection, 4 for preSelection. so 7 if all selection; 0 if no selection 
     isHuiling = True
