b239b45fb2a393e73cb76c0d9cccdd2e1a303334
huilling110 <huiling2016@outlook.com>
2023-03-29 21:05:56 +0800

    added getHistFromFile() to usefulFuction.h

--- hua/src_cpp/usefulFuction.h
+++ hua/src_cpp/usefulFuction.h
@@ -40,4 +40,17 @@ Double_t getGenSum(TString inputFile)
     return genWeightSumInitial;
 }
 
+TH2D *getHistFromFile(TString fileName, TString histName)
+{
+    file = new TFile(fileName, "READ");
+    // if (!file || file->IsZombie())
+    // {
+    //     return
+    // }
+    TH2D *hist = (TH2D *)file->Get(histName)->Clone();
+    file.Close();
+    delete file;
+    return hist;
+}
+
 #endif
--- hua/writeVariableHist/commenFuncForWriteHist.h
+++ hua/writeVariableHist/commenFuncForWriteHist.h
@@ -1,13 +1,14 @@
 
-
 #include "TH1D.h"
 
 TH2D *getHistFromFile(TString fileName, TString histName)
 {
     file = new TFile(fileName, "READ");
     // if (file.IsZombie()){
-
-    // }
+    if (!file || file->IsZombie())
+    {
+        return
+    }
     TH2D *hist = (TH2D *)file->Get(histName)->Clone();
     file.Close();
     delete file;
