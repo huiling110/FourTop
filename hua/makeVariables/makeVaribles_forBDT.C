d3b0b21f9cd82ddff7c0aa1d5a8b5751d24c44d2
huilling110 <huiling2016@outlook.com>
2023-04-12 15:24:33 +0800

    JOB: v5extra1tau1lCut_v56NoHLTButPre for 2017

--- hua/makeVariables/makeVaribles_forBDT.C
+++ hua/makeVariables/makeVaribles_forBDT.C
@@ -675,8 +675,8 @@ Bool_t makeVaribles_forBDT::Process(Long64_t entry)
     }
 
     // !!!extra cut for faster BDT training
-    // if (!(tausT_number == 1 && leptonsMVAT_number == 1 && jets_number >= 7 && bjetsM_num >= 2)) // for 1tau1l training
-    if (!(tausT_number == 1 && leptonsMVAT_number == 1))
+    if (!(tausT_number == 1 && leptonsMVAT_number == 1 && jets_number >= 7 && bjetsM_num >= 2)) // for 1tau1l training
+    // if (!(tausT_number == 1 && leptonsMVAT_number == 1))
     {
         return kFALSE;
     }
