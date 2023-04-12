2a9e043a2373b70b185654d6a1f3b9b4449e6e05
huilling110 <huiling2016@outlook.com>
2023-04-12 20:58:43 +0800

    JOB: v6baseline_v56NoHLTButPre for 2017

--- hua/makeVariables/makeJob_makeVaribles_forBDT.py
+++ hua/makeVariables/makeJob_makeVaribles_forBDT.py
@@ -26,8 +26,8 @@ import usefulFunc as uf
 # inVersion = 'v54noHLTButPreMetFixed'
 # inVersion = 'v55ojectRemovalwithTight'
 # inVersion = 'v55ojectRemovalwithTightNoHLT'
-# inVersion = 'v56NoHLTButPre'
-inVersion = 'v56preselection'
+inVersion = 'v56NoHLTButPre'
+# inVersion = 'v56preselection'
 
 # outVersion = 'v0noBaseline'
 # outVersion = 'v0baseline'
