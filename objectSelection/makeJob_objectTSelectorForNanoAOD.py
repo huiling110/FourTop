85c5e6492732054a133b6140a88527bf6a3f2a80
huilling110 <huiling2016@outlook.com>
2023-04-17 21:09:42 +0800

    JOB: v57ovelapWithTausF for 2016

--- objectSelection/makeJob_objectTSelectorForNanoAOD.py
+++ objectSelection/makeJob_objectTSelectorForNanoAOD.py
@@ -34,9 +34,9 @@ outputBase = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/'
 jobVersionName = 'v57ovelapWithTausF/'
 
 onlyMC = False
-# era = '2016'
+era = '2016'
 # era = '2016APV'
-era = '2017'
+# era = '2017'
 # era = '2018'
 def main():
     # onlyMC = True
