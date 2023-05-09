d8294a115bef1a8aa2327cf9d00a335fc3598e90
huilling110 <huiling2016@outlook.com>
2023-05-09 16:33:42 +0800

    treeAnalyzer runs good too

--- hua/writeVariableHist/myTreeAnalyzer/treeAnalyzer.C
+++ hua/writeVariableHist/myTreeAnalyzer/treeAnalyzer.C
@@ -7,6 +7,16 @@ void treeAnalyzer::Init()
     std::cout << "start to initilation\n";
 }
 
+void treeAnalyzer::LoopTree()
+{
+    Long64_t allEvent = m_tree->GetEntries();
+    if (m_isTest)
+    {
+        allEvent = 1000;
+    }
+    std::cout << "looping over trees of " << allEvent << "\n";
+}
+
 void treeAnalyzer::Terminate()
 {
     delete m_tree;
--- hua/writeVariableHist/myTreeAnalyzer/treeAnalyzer.h
+++ hua/writeVariableHist/myTreeAnalyzer/treeAnalyzer.h
@@ -9,7 +9,7 @@
 class treeAnalyzer
 {
 public:
-    treeAnalyzer(TString inputFile, TString treeName)
+    treeAnalyzer(TString inputFile, TString treeName, Bool_t isTest = kTRUE) : m_isTest{isTest}
     {
         m_file = new TFile(inputFile, "READ");
         m_tree = (TTree *)m_file->Get(treeName);
@@ -26,6 +26,7 @@ private:
     TFile *m_file;
     TString m_processName;
     TString m_era;
+    Bool_t m_isTest = kTRUE;
 };
 
 #endif
