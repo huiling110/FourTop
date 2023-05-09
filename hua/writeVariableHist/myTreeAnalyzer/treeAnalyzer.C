e4a5e070e704366ace250305e667f497fb627c9c
huilling110 <huiling2016@outlook.com>
2023-05-09 18:59:54 +0800

    TA: good

--- hua/writeVariableHist/myTreeAnalyzer/treeAnalyzer.C
+++ hua/writeVariableHist/myTreeAnalyzer/treeAnalyzer.C
@@ -5,6 +5,35 @@
 void treeAnalyzer::Init()
 {
     std::cout << "start to initilation\n";
+    // m_outFile = new TFile(m_outputFolder + "variableHists" + "_" + m_version + "/" + m_processName + ".root", "RECREATE");
+    readBranch();
+    std::cout << "done initializing\n";
+    std::cout << "\n";
+}
+
+void treeAnalyzer::readBranch()
+{
+    m_tree->SetBranchAddress("tausT_number", &tausT_number);
+    m_tree->SetBranchAddress("elesTopMVAT_number", &elesTopMVAT_number);
+    m_tree->SetBranchAddress("muonsTopMVAT_number", &muonsTopMVAT_number);
+    m_tree->SetBranchAddress("jets_number", &jets_number);
+    m_tree->SetBranchAddress("bjetsM_num", &bjetsM_num);
+    m_tree->SetBranchAddress("leptonsMVAT_2OS", &leptonsMVAT_2OS);
+    m_tree->SetBranchAddress("jets_HT", &jets_HT);
+    m_tree->SetBranchAddress("jets_6pt", &jets_6pt);
+    m_tree->SetBranchAddress("EVENT_genWeight", &EVENT_genWeight);
+    m_tree->SetBranchAddress("EVENT_prefireWeight", &EVENT_prefireWeight);
+    m_tree->SetBranchAddress("EVENT_prefireWeight_up", &EVENT_prefireWeight_up);
+    m_tree->SetBranchAddress("EVENT_prefireWeight_down", &EVENT_prefireWeight_down);
+    m_tree->SetBranchAddress("PUweight_", &PUweight_);
+    m_tree->SetBranchAddress("PUweight_up_", &PUweight_up_);
+    m_tree->SetBranchAddress("PUweight_down_", &PUweight_down_);
+    m_tree->SetBranchAddress("HLT_weight", &HLT_weight);
+    m_tree->SetBranchAddress("tauT_IDSF_weight_new", &tauT_IDSF_weight_new);
+    m_tree->SetBranchAddress("elesTopMVAT_weight", &elesTopMVAT_weight);
+    m_tree->SetBranchAddress("musTopMVAT_weight", &musTopMVAT_weight);
+    m_tree->SetBranchAddress("btagShape_weight", &btagShape_weight);
+    m_tree->SetBranchAddress("btagShapeR", &btagShapeR);
 }
 
 void treeAnalyzer::LoopTree()
@@ -12,18 +41,27 @@ void treeAnalyzer::LoopTree()
     Long64_t allEvent = m_tree->GetEntries();
     if (m_isTest)
     {
-        allEvent = 1000;
+        allEvent = 100;
     }
     std::cout << "looping over trees of " << allEvent << "\n";
+
+    for (UInt_t e = 0; e < allEvent; e++)
+    {
+        m_tree->GetEntry(e);
+        std::cout << tausT_number << "\n";
+    }
 }
 
 void treeAnalyzer::Terminate()
 {
-    delete m_tree;
-    m_file->Close();
 }
 
 treeAnalyzer::~treeAnalyzer()
 {
     // code to free any dynamically allocated resources
+    m_file->Close();
+    m_outFile->Close();
+    // delete m_tree;
+    // delete m_file;
+    // delete m_outFile;
 }
--- hua/writeVariableHist/myTreeAnalyzer/treeAnalyzer.h
+++ hua/writeVariableHist/myTreeAnalyzer/treeAnalyzer.h
@@ -5,19 +5,24 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1.h"
+#include <iostream>
 
 class treeAnalyzer
 {
 public:
-    treeAnalyzer(TString inputFile, TString treeName, Bool_t isTest = kTRUE) : m_isTest{isTest}
+    treeAnalyzer(const TString inputFile, TString treeName, TString outputFolder = "./", Bool_t isData = kFALSE, TString era = "2016", TString outVersion = "v0", Bool_t isTest = kTRUE) : m_outputFolder{outputFolder}, m_isData{isData}, m_era{era}, m_isTest{isTest}
     {
         m_file = new TFile(inputFile, "READ");
         m_tree = (TTree *)m_file->Get(treeName);
+        m_processName = inputFile(inputFile.Last('/'), inputFile.Sizeof());
+        std::cout << "m_processName: " << m_processName << "\n";
+        m_outFile = new TFile(m_outputFolder + "variableHists" + "_" + outVersion + "/" + m_processName + ".root", "RECREATE");
     };
     ~treeAnalyzer();
 
     void Init();
     void LoopTree();
+    void readBranch();
     Bool_t eventCut();
     void Terminate();
 
@@ -27,6 +32,14 @@ private:
     TString m_processName;
     TString m_era;
     Bool_t m_isTest = kTRUE;
+    Bool_t m_isData = kFALSE;
+    TString m_outputFolder;
+    TFile *m_outFile;
+
+    // branch values;
+    Int_t tausT_number, leptonsMVAT_number, jets_number, bjetsM_num, leptonsMVAT_2OS, elesTopMVAT_number, muonsTopMVAT_number;
+    Double_t jets_HT, jets_6pt;
+    Double_t EVENT_genWeight, EVENT_prefireWeight, EVENT_prefireWeight_up, EVENT_prefireWeight_down, PUweight_, PUweight_up_, PUweight_down_, HLT_weight, tauT_IDSF_weight_new, elesTopMVAT_weight, musTopMVAT_weight, btagShape_weight, btagShapeR;
 };
 
 #endif
