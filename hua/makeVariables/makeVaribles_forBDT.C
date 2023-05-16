709ecf2ffb51c9d5bc66b93f41a63a9001cda03d
huilling110 <huiling2016@outlook.com>
2023-05-16 21:48:36 +0800

    MV: PS by reverse  ../src_cpp/usefulFuction.h and makeVaribles_forBDT.C to previous 8b681d89 commits

--- hua/makeVariables/makeVaribles_forBDT.C
+++ hua/makeVariables/makeVaribles_forBDT.C
@@ -32,7 +32,7 @@
 #include "makeVaribles_forBDT.h"
 #include "weightCal.h"
 #include "inputFileClass.h"
-#include "../src_cpp/usefulFuction.C"
+#include "../src_cpp/usefulFuction.h"
 
 void makeVaribles_forBDT::Begin(TTree * /*tree*/)
 {
@@ -743,11 +743,11 @@ Bool_t makeVaribles_forBDT::Process(Long64_t entry)
     }
 
     // !!!extra cut for faster BDT training
-    if (!(tausT_number == 1 && leptonsMVAT_number == 1 && jets_number >= 7 && bjetsM_num >= 2)) // for 1tau1l training
+    // if (!(tausT_number == 1 && leptonsMVAT_number == 1 && jets_number >= 7 && bjetsM_num >= 2)) // for 1tau1l training
     // if (!(tausT_number == 1 && leptonsMVAT_number == 1))
-    {
-        return kFALSE;
-    }
+    // {
+    //     return kFALSE;
+    // }
     fPassingPreselection++;
     eventCount_baseline->Fill(0.0, basicWeight);
     newtree->Fill();
--- hua/src_cpp/usefulFuction.h
+++ hua/src_cpp/usefulFuction.h
@@ -7,20 +7,75 @@
 #include "TString.h"
 #include "TTree.h"
 #include "TFile.h"
-#include "TH1D.h"
 #include <TTreeReader.h>
 #include <TTreeReaderValue.h>
 #include <TTreeReaderArray.h>
 
-void getOption(TString option, std::vector<TString> &optionVec);
+void getOption(TString option, std::vector<TString> &optionVec)
+{
+    optionVec.clear();
+    while (option.Contains(":"))
+    {
+        TString ioption = option(0, option.First(":"));
+        option.Remove(0, option.First(":") + 1);
+        optionVec.push_back(ioption);
+    }
+}
 
-Double_t getGenSum(TString inputFile);
+Double_t getGenSum(TString inputFile)
+{
+    Double_t genWeightSumInitial = 1.0;
+    TFile *m_file = new TFile(inputFile, "READ");
+    // if (!isData)
+    // {
+    TTree *Runs = (TTree *)m_file->Get("Runs");
+    Double_t igen;
+    Runs->SetBranchAddress("genEventSumw", &igen);
+    genWeightSumInitial = 0.0;
+    for (int iEntry = 0; Runs->LoadTree(iEntry) >= 0; ++iEntry)
+    {
+        Runs->GetEntry(iEntry);
+        genWeightSumInitial += igen;
+    }
+    // }
+    m_file->Close();
+    std::cout << "genWeightSumInitial: " << genWeightSumInitial << "\n";
+    return genWeightSumInitial;
+}
 
 // template <typename T>
 // T *getHistogramFromFile(TString filename, TString histname)
-TH1D *getHistogramFromFile(TString filename, const char *histname);
+TH1D *getHistogramFromFile(TString filename, const char *histname)
+{
+    TFile *file = TFile::Open(filename);
+    if (!file || file->IsZombie())
+    {
+        std::cerr << "Error: could not open file " << filename << std::endl;
+        return nullptr;
+    }
+    TH1D *hist = (TH1D *)(file->Get(histname));
+    if (!hist)
+    {
+        std::cerr << "Error: could not retrieve histogram " << histname << " from file " << filename << std::endl;
+        file->Close();
+        return nullptr;
+    }
+    hist->SetDirectory(nullptr); // detach histogram from file to prevent it from being deleted
+    file->Close();
+    return hist;
+}
 
-void histFill(Bool_t ifFill, Bool_t isData, Double_t weight, Double_t variableVal, TH1D *&hist);
+void histFill(Bool_t ifFill, Bool_t isData, Double_t weight, Double_t variableVal, TH1D *&hist)
+{
+    if (isData && ifFill)
+    {
+        hist->Fill(variableVal);
+    };
+    if (ifFill && !isData)
+    {
+        hist->Fill(variableVal, weight);
+    };
+}
 
 // void copy_TTreeReaderArray_toVector(const TTreeReaderArray<Double_t> &array, vector<Double_t> &vec)
 // {
