e2d8f57c35cd0e495a7620fe5e0a9389e1bcea20
huilling110 <huiling2016@outlook.com>
2023-05-30 17:19:52 +0800

    OS: goodLumiAndPVSel.C good

--- objectSelection/objectSelectionOptimized/goodLumiAndPVSel.h
+++ objectSelection/objectSelectionOptimized/goodLumiAndPVSel.h
@@ -13,57 +13,11 @@
 class LumiAndPVSel
 {
 public:
-    LumiAndPVSel(const Bool_t isData, const TString era)
-    {
-        std::cout << "initialize LumiAndPVSel class........\n";
-        readJSON(isData, GoldenJSONs[era], m_goodLumis);
-        std::cout << "done LumiAndPVSel initializing.........\n";
-    };
+    LumiAndPVSel(const Bool_t isData, const TString era);
     ~LumiAndPVSel(){};
 
     // Bool_t Select(const Bool_t isData, const eventForNano *e)//??? can not dereference a  const pointer?
-    Bool_t Select(const Bool_t isData, eventForNano *e)
-    {
-
-
-        // https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideGoodLumiSectionsJSONFile
-
-        Bool_t ifGoodLumi = kTRUE;
-        if (isData)
-        {
-            if (m_goodLumis.find(*e->run) == m_goodLumis.end())
-            {
-                ifGoodLumi = kFALSE;
-            }
-            else
-            { // if run number is in map
-                Bool_t keepEvent = false;
-                for (Int_t i = 0; i < m_goodLumis[*e->run].size() / 2.; i++)
-                {
-                    if (*e->luminosityBlock >= m_goodLumis[*e->run][i * 2] && *e->luminosityBlock <= m_goodLumis[*e->run][i * 2 + 1])
-                    {
-                        keepEvent = true;
-                        break;
-                    }
-                }
-                if (!keepEvent)
-                {
-                    // veto luminosity blocks not in JSON
-                    ifGoodLumi = kFALSE;
-                }
-            }
-        }
-
-        //
-        Bool_t ifPV = kFALSE;
-        if (*e->PV_npvsGood > 0)
-        {
-            ifPV = kTRUE;
-        }
-        return ifGoodLumi&& ifPV;
-
-    };
-    // void clearBranch();
+    Bool_t Select(const Bool_t isData, eventForNano *e);
 
 private:
     std::map<Int_t, std::vector<UInt_t>> m_goodLumis;
