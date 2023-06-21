e5a1e7babf2e97db19cc15f10a86b8517c93e3b6
huilling110 <huiling2016@outlook.com>
2023-06-21 21:49:30 +0800

    OS: pinpointed the line fo causing the memory exceeding problem but not sure how to fix yet

--- objectSelectionOptimized/src/copyBranch.C
+++ objectSelectionOptimized/src/copyBranch.C
@@ -4,7 +4,7 @@
 // CopyBranch(TTree *outTree) : m_outTree{outTree}
 CopyBranch::CopyBranch(TTree *outTree)
 {
-    std::cout<<"Initializing CopyBranch .........\n";
+    std::cout << "Initializing CopyBranch .........\n";
     outTree->Branch("run_", &run_);
     outTree->Branch("event_", &event_);
     outTree->Branch("PV_npvsGood_", &PV_npvsGood_);
@@ -17,8 +17,8 @@ CopyBranch::CopyBranch(TTree *outTree)
     outTree->Branch("GenPart_pdgId_", &GenPart_pdgId_);
     outTree->Branch("GenPart_genPartIdxMother_", &GenPart_genPartIdxMother_);
     outTree->Branch("EVENT_genWeight_", &EVENT_genWeight_);
-    std::cout<<"Done intializing ...........\n";
-    std::cout<<"\n";
+    std::cout << "Done intializing ...........\n";
+    std::cout << "\n";
 };
 
 void CopyBranch::Select(eventForNano *e, Bool_t isData)
@@ -32,19 +32,21 @@ void CopyBranch::Select(eventForNano *e, Bool_t isData)
     EVENT_prefireWeight_down_ = *e->L1PreFiringWeight_Dn;
     EVENT_genWeight_ = **e->genWeight;
     //!!!causing too much memory consumption!!!???
-    // copy_TTreeReaderArray_toVector(e->Electron_charge, Electron_charge_);
-    // copy_TTreeReaderArray_toVector(e->Muon_charge, Muon_charge_);
-    // if (!isData){
-    // copy_TTreeReaderArray_toVector(*e->GenPart_genPartIdxMother, GenPart_genPartIdxMother_ );
-    // copy_TTreeReaderArray_toVector(*e->GenPart_pdgId, GenPart_pdgId_ );
+    // maybe template argument deduction  is not working well, not the culprit
+    copy_TTreeReaderArray_toVector<Int_t>(e->Electron_charge, Electron_charge_); // this line is fine
+    copy_TTreeReaderArray_toVector<Int_t>(e->Muon_charge, Muon_charge_);         // this okay too
+    // if (!isData)
+    // {
+    // copy_TTreeReaderArray_toVector<Int_t>(*e->GenPart_genPartIdxMother, GenPart_genPartIdxMother_);
+    //!!!???it seem this is causing the memery exceeding problem
+    // copy_TTreeReaderArray_toVector<Int_t>(static_cast<TTreeReaderArray<Int_t>>(*e->GenPart_genPartIdxMother), GenPart_genPartIdxMother_); //???it seem this is causing the memery exceeding problem
+    // copy_TTreeReaderArray_toVector<Int_t>(*e->GenPart_pdgId, GenPart_pdgId_ );
     // }
-
 };
-void CopyBranch::clearBranch(){
+void CopyBranch::clearBranch()
+{
     Electron_charge_.clear();
     Muon_charge_.clear();
     GenPart_genPartIdxMother_.clear();
     GenPart_pdgId_.clear();
-
-
 };
