39f263311e0e03741b77aa64cf413a15e2150c98
huilling110 <huiling2016@outlook.com>
2023-06-23 22:00:07 +0800

    WH: going to add another constructor for event class

--- writeVariableHist/myTreeAnalyzer/include/writeHist_forDataMC.h
+++ writeVariableHist/myTreeAnalyzer/include/writeHist_forDataMC.h
@@ -1,5 +1,5 @@
-#ifndef TREEANALYZER_H
-#define TREEANALYZER_H
+#ifndef WH_FORDATAMC_H
+#define WH_FORDATAMC_H
 
 #include <iostream>
 #include <vector>
@@ -10,7 +10,6 @@
 #include "TTree.h"
 #include "TH1.h"
 #include "TH1D.h"
-#include "TMVA/Reader.h"
 
 #include "histsForRegionsMap_class.h"
 #include "functions.h"
@@ -18,10 +17,10 @@
 
 #include "myEventClass.h"
 
-class treeAnalyzer
+class WH_forDataMC
 {
 public:
-    treeAnalyzer(const TString inputDir, const TString process, TString outVersion = "v0", Int_t channel=0, Bool_t isTest = kTRUE) : m_inputDir{inputDir}, m_processName{process}, m_channel{channel}, m_isTest{isTest}
+    WH_forDataMC(const TString inputDir, const TString process, TString outVersion = "v0", Int_t channel=0, Bool_t isTest = kTRUE) : m_inputDir{inputDir}, m_processName{process}, m_channel{channel}, m_isTest{isTest}
     {
         m_file = new TFile(m_inputDir + m_processName + ".root", "READ"); //???what is this initialization
         if (!m_file || m_file->IsZombie())
@@ -43,12 +42,10 @@ public:
 
         e = new event(m_tree);
     };
-    ~treeAnalyzer();
+    ~WH_forDataMC();
 
     void Init();
     void LoopTree();
-    // void readBranch();
-    // Bool_t eventCut(event);
     void Terminate();
 
 private:
@@ -62,15 +59,8 @@ private:
     Bool_t m_isData = kFALSE;
     TString m_outputFolder;
     TFile *m_outFile;
-    TH1D *cutFlowHist = new TH1D("cutFlow", "cutFlow", 3, 0, 3);
+    // TH1D *cutFlowHist = new TH1D("cutFlow", "cutFlow", 3, 0, 3);
     event *e;
-
-    // for TMVA reader
-    TMVA::Reader *reader = new TMVA::Reader("!Color:!Silent");
-    std::vector<TString> variablesName{};
-    std::vector<Float_t> variablesForReader;
-    std::map<TString, Float_t> varForReaderMap;
-    std::vector<std::variant<Int_t, Double_t>> variablesOriginAll;
 
     // hists regions
     histsForRegionsMap SR1tau1lSys; // calls for default constructor
