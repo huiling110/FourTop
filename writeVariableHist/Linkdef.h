a04124dda5d848d4a7845731ff50967799fe52f4
huilling110 <huiling2016@outlook.com>
2023-06-23 19:15:24 +0800

    WH: moved writeVariableHist to root folder

---
+++ writeVariableHist/Linkdef.h
@@ -0,0 +1,19 @@
+#ifdef __CINT__
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+// includes all headrefiles
+// #include "TLorentzVector.h"
+#include <vector>
+#include "writeHist_BDT.h"
+
+// All classes
+// #pragma link C++ class vector<TLorentzVector>+;
+// #pragma link C++ class vector < ROOT::Math::PtEtaPhiMVector> + ;
+#pragma link C++ class writeHist_BDT+;
+// #pragma link C++ function utilityFunctions+;
+// #pragma link C++ class TauIDSFTool+; 
+
+
+#endif
