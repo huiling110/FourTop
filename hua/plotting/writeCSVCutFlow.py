7f7ba30d491b9903d9636ee1e9a543a64ef8ca7a
huilling110 <huiling2016@outlook.com>
2023-07-12 22:53:26 +0800

    PLT: can get df

--- hua/plotting/writeCSVCutFlow.py
+++ hua/plotting/writeCSVCutFlow.py
@@ -1,5 +1,8 @@
 import os
 import ROOT
+import pandas as pd
+
+import usefulFunc as uf
 
 def main():
     osDir = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/ReReco2022PreEE/v1newCrab/'
@@ -8,7 +11,50 @@ def main():
     proHistDic = {}  
     getCutFlowForDir(osDir+'mc/', proHistDic)
     getCutFlowForDir(osDir+'data/', proHistDic)
-    print(proHistDic)    
+    print(proHistDic)
+   
+    outDir = osDir+'mc/results/'
+    uf.checkMakeDir(outDir) 
+    writeProHistsToCSV(proHistDic, outDir)
+   
+def writeProHistsToCSV(proHistDic, outDir): 
+    df = getDataFrameFromHistDic(proHistDic)
+    print(df)
+    
+    writeDFtoCSV(df, outDir)
+    
+def  writeDFtoCSV(df, outDir):
+    dataList = ['JetMet2022C', 'JetMet2022D', 'JetHT2022C']
+    bgList = ['TTtoLNu2Q', 'TTto2L2Nu', 'TTto4Q']
+    df['data'] = df[dataList].sum(axis=1) 
+    df['bg'] = df[bgList].sum(axis=1)
+    print(df)
+   
+   
+# def addDF(colist,name, df):
+#     for ilist in colist:
+#         df.loc[name] =
+        
+     
+    
+    
+def getDataFrameFromHistDic(histDic):
+    data = {}
+    for ipro in histDic.keys():
+        icutflow = [histDic[ipro].GetBinContent(1), histDic[ipro].GetBinContent(2), histDic[ipro].GetBinContent(3), histDic[ipro].GetBinContent(4) ]
+        data[ipro] = icutflow
+    iListName = ['initial', 'Metfilter', 'HLT', 'preSelection']   
+    df = pd.DataFrame( data, index=iListName )
+    return df
+    
+    
+     
+    
+    
+    
+    
+    
+        
     
 def getCutFlowForDir(indir, proHistDic):
     # mcDir = osDir+'mc/'
