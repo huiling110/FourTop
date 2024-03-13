b7f2c3b78ef6f61e3677fb687dfc157e75231636
huilling110 <huiling2016@outlook.com>
2024-03-13 21:41:02 +0800

    JOB: v0NoBtagSel_v3NoHLTNoBtagButTauSel for 2022postEE

--- makeVariables_goodCode/jobs/makeJob_makeVaribles_forBDT.py
+++ makeVariables_goodCode/jobs/makeJob_makeVaribles_forBDT.py
@@ -55,14 +55,16 @@ def main():
     # outVersion = 'v2cut1tau1l'
     
     #2022
-    # year = '2022postEE'
-    year = '2022preEE'
+    year = '2022postEE'
+    # year = '2022preEE'
     # inVersion = 'v0preSel'
     # inVersion = 'v1for2022preSel'
     # inVersion = 'v2leptonsNameChange'
     # inVersion = 'v0for2022baseline'
-    inVersion = 'v3EnergyScaleDone'
-    outVersion = 'v0baseline'
+    # inVersion = 'v3EnergyScaleDone'
+    inVersion = 'v3NoHLTNoBtagButTauSel'
+    # outVersion = 'v0baseline'
+    outVersion = 'v0NoBtagSel'
    
     
     
