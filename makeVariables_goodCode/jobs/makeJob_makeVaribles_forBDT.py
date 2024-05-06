62173141edb157b4fbcdc97f630e69340c3d3a67
huilling110 <huiling2016@outlook.com>
2024-05-06 15:15:50 +0800

    JOB: v3baselineAddFRWeight_v75OverlapRemovalFTau

--- makeVariables_goodCode/jobs/makeJob_makeVaribles_forBDT.py
+++ makeVariables_goodCode/jobs/makeJob_makeVaribles_forBDT.py
@@ -47,7 +47,8 @@ def main():
     # outVersion = 'v0baselineHT450Cut1tau1l'
     # outVersion = 'v2baselineHT450Cut1tau0l'
     # outVersion = 'v0baselineHT450AddTauProng'
-    outVersion = 'v1cut1tau1lSR'
+    # outVersion = 'v1cut1tau1lSR'
+    outVersion = 'v3baselineAddFRWeight'
     
     #2022
     # year = '2022postEE'
