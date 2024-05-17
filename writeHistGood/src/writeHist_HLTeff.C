85ffd76b743b5b36b90d9d041eb051d8341cc5c1
huilling110 <huiling2016@outlook.com>
2024-05-17 16:29:27 +0800

    WH: trigger SF with different binning for different b

--- writeHistGood/src/writeHist_HLTeff.C
+++ writeHistGood/src/writeHist_HLTeff.C
@@ -119,22 +119,35 @@ void WH_HLTeff::Init()
     // Double_t xbins[] = {450, 500, 550, 700, 1000,  2500}; //BinM
     // Double_t ybins[] = {34,  38, 42 , 50., 60,  200}; //BinN                                
     // Double_t xbins[] = {450, 490, 530, 700, 1000,  2500}; //BinN
-    Double_t xbins[] = {450, 500, 550,  900,  2500}; //BinZ
-    Double_t ybins[] = {34,  40, 48,  200}; //BinZ                           
+    // Double_t xbins[] = {450, 500, 550,  900,  2500}; //BinZ
+    // Double_t ybins[] = {34,  40, 48,  200}; //BinZ                           
 
-    b1HT6pt_de = new TH2D(m_processName + "_baseline1Muon4b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
-    b1HT6pt_nu = new TH2D(m_processName + "_baseline1MuonAndHLT4b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
+    //different binning for different b jet multiplicity
+    Double_t xbins[] = {450, 490, 530, 700, 1000,  2500}; //2b
+    Double_t ybins[] = {34,  38, 42 , 50., 60,  200}; //2b                              
+    Double_t xbins3b[] = {450, 500, 550,  900,  2500}; //3b
+    Double_t ybins3b[] = {34,  39, 44, 60,  200}; //3b                              
+    Double_t xbins4b[] = {450, 500,  800,  2500}; //4b
+    Double_t ybins4b[] = {34,  40, 48,  200}; //4b                         
+
+
+    b1HT6pt_de = new TH2D(m_processName + "_baseline1Muon4b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins4b) / sizeof(Double_t) - 1, xbins4b, sizeof(ybins4b) / sizeof(Double_t) - 1, ybins4b);
+    b1HT6pt_nu = new TH2D(m_processName + "_baseline1MuonAndHLT4b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins4b) / sizeof(Double_t) - 1, xbins4b, sizeof(ybins4b) / sizeof(Double_t) - 1, ybins4b);
+
     b2HT6pt_de = new TH2D(m_processName + "_baseline1Muon2b" + "_jetsHTAnd6pt", "H(GeV)Tpt_{6th jet}t", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
     b2HT6pt_nu = new TH2D(m_processName + "_baseline1MuonAndHLT2b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
-    b3HT6pt_de = new TH2D(m_processName + "_baseline1Muon3b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
-    b3HT6pt_nu = new TH2D(m_processName + "_baseline1MuonAndHLT3b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
+
+    b3HT6pt_de = new TH2D(m_processName + "_baseline1Muon3b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins3b) / sizeof(Double_t) - 1, xbins3b, sizeof(ybins3b) / sizeof(Double_t) - 1, ybins3b);
+    b3HT6pt_nu = new TH2D(m_processName + "_baseline1MuonAndHLT3b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins3b) / sizeof(Double_t) - 1, xbins3b, sizeof(ybins3b) / sizeof(Double_t) - 1, ybins3b);
     //noMuon
-    b1HT6ptNoMu_de = new TH2D(m_processName + "_baseline4b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
-    b1HT6ptNoMu_nu = new TH2D(m_processName + "_baselineAndHLT4b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
+    b1HT6ptNoMu_de = new TH2D(m_processName + "_baseline4b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins4b) / sizeof(Double_t) - 1, xbins4b, sizeof(ybins4b) / sizeof(Double_t) - 1, ybins4b);
+    b1HT6ptNoMu_nu = new TH2D(m_processName + "_baselineAndHLT4b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins4b) / sizeof(Double_t) - 1, xbins4b, sizeof(ybins4b) / sizeof(Double_t) - 1, ybins4b);
+
     b2HT6ptNoMu_de = new TH2D(m_processName + "_baseline2b" + "_jetsHTAnd6pt", "H(GeV)Tpt_{6th jet}t", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
     b2HT6ptNoMu_nu = new TH2D(m_processName + "_baselineAndHLT2b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
-    b3HT6ptNoMu_de = new TH2D(m_processName + "_baseline3b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
-    b3HT6ptNoMu_nu = new TH2D(m_processName + "_baselineAndHLT3b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins) / sizeof(Double_t) - 1, xbins, sizeof(ybins) / sizeof(Double_t) - 1, ybins);
+
+    b3HT6ptNoMu_de = new TH2D(m_processName + "_baseline3b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins3b) / sizeof(Double_t) - 1, xbins3b, sizeof(ybins3b) / sizeof(Double_t) - 1, ybins3b);
+    b3HT6ptNoMu_nu = new TH2D(m_processName + "_baselineAndHLT3b" + "_jetsHTAnd6pt", "HT(GeV):p_{T}^{6th jet}", sizeof(xbins3b) / sizeof(Double_t) - 1, xbins3b, sizeof(ybins3b) / sizeof(Double_t) - 1, ybins3b);
 }
 
 void WH_HLTeff::LoopTree(UInt_t entry)
