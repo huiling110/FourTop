1652f7eecd01aa7b2c21852990dadf79a2cbde4a
huilling110 <huiling2016@outlook.com>
2024-05-22 13:17:47 +0800

    JOB: v0baselineHT450Jet6pt34_v77ForHLT

--- makeVariables_goodCode/src/makeVariablesMain.C
+++ makeVariables_goodCode/src/makeVariablesMain.C
@@ -57,7 +57,8 @@ void MakeVariablesMain::EventLoop(Bool_t baselineSel, Bool_t  tau1e1Sel, ULong_t
         if (baselineSel)
         {
             // if (!(jetVarMaker.getHT() > 550. && jetVarMaker.getJet_6pt() > 40. && jetVarMaker.getJet_num() >=6 && bjetM_num >= 1))
-            if (!(jetVarMaker.getHT() > 450. && jetVarMaker.getJet_num() >=6 && bjetM_num >= 2 )) //!1tau1l and 1tau0l baseline selection
+            // if (!(jetVarMaker.getHT() > 450. && jetVarMaker.getJet_num() >=6 && bjetM_num >= 2 )) //!1tau1l and 1tau0l baseline selection
+            if (!(jetVarMaker.getHT() > 450. && jetVarMaker.getJet_num() >=6 && bjetM_num >= 2 && jetVarMaker.getJet_6pt()>34.)) //!for HLT
             // if (!(jetVarMaker.getHT() > 200. && jetVarMaker.getJet_num() >=4 && bjetM_num >= 2)) //!1tau2l basleline selection
             // if (!(jetVarMaker.getHT() > 550 && jetVarMaker.getJet_6pt() > 40 && jetVarMaker.getJet_num() >=6 )) //!!!for btag-efficiency measurement
             {
