d49414d70294c930457490d139657957a66e7efd
huilling110 <huiling2016@outlook.com>
2024-05-31 10:42:36 +0800

    PLT: copied VLL all mass points

--- plotting/copyVLLsamples.py
+++ plotting/copyVLLsamples.py
@@ -2,38 +2,109 @@ import subprocess
 import os
 #!set up Voms credentials first 
 #!also cmsenv, for the xroot dependance wrong
+import input.VLL_Charis as VLL_Charis
+
+#!xroot -r for copying directory too slow
 
 
-# redirector = ''
-#from Charis: https://gitlab.cern.ch/ckoraka/vll-analysis/-/blob/master/samples/fileLists/Signal_2018.py?ref_type=heads
-sampleDic = {
-'VLL_EE_M600': '/store/user/ckoraka/VLferm_EWcouplings_4321_m600GeV_EE_to_4b/RunIISummer20UL18_NanoAODv9/230718_212705/0000/',
-'VLL_EN_M600': '/store/user/ckoraka/VLferm_EWcouplings_4321_m600GeV_EN_to_4b/RunIISummer20UL18_NanoAODv9/230718_212731/0000/',
-'VLL_NN_M600': '/store/user/ckoraka/VLferm_EWcouplings_4321_m600GeV_NN_to_4b/RunIISummer20UL18_NanoAODv9/230718_212756/0000/',
-}
+def copyDir():
+    redirector = 'root://cmsxrootd.hep.wisc.edu//'
+    # redirector = ''
+    #from Charis: https://gitlab.cern.ch/ckoraka/vll-analysis/-/blob/master/samples/fileLists/Signal_2018.py?ref_type=heads
+    sampleDic = {
+    # 'VLL_EE_M600': '/store/user/ckoraka/VLferm_EWcouplings_4321_m600GeV_EE_to_4b/RunIISummer20UL18_NanoAODv9/230718_212705/0000/',
+    # 'VLL_EN_M600': '/store/user/ckoraka/VLferm_EWcouplings_4321_m600GeV_EN_to_4b/RunIISummer20UL18_NanoAODv9/230718_212731/0000/',
+    # 'VLL_NN_M600': '/store/user/ckoraka/VLferm_EWcouplings_4321_m600GeV_NN_to_4b/RunIISummer20UL18_NanoAODv9/230718_212756/0000/',#done
 
-redirector = 'root://cmsxrootd.hep.wisc.edu//'
-VLLfile = 'root://cmsxrootd.hep.wisc.edu///store/user/ckoraka/VLferm_EWcouplings_4321_m500GeV_EE_to_4b/RunIISummer20UL18_NanoAODv9/230718_212434/0000/NanoAODv9_10.root'
+    # 'VLL_EE_M650': '/store/user/ckoraka/VLferm_EWcouplings_4321_m650GeV_EE_to_4b/RunIISummer20UL18_NanoAODv9/230718_212823/0000/',
+    # 'VLL_EN_M650': '/store/user/ckoraka/VLferm_EWcouplings_4321_m650GeV_EN_to_4b/RunIISummer20UL18_NanoAODv9/230718_212847/0000/',
+    # 'VLL_NN_M650': '/store/user/ckoraka/VLferm_EWcouplings_4321_m650GeV_NN_to_4b/RunIISummer20UL18_NanoAODv9/230718_212915/0000/',#done
 
-outDir = '/publicfs/cms/data/TopQuark/nanoAOD/2018/mc/VVL_600/' 
+    'VLL_EE_M700': '/store/user/ckoraka/VLferm_EWcouplings_4321_m700GeV_EE_to_4b/RunIISummer20UL18_NanoAODv9/230718_212943/0000/',
+    'VLL_EN_M700': '/store/user/ckoraka/VLferm_EWcouplings_4321_m700GeV_EN_to_4b/RunIISummer20UL18_NanoAODv9/230718_213011/0000/',
+    'VLL_NN_M700': '/store/user/ckoraka/VLferm_EWcouplings_4321_m700GeV_NN_to_4b/RunIISummer20UL18_NanoAODv9/230718_213039/0000/',
 
-for isam, ifile in sampleDic.items():
-    inputDir = redirector + ifile
-    ioutDir = outDir + isam + '/'
-    if not os.path.exists( ioutDir ):
-        os.mkdir( ioutDir )
-    command = 'xrdcp -r ' + inputDir + ' ' + ioutDir
-    print(command)
+    'VLL_EE_M750': '/store/user/ckoraka/VLferm_EWcouplings_4321_m750GeV_EE_to_4b/RunIISummer20UL18_NanoAODv9/230718_213110/0000/',
+    'VLL_EN_M750': '/store/user/ckoraka/VLferm_EWcouplings_4321_m750GeV_EN_to_4b/RunIISummer20UL18_NanoAODv9/230718_213135/0000/',
+    'VLL_NN_M750': '/store/user/ckoraka/VLferm_EWcouplings_4321_m750GeV_NN_to_4b/RunIISummer20UL18_NanoAODv9/230718_213202/0000/',
+
+    # 'VLL_EE_M800': '/store/user/ckoraka/VLferm_EWcouplings_4321_m800GeV_EE_to_4b/RunIISummer20UL18_NanoAODv9/230718_213240/0000/',
+    # 'VLL_EN_M800': '/store/user/ckoraka/VLferm_EWcouplings_4321_m800GeV_EN_to_4b/RunIISummer20UL18_NanoAODv9/230718_213240/0000/',
+    # 'VLL_NN_M800': '/store/user/ckoraka/VLferm_EWcouplings_4321_m800GeV_NN_to_4b/RunIISummer20UL18_NanoAODv9/230718_213240/0000/',
+
+
+
+    }
+
+    # redirector = 'root://cmsxrootd-cms.infn.it//'
+    VLLfile = 'root://cmsxrootd.hep.wisc.edu///store/user/ckoraka/VLferm_EWcouplings_4321_m500GeV_EE_to_4b/RunIISummer20UL18_NanoAODv9/230718_212434/0000/NanoAODv9_10.root'
+
+    # outDir = '/publicfs/cms/data/TopQuark/nanoAOD/2018/mc/VVL_600/' 
+    outDir = '/publicfs/cms/data/TopQuark/nanoAOD/2018/mc/' 
+
+    for isam, ifile in sampleDic.items():
+        inputDir = redirector + ifile
+        ioutDir = outDir + isam + '/'
+        if not os.path.exists( ioutDir ):
+            os.mkdir( ioutDir )
+        # command = 'xrdcp -r ' + inputDir + ' ' + ioutDir
+        command = 'xrdcp -r --parallel 5 ' + inputDir + ' ' + ioutDir
+        # command = 'xrdcp -r --parallel 5 ' + inputDir + '*.root ' + ioutDir
+        print(command)
+        # process = subprocess.run([command], shell=True)
+        # output =  process.stdout
+        # print(output)
+        
+    # inputDir = redirector + sampleDic['VLL_EE_M600']
+    # # command = 'xrdcp '+ VLLfile + ' ' + outDir #only works with CMSSW
+    # command = 'xrdcp -r ' + inputDir + ' ' + outDir
+    # print(command)
     # process = subprocess.run([command], shell=True)
     # output =  process.stdout
     # print(output)
     
-# inputDir = redirector + sampleDic['VLL_EE_M600']
-# # command = 'xrdcp '+ VLLfile + ' ' + outDir #only works with CMSSW
-# command = 'xrdcp -r ' + inputDir + ' ' + outDir
-# print(command)
-# process = subprocess.run([command], shell=True)
-# output =  process.stdout
-# print(output)
+def copySingleFile():
+    for isam, ifile in VLL_Charis.filesetSignal.items():
+        print(isam)
+        outName = transform_string(isam) 
+        # outDir = '/publicfs/cms/data/TopQuark/nanoAOD/2018/mc/VLL/' + outName + '/'
+        outDir = '/publicfs/cms/data/TopQuark/nanoAOD/2018/mc/VLL/' + outName + '/'
+        
+        if not os.path.exists( outDir ):
+            os.mkdir( outDir )
+        print(outName)
+        # if not outName == 'VLL_EE_M500': continue
+        # if outName == 'VLL_EE_M500': continue
+        if not '1000' in outName: continue
+        
+        for inano in ifile:
+            # inputDir = redirector + inano
+            command = 'xrdcp ' + inano + ' ' + outDir
+            print(command)
+            process = subprocess.run([command], shell=True)
+            output =  process.stdout
+            print(output)
+        print('\n\n') 
+        
+        
+        
+        
+        
+def transform_string(input_str):
+    # Split the string by underscores
+    parts = input_str.split('_')
+    # Extract the relevant parts
+    prefix = parts[0] + '_' + parts[1]  # 'VLL_EE'
+    mass_part = parts[2]  # 'M-500GeV'
+        # Remove the hyphen and 'GeV'
+    mass_value = mass_part.replace('-', '').replace('GeV', '')
+    # Combine the parts
+    # output_str = f"{prefix}_M{mass_value}"
+    output_str = f"{prefix}_{mass_value}"
+    return output_str
+        
+if __name__=='__main__':
+    # copyDir()
+    copySingleFile()
 
 
