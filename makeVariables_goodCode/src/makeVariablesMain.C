2ed7114ecf0e3d1b819b5e6b4c7d774990c9270b
huilling110 <huiling2016@outlook.com>
2024-06-10 14:00:53 +0800

    JOB: v1baselineHardro_btagMTWeight_v76WithVLLAllMass

--- makeVariables_goodCode/src/makeVariablesMain.C
+++ makeVariables_goodCode/src/makeVariablesMain.C
@@ -56,7 +56,6 @@ void MakeVariablesMain::EventLoop(Bool_t baselineSel, Bool_t  tau1e1Sel, ULong_t
         Int_t bjetM_num = m_isRun3? bjetPTMVarMaker.getJet_num(): bjetMVarMaker.getJet_num();
         if (baselineSel)
         {
-            // if (!(jetVarMaker.getHT() > 450. && jetVarMaker.getJet_num() >=6 && bjetM_num >= 2 )) //!1tau1l and 1tau0l baseline selection
             // if (!(jetVarMaker.getHT() > 450. && jetVarMaker.getJet_num() >=6 && bjetM_num >= 2 && jetVarMaker.getJet_6pt()>34.)) //!for HLT
             // if (!(jetVarMaker.getHT() > 200. && jetVarMaker.getJet_num() >=4 && bjetM_num >= 2)) //!1tau2l basleline selection
             // if (!(jetVarMaker.getHT() > 550 && jetVarMaker.getJet_6pt() > 40 && jetVarMaker.getJet_num() >=6 )) //!!!for btag-efficiency measurement
