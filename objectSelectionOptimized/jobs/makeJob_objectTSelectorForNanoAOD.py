ef6efe1496d8bd3c96ab7cecc1d6b0657e781879
huilling110 <huiling2016@outlook.com>
2024-07-01 10:20:51 +0800

    JOB: v77ForHLT for 2017

--- objectSelectionOptimized/jobs/makeJob_objectTSelectorForNanoAOD.py
+++ objectSelectionOptimized/jobs/makeJob_objectTSelectorForNanoAOD.py
@@ -48,10 +48,10 @@ codePath = os.path.dirname(os.path.abspath(__file__)) + '/'
 # jobVersionName = 'v75AddTauTTTTNoHTCut/'
 # jobVersionName = 'v75OverlapRemovalFTau/'
 # jobVersionName = 'v76For1tau2l/'
-# jobVersionName = 'v77ForHLT/'
+jobVersionName = 'v77ForHLT/'
 # jobVersionName = 'v76WithVLLSample/'
 # jobVersionName = 'v76WithVLLAllMass/'
-jobVersionName = 'v77forBtagMeasurement/'
+# jobVersionName = 'v77forBtagMeasurement/'
 
 #run3
 # jobVersionName = 'v0Testing/'
@@ -78,9 +78,9 @@ jobVersionName = 'v77forBtagMeasurement/'
 #!same version numbers means no change in algrithm but only in selection
 
 def main():
-    era = '2016'
+    # era = '2016'
     # era = '2016APV'
-    # era = '2017'
+    era = '2017'
     # era = '2018'
     # era = '2022_13p6/crabNanoPost_2022postEE_v3'
     # era = '2022_13p6/crabNanoPost_2022preEE_v3'
