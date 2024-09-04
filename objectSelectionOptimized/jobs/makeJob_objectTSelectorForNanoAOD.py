90d456d96095075185872160adca0020ecfde773
huilling110 <huiling2016@outlook.com>
2024-09-04 17:32:08 +0800

    small changes

--- objectSelectionOptimized/jobs/makeJob_objectTSelectorForNanoAOD.py
+++ objectSelectionOptimized/jobs/makeJob_objectTSelectorForNanoAOD.py
@@ -40,9 +40,9 @@ jobVersionName = 'v81addSysSum/'
 #!same version numbers means no change in algrithm but only in selection
 
 def main():
-    # era = '2016'
+    era = '2016'
     # era = '2016APV'
-    era = '2017'
+    # era = '2017'
     # era = '2018'
     # era = '2022_13p6/crabNanoPost_2022postEE_v3'
     # era = '2022_13p6/crabNanoPost_2022preEE_v3'
