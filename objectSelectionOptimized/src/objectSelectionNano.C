9677d9a0b3b773c973d40dcb3114e076e34fcda3
huilling110 <huiling2016@outlook.com>
2024-09-09 09:21:34 +0800

    OS: switched baseline bjet>0 for 1tau2l to allow CR

--- objectSelectionOptimized/src/objectSelectionNano.C
+++ objectSelectionOptimized/src/objectSelectionNano.C
@@ -105,7 +105,8 @@ void objectSelection::EventLoop(const Bool_t iftauSel, const Bool_t preSelection
         if(!(OS::ifEventPass(preSelection, jetSel.getSize()>3, m_cutflow, 4))){//!for 1tau2l
             continue;
         }
-        if(!(OS::ifEventPass(preSelection, bjetMSel.getSize()>1, m_cutflow, 5))){//baseline for 1tau1l and 1tau0l
+        // if(!(OS::ifEventPass(preSelection, bjetMSel.getSize()>1, m_cutflow, 5))){//baseline for 1tau1l and 1tau0l
+        if(!(OS::ifEventPass(preSelection, bjetMSel.getSize()>0, m_cutflow, 5))){//baseline for 1tau2l
             continue;
         }//!No b-tag ,for b-tag efficiency measurement!
         // if(!OS::ifEventPass(preSelection, jetSel.getHT()>480.&& jetSel.get6thPt()>38., m_cutflow, 6)){//!baseline for 1tau1l and 1tau0l
--- plotting/dataPDOverlapRemoval.py
+++ plotting/dataPDOverlapRemoval.py
@@ -1,5 +1,8 @@
 import ROOT
 import pandas as pd
+# from root_pandas import to_root
+import uproot
+
 import usefulFunc as uf
 
 def main():
@@ -15,39 +18,65 @@ def main():
         for i in ilist:
             print(i)
         print('\n')
+    
+    df_singleMu = getPandasDF(inputDir, dic, dataPD='singleMu')
+    df_doubleMu = getPandasDF(inputDir, dic, dataPD='doubleMu')
+    df_muonEG = getPandasDF(inputDir, dic, dataPD='muonEG')
+    df_eGamma = getPandasDF(inputDir, dic, dataPD='eGamma')
+    #seems no overlaping event in each PD with regard to 'event' value
+    
+    df_lepton = pd.concat([df_singleMu, df_doubleMu, df_muonEG, df_eGamma], axis=0)
+    print('lepton entries: ', df_lepton.shape[0])
+    
+    #!check if only filtering by 'event' is enough to remove the overlap  
+    duplicates = df_lepton[df_lepton.duplicated(subset='event', keep=False)]
+    print('Entries with the same event value:', duplicates.shape[0])
+    # Print the rows that have duplicate 'event' values
+    duplicates.sort_values(by='event', inplace=True)
+    print(duplicates[['event', 'run', 'jets_1pt', 'HLT_PFHT450_SixJet40_BTagCSV_p056']])
+    
+    df_unique = df_lepton.drop_duplicates(subset='event', keep='first')
+    print('unique lepton entries: ', df_unique.shape[0])
+    
+    output_file = inputDir + 'leptonSum.root'
+    # rdf = ROOT.RDF.MakeNumpyDataFrame(df_unique.to_dict(orient='list'))#not working
+    # rdf = ROOT.RDF.MakeNumpyDataFrame(data)
+    # rdf.Snapshot("newtree", output_file)
+    
+    print(f'Output file: {output_file}')
+    
      
-    df_singleMu =  ROOT.RDataFrame("newtree", [f"{inputDir}{sub}.root" for sub in dic['singleMu']])
-    print('singleMu entries: ', df_singleMu.Count().GetValue())
+def getPandasDF(inputDir, dic, dataPD='singleMu'):
+    df_singleMu =  ROOT.RDataFrame("newtree", [f"{inputDir}{sub}.root" for sub in dic[dataPD]])
+    print(f'{dataPD} entries: ', df_singleMu.Count().GetValue())
     print('run min= ', df_singleMu.Min("run").GetValue(), 'run max= ', df_singleMu.Max("run").GetValue())
     
     columns = df_singleMu.GetColumnNames()
-    # print(columns)
-    columns_pre = list(columns)
-    print(columns_pre)
+    # columns_pre = list(columns)
+    columns_pre = [str(col) for col in columns]#!have to manuelly convert the column names to string
     df_numpy = df_singleMu.AsNumpy(columns=columns_pre)
-    # # df_pandas_pre = df_singleMu.AsNumpy(columns=columns)
-    print("Column names in the numpy structure:")
-    for col in df_numpy.keys():
-        print(col)
-    print("Numpy structure:")
-    for col, array in df_numpy.items():
-        print(f"Column: {col}")
-        print(f"  Type: {array.dtype}")
-        print(f"  Shape: {array.shape}")
-        print(f"  First few values: {array[:5]}")
-        print()
+    # for col in df_numpy.keys():
+    #     print(col)
+    # print("Numpy structure:")
+    # for col, array in df_numpy.items():
+    #     print(f"Column: {col}")
+    #     print(f"  Type: {col.__class__}")
+    #     print(f"  Type: {array.dtype}")
+    #     print(f"  Shape: {array.shape}")
+    #     print(f"  First few values: {array[:5]}")
 
-
-    # df_pandas = pd.DataFrame(df_pandas_pre)
+    df_pandas = pd.DataFrame(df_numpy)
     # print(df_pandas.head())
     # print(df_pandas.columns)
-    # duplicates = df_pandas[df_pandas.duplicated(subset=['event'], keep=False)]
-    # if not duplicates.empty:
-    #     print("Entries with the same 'event' value:")
-    #     print(duplicates)
-    # else:
-    #     print("No duplicate 'event' values found.")
+    duplicates = df_pandas[df_pandas.duplicated(subset=['event'], keep=False)]
+    if not duplicates.empty:
+        print("Entries with the same 'event' value:")
+        print(duplicates)
+    else:
+        print("No duplicate 'event' values found.")
     
+    print('\n')
+    return df_pandas 
     
     # df_doubleMu =  ROOT.RDataFrame("newtree", [f"{inputDir}{sub}.root" for sub in dic['doubleMu']])
     # print('doubleMu entries: ', df_doubleMu.Count().GetValue())
