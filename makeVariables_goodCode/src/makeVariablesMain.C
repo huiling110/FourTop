0741b9640b4f7ceb09005d3d65bedd04a88815c8
huilling110 <huiling2016@outlook.com>
2024-09-16 13:19:19 +0800

    MV: added tau and lepton cut in 1tau2l baseline

--- makeVariables_goodCode/src/makeVariablesMain.C
+++ makeVariables_goodCode/src/makeVariablesMain.C
@@ -59,7 +59,7 @@ void MakeVariablesMain::EventLoop(Bool_t baselineSel, Bool_t  tau1e1Sel, ULong_t
         Int_t bjetM_num = m_isRun3? bjetPTMVarMaker.getJet_num(): bjetMVarMaker.getJet_num();
         if (baselineSel)
         {
-            if (!(jetVarMaker.getHT() > 200. && jetVarMaker.getJet_num() >=2 && bjetM_num >= 1)) //!1tau2l basleline selection
+            if (!(jetVarMaker.getHT() > 200. && jetVarMaker.getJet_num() >=2 && bjetM_num >= 1&& tauVarMaker.getNum()==1 && eleTopVarMaker.getNum()+muTopTVarMaker.getNum()==2) ) //!1tau2l basleline selection
             {
                 continue;
             }
