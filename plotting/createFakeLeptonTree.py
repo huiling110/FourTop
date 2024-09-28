aa1632e1d5dbb9063274fb8d24d3640429a91555
huilling110 <huiling2016@outlook.com>
2024-09-28 19:31:54 +0800

    PLT: createFakeLepton can not save lepTopMVAT_1pt

--- plotting/createFakeLeptonTree.py
+++ plotting/createFakeLeptonTree.py
@@ -6,18 +6,27 @@ def main():
     # inputData = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2018/v0baseline1tau2l_v2_v84fakeLeptonUpdate/data/leptonSum_2018.root'
     inputData = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2018/v0baseline1tau2l_v2_v84fakeLeptonUpdateV2/data/leptonSum_2018.root'
     
+    
+    inputDir = os.path.dirname(inputData)
+    outFile = inputDir.replace('data', 'mc') + '/fakeLepton.root'
+    
     AR = 'lepTopMVAF_isAR'
     df = ROOT.RDataFrame('newtree', inputData)
     df_AR = df.Filter(AR)
     print('AR entries: ', df_AR.Count().GetValue())
+   
+    all_columns = df_AR.GetColumnNames() 
+    #!replace lepTopMVAT_pt with lepTopMVAF_ptCorrected
+    columns_to_remove = ["lepTopMVAT_1pt", "lepTopMVAT_2pt"]
+    columns_to_keep = [col for col in all_columns if col not in columns_to_remove]
+    # Create a new dataframe with the selected columns
+    df_AR.Snapshot('newtree', outFile, columns_to_keep)
     
-    #!replace lepTopMVAT_pt with lepTopMVAF_ptConeCorrected 
-    
-    
-    
-    inputDir = os.path.dirname(inputData)
-    outFile = inputDir.replace('data', 'mc') + '/fakeLepton.root'
-    df_AR.Snapshot('newtree', outFile)
+    df_new = ROOT.RDataFrame('newtree', outFile)
+    df_new = df_new.Define("lepTopMVAT_1pt", "lepTopMVAF_1ptCorrected")
+    df_new = df_new.Define("lepTopMVAT_2pt", "lepTopMVAF_2ptCorrected")
+    output_file = ROOT.TFile(outFile, "RECREATE")
+    df_new.Snapshot('newtree', outFile)#overwrite the file
     print(f'Output file: {outFile}')
     
     
