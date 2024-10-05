8ab620f173bc0f2d6494994ee3e058992dd04698
huilling110 <huiling2016@outlook.com>
2024-10-05 09:37:54 +0800

    JOB: v0baselineLep_v84Pre1tau2lLepF2V2 for 2017

--- makeVariables_goodCode/jobs/makeJob_makeVaribles_forBDT.py
+++ makeVariables_goodCode/jobs/makeJob_makeVaribles_forBDT.py
@@ -36,8 +36,9 @@ def main():
     # inVersion = 'v83for1tau2lEleEtaCut'
     # inVersion = 'v84fakeLeptonUpdateV2'
     # inVersion = 'v84Pre1tau2lNoLepCut'
-    inVersion = 'v84Pre1tau2lLepF2'
+    # inVersion = 'v84Pre1tau2lLepF2'
     # inVersion = 'v84Pre1tau2lNoLepCut'
+    inVersion = 'v84Pre1tau2lLepF2V2'
 
     # outVersion = 'v0baselineHardro'
     outVersion = 'v0baselineLep'
