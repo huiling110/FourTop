8aa515ef96fa89d191e377a560e7594533ce1b77
huilling110 <huiling2016@outlook.com>
2024-11-04 11:56:51 +0800

    PLT: writeDatacard.py looking good

--- plotting/writeDatacard.py
+++ plotting/writeDatacard.py
@@ -26,9 +26,42 @@ def main():
     
    processes = gq.proChannelDic[channel]
    print(processes)
+  
+   era = uf.getEraFromDir(inputTemplate) 
+   sysDic = getSysDic(processes, channel, era) 
+#    print(sysDic)
+   for i, iv in sysDic.items():
+       print(i, iv)
+       
+   write_shape_datacard(outCard, inputTemplate, channel, processes,  sysDic) 
 
+def getSysDic(processes, channel, era):
+    sysDic = {}
+    for sys, sysList in MCSys.items():
+        sysName = sys if sysList[0] else f"{sys}_{era}"
+        sysDic[sysName] = []
+        sysDic[sysName].append("shape")     
+        proSysDic = getProSysDic(sysName, sysList, processes)   
+        sysDic[sysName].append(proSysDic)
+
+    return  sysDic
+     
+     
+def getProSysDic(sys, sysList, processes):
+    proSys = {}
+    for ipro in processes:
+        if uf.isData(ipro) : continue
+        if ipro=='fakeTau':
+            proSys[ipro] = 1 if sysList[1]==1 else 0
+        elif ipro=='fakeLepton':
+            proSys[ipro] = 1 if sysList[1]==2 else 0
+        else: 
+            proSys[ipro] = 1 if sysList[1]==0 else 0
+             
+    return proSys            
+             
    
-def write_shape_datacard(output_file, root_file, channel_name, processes, rates, systematics):
+def write_shape_datacard(output_file, root_file, channel_name, processes,  systematics):
     """
     Write a shape datacard for CMS Combine.
 
@@ -44,8 +77,14 @@ def write_shape_datacard(output_file, root_file, channel_name, processes, rates,
     """
 
     num_processes = len(processes)
-    signal_index = 0  # Assuming the first process is the signal
-
+    signal_index = -1  # Assuming the first process is the signal
+    
+    process_col_width = 40
+    value_col_width = 12
+    
+    rates = f"rate".ljust(process_col_width)
+    proString = f"process".ljust(process_col_width)
+    binString = f""
     lines = [
         "imax 1  number of channels",
         "jmax {}  number of background processes".format(num_processes - 1),
@@ -56,19 +95,23 @@ def write_shape_datacard(output_file, root_file, channel_name, processes, rates,
         f"bin         {channel_name}",
         "observation -1",
         "---------------",
-        "bin                        " + " ".join([channel_name] * num_processes),
-        "process                   " + " ".join(processes),
-        "process                   " + " ".join(str(i - signal_index) for i in range(num_processes)),
-        "rate                      " + " ".join(f"{rate:.3f}" for rate in rates),
+        f"{binString}{''.join([channel_name.ljust(value_col_width) for _ in range(num_processes)])}",
+        f"{proString}{''.join([proc.ljust(value_col_width) for proc in processes])}",
+        f"{proString}{''.join([str(i - signal_index).ljust(value_col_width) for i in range(num_processes)])}",
+        f"{rates}{''.join(['-1'.ljust(value_col_width) for i in range(num_processes)])}" ,
+        
         "---------------"
     ]
 
     for unc_name, (unc_type, uncertainties) in systematics.items():
-        line = f"{unc_name}   {unc_type}   "
+        line = f"{unc_name.ljust(process_col_width)}{unc_type.ljust(value_col_width)}"
         line += " ".join(
-            f"{uncertainties[proc][0]:.3f}/{uncertainties[proc][1]:.3f}" if proc in uncertainties else "-"
+            # f"{uncertainties[proc][0]:.3f}/{uncertainties[proc][1]:.3f}" if proc in uncertainties else "-"
+            # for proc in processes
+            f"{uncertainties[proc]}" if proc in uncertainties else "-"
             for proc in processes
         )
+
         lines.append(line)
 
     # Write to file
