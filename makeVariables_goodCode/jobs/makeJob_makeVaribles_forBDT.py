7fe4813a0cdb9c642c726610d1df60e7a0ed2e0c
huilling110 <huiling2016@outlook.com>
2024-11-14 18:45:13 +0800

    JOB: v0baselineLep_v88PSWeightFixedLepPre

--- makeVariables_goodCode/jobs/makeJob_makeVaribles_forBDT.py
+++ makeVariables_goodCode/jobs/makeJob_makeVaribles_forBDT.py
@@ -45,7 +45,8 @@ def main():
     # inVersion = 'v86HadroPreSelWithTTWTTZNLO'
     # inVersion = 'v86LepPreSel'
     # inVersion = 'v87LepPreSel_GammaRemovalBugFixed'
-    inVersion = 'v87addPdfPSWeightSum'
+    # inVersion = 'v87addPdfPSWeightSum'
+    inVersion = 'v88PSWeightFixedLepPre'
 
     # outVersion = 'v0baselineHardro'
     outVersion = 'v0baselineLep'
