d2b200190ec2e64e177096a8d86047aad0e45195
huilling110 <huiling2016@outlook.com>
2024-11-14 15:25:16 +0800

    JOB: v88PSWeightFixedLepPre for 2018

--- objectSelectionOptimized/jobs/makeJob_OS_fromRuobing2.py
+++ objectSelectionOptimized/jobs/makeJob_OS_fromRuobing2.py
@@ -40,7 +40,8 @@ codePath = os.path.dirname(os.path.abspath(__file__)) + '/'
 # jobVersionName = 'v86HadroPreSelWithGammaRemoval/'
 # jobVersionName = 'v86HadroPreSelWithTTWTTZNLO_testtest/'
 # jobVersionName = 'v87addPdfPSWeightSum/'
-jobVersionName = 'v87addPdfPSWeightSum/'# with new downloaded MC samples actuaclly
+# jobVersionName = 'v87addPdfPSWeightSum/'# with new downloaded MC samples actuaclly
+jobVersionName = 'v88PSWeightFixedLepPre/'
 
 isRuobing = False
 #!same version numbers means no change in algrithm but only in selection
@@ -52,17 +53,14 @@ def main():
     era = '2018'
     # era = '2022_13p6/crabNanoPost_2022postEE_v3'
     # era = '2022_13p6/crabNanoPost_2022preEE_v3'
-    # onlyMC = True
     # dataList = ['jetHT', 'BTagCSV']#!for 1tau1l and 1tau0l
     # dataList = ['singleMu'] #!for HLT
     # dataList = ['JetMET', 'JetHT']
     # dataList = [ 'Muon']
     # dataList = ['doubleMu', 'muonEG', 'eGamma', 'singleMu'] #!for 1tau2l  
     
-    # sumProToSkip = ['jetHT', 'ttExtra'] #! and 1tau2l
-    # sumProToSkip = ['jetHT', 'BTagCSV', 'qcd', 'ttExtra'] #1tau2l #! need ttExtra for BDT training
-    # sumProToSkip = ['jetHT', 'BTagCSV', 'qcd', 'ttExtra'] #1tau2l #! need ttExtra for BDT training
-    sumProToSkip = ['singleMu', 'singleE','doubleMu', 'muonEG', 'eGamma', 'doubleEG', 'ttExtra'] #1tau1l and 1tau0l , 
+    sumProToSkip = ['jetHT', 'BTagCSV', 'qcd', 'ttExtra'] #1tau2l #! need ttExtra for BDT training
+    # sumProToSkip = ['singleMu', 'singleE','doubleMu', 'muonEG', 'eGamma', 'doubleEG', 'ttExtra'] #1tau1l and 1tau0l , 
 
 
 
