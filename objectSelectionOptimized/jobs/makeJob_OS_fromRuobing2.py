b7a2c5f0026e6d77c8495f2956f896fc86f70018
huilling110 <huiling2016@outlook.com>
2024-11-14 19:02:17 +0800

    JOB: v88PSWeightFixedLepPre resubmit

--- objectSelectionOptimized/jobs/makeJob_OS_fromRuobing2.py
+++ objectSelectionOptimized/jobs/makeJob_OS_fromRuobing2.py
@@ -41,8 +41,8 @@ codePath = os.path.dirname(os.path.abspath(__file__)) + '/'
 # jobVersionName = 'v86HadroPreSelWithTTWTTZNLO_testtest/'
 # jobVersionName = 'v87addPdfPSWeightSum/'
 # jobVersionName = 'v87addPdfPSWeightSum/'# with new downloaded MC samples actuaclly
-# jobVersionName = 'v88PSWeightFixedLepPre/'
-jobVersionName = 'v88PSWeightFixedHadroPre/'
+jobVersionName = 'v88PSWeightFixedLepPre/'
+# jobVersionName = 'v88PSWeightFixedHadroPre/'
 
 isRuobing = False
 #!same version numbers means no change in algrithm but only in selection
@@ -56,12 +56,10 @@ def main():
     # era = '2022_13p6/crabNanoPost_2022preEE_v3'
     # dataList = ['jetHT', 'BTagCSV']#!for 1tau1l and 1tau0l
     # dataList = ['singleMu'] #!for HLT
-    # dataList = ['JetMET', 'JetHT']
-    # dataList = [ 'Muon']
     # dataList = ['doubleMu', 'muonEG', 'eGamma', 'singleMu'] #!for 1tau2l  
     
-    # sumProToSkip = ['jetHT', 'BTagCSV', 'qcd', 'ttExtra'] #1tau2l #! need ttExtra for BDT training
-    sumProToSkip = ['singleMu', 'singleE','doubleMu', 'muonEG', 'eGamma', 'doubleEG', 'ttExtra'] #1tau1l and 1tau0l , 
+    sumProToSkip = ['jetHT', 'BTagCSV', 'qcd', 'ttExtra'] #1tau2l #! need ttExtra for BDT training
+    # sumProToSkip = ['singleMu', 'singleE','doubleMu', 'muonEG', 'eGamma', 'doubleEG', 'ttExtra'] #1tau1l and 1tau0l , 
 
 
 
