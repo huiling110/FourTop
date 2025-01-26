d5f4cbdd3453a246ecb180b5e65f2b029010a6b7
huilling110 <huiling2016@outlook.com>
2025-01-26 10:49:05 +0800

    JOB: v94HadroPreJetVetoHemOnly

--- objectSelectionOptimized/jobs/makeJob_OS_fromRuobing2.py
+++ objectSelectionOptimized/jobs/makeJob_OS_fromRuobing2.py
@@ -39,6 +39,7 @@ codePath = os.path.dirname(os.path.abspath(__file__)) + '/'
 # jobVersionName = 'v93HadroPreJetVetoPileupID_TESdm0Up/'
 jobVersionName = 'v94HadroPreJetVetoHemOnly/'
 
+
 #!!!TES = 0, //no correction; 1: up; 2: down; 3: up, decayMode=0; 4: down, decayMode=0; 5: up, decayMode=1; 6: down, decayMode=1; 7: up, decayMode=10; 8: down, decayMode=10; 9: up, decayMode=11; 10: down, decayMode=11
 TES = 0 #!!!
 
