31475bc9005918ad238633168fa1f57a5b5863f9
huilling110 <huiling2016@outlook.com>
2025-07-15 14:38:19 +0800

    TTTT: setEnv_cmsenv.sh good

---
+++ setEnv_cmsenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export PATH=/cvmfs/container.ihep.ac.cn/bin:$PATH
+# alias hep7='hep_container shell CentOS7'
+hep_container shell CentOS7 
+
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cmsenv
+
+
+
+
+
