0ffa6f7c6e56371a072eda0db1f894a60452b597
huilling110 <huiling2016@outlook.com>
2025-07-27 09:20:03 +0800

    OS: made job submitting with parameters

--- objectSelectionOptimized/jobs/makeJob_OS_fromRuobing2.py
+++ objectSelectionOptimized/jobs/makeJob_OS_fromRuobing2.py
@@ -32,18 +32,6 @@ codePath = os.path.dirname(os.path.abspath(__file__)) + '/'
 # jobVersionName = 'v94HadroPreJetVetoHemOnly_METDown/' #2018
 # jobVersionName = 'v94HadroPreJetVetoHemOnly_TESdm11Down/'
 
-#!!!TES = 0, //no correction; 1: up; 2: down; 3: up, decayMode=0; 4: down, decayMode=0; 5: up, decayMode=1; 6: down, decayMode=1; 7: up, decayMode=10; 8: down, decayMode=10; 9: up, decayMode=11; 10: down, decayMode=11
-TES = 0 #!!!
-eleScale = 0 #!!! 0: nominal; 1: up; 2: down
-JESSys = 0 #!!! nominal: 0;
-JERSys = 0 #!!! 0: no correction; 1: up; 2: down
-METSys = 2 #!!! nominal: 0; 1: up; 2: down
-# if1tau2l = 0 #!!!True 
-if1tau2l = 1 #!!!True 
-# era = '2016'
-# era = '2016APV'
-# era = '2017'
-era = '2018'
 
 # jobVersionName = 'v91TESAddedLepPre/'
 # jobVersionName = 'v91TESAddedLepPre_JETPt22/'
@@ -51,18 +39,33 @@ era = '2018'
 # jobVersionName = 'v91TESAddedLepPre_METDown/'
 # jobVersionName = 'v91TESAddedLepPre_TESdm11Down/'
 # jobVersionName = 'v91TESAddedLepPre_EleScaleDown/'
-# jobVersionName = 'v94LepPreJetVetoHemOnly/'#3 years submitted
 # jobVersionName = 'v94LepPreJetVetoHemOnly_JESPt22/' #2018; 2017, 2016
 # jobVersionName = 'v94LepPreJetVetoHemOnly_JERDown/'#2018
-jobVersionName = 'v94LepPreJetVetoHemOnly_METDown/' # 3 years submitted
-# jobVersionName = 'v94LepPreJetVetoHemOnly_EleScalHeDown/'
-# jobVersionName = 'v94LepPreJetVetoHemOnly_TESdm11Down/' #dm0 3years down
+# jobVersionName = 'v94LepPreJetVetoHemOnly_METDown/' # 3 years submitted
+# jobVersionName = 'v94LepPreJetVetoHemOnly_EleScaleDown/'
+# jobVersionName = 'v94LepPreJetVetoHemOnly_TESdm0Up/' #dm0 3years down
 
 #!Have to submit one version at one time. It seems job sricpts can not be overwriten
 #!same version numbers means no change in algrithm but only in selection
 isRuobing = False
 #done by Ruobing: submit jobs in bunches for faster job submission; http://afsapply.ihep.ac.cn/cchelp/zh/local-cluster/jobs/HTCondor/
-def main():
+def main(
+    TES = 4 ,#!!!TES = 0, //no correction; 1: up; 2: down; 3: up, decayMode=0; 4: down, decayMode=0; 5: up, decayMode=1; 6: down, decayMode=1; 7: up, decayMode=10; 8: down, decayMode=10; 9: up, decayMode=11; 10: down, decayMode=11
+    eleScale = 0, #!!! 0: nominal; 1: up; 2: down
+    JESSys = 0 ,#!!! nominal: 0;
+    JERSys = 0, #!!! 0: no correction; 1: up; 2: down
+    METSys = 0, #!!! nominal: 0; 1: up; 2: down
+    # if1tau2l = 0 #!!!True 
+    if1tau2l = 1, #!!!True 
+    # era = '2016'
+    # era = '2016APV'
+    # era = '2017'
+    era = '2018',
+    jobVersionNamePre = 'v94LepPreJetVetoHemOnly',#3 years submitted
+    
+):
+    
+    jobVersionName = getJobVersionName(jobVersionNamePre, TES, eleScale, JESSys, JERSys, METSys)
     
     ifSys = any(substring in jobVersionName for substring in ['Up', 'Down', 'JES'])
     print('ifSys=', ifSys)
@@ -100,20 +103,37 @@ def main():
     uf.checkMakeDir(jobScriptsFolder)
 
     inputDirMC = inputDir + 'mc/'
-    makeJobsInDir( inputDirMC, outputDir , jobScriptsFolder, False,   eraOut, isRun3,  sumProToSkip)
+    makeJobsInDir( inputDirMC, outputDir , jobScriptsFolder, False,   eraOut, isRun3,  sumProToSkip, TES, eleScale, JESSys, JERSys, METSys, if1tau2l)
     # if not onlyMC:
         # for idata in dataList:
     inputDirData = inputDir + 'data/'
-    makeJobsInDir( inputDirData, outputDir, jobScriptsFolder, True, eraOut , isRun3, sumProToSkip)
+    makeJobsInDir( inputDirData, outputDir, jobScriptsFolder, True, eraOut , isRun3, sumProToSkip, TES, eleScale, JESSys, JERSys, METSys, if1tau2l)
 
     makeSubAllJobs( jobsDir , eraOut)
-    # subprocess.run('chmod 777 '+jobScriptsFolder + "*sh", shell=True )
     subprocess.run(f"nohup python3 /workfs2/cms/huahuil/CMSSW_10_6_20/src/FourTop/objectSelectionOptimized/jobs/speedOS.py {jobScriptsFolder}/cluster_logs.csv > {jobScriptsFolder}output.log 2>&1 &", shell=True )
-    # subprocess.Popen(f"python3 /workfs2/cms/huahuil/CMSSW_10_6_20/src/FourTop/objectSelectionOptimized/jobs/speedOS.py {jobScriptsFolder}/cluster_logs.csv", shell=True )
-    # subprocess.Popen(f"python3 /workfs2/cms/huahuil/CMSSW_10_6_20/src/FourTop/objectSelectionOptimized/jobs/speedOS.py {jobScriptsFolder}/cluster_logs.csv", shell=True )
 
     print('done submitting jobs for: ', jobVersionName) 
 
+def getJobVersionName(jobVersionNamePre, TES, eleScale, JESSys, JERSys, METSys):
+    sysKey = {1: 'Up', 2: 'Down'}
+    if TES ==0 and eleScale==0 and JESSys==0 and JERSys==0 and METSys==0:
+        jobVersionName = f'{jobVersionNamePre}/'
+    elif eleScale==0 and JESSys==0 and JERSys==0 and METSys==0 and (not TES==0): 
+#!!!TES = 0, //no correction; 1: up; 2: down; 3: up, decayMode=0; 4: down, decayMode=0; 5: up, decayMode=1; 6: down, decayMode=1; 7: up, decayMode=10; 8: down, decayMode=10; 9: up, decayMode=11; 10: down, decayMode=11
+        TESkey = {1: 'Up', 2: 'Down', 3: 'dm0Up', 4: 'dm0Down', 5: 'dm1Up', 6: 'dm1Down', 7: 'dm10Up', 8: 'dm10Down', 9: 'dm11Up', 10: 'dm11Down'}
+        jobVersionName = f'{jobVersionNamePre}_TES{TESkey[TES]}/'
+    elif TES==0 and JESSys==0 and JERSys==0 and METSys==0 and (not eleScale==0):
+        jobVersionName = f'{jobVersionNamePre}_EleScale{sysKey[eleScale]}/'
+    elif TES==0 and eleScale==0 and JERSys==0 and METSys==0 and (not JESSys==0):
+        jobVersionName = f'{jobVersionNamePre}_JESPt22/'
+    elif TES==0 and eleScale==0 and JESSys==0 and (not JERSys==0):
+        jobVersionName = f'{jobVersionNamePre}_JER{sysKey[JERSys]}/'
+    else: #error
+        #assert error of input 
+        print('!!!error in input sys, please check')
+    
+    print ('jobVersionName=', jobVersionName) 
+    return jobVersionName
 
 def getInputOutDir( jobVersionName, era):
     eraDic = {
@@ -164,8 +184,8 @@ def makeSubAllJobs( jobsDir , era):
 
 
 
-# def makeJobsInDir( inputDir, outputDir, jobScriptsFolder, isData, dataSet, era, isRun3, ifSkipTTExtra=True):
-def makeJobsInDir( inputDir, outputDir, jobScriptsFolder, isData, era, isRun3,  sumProToSkip=[]):
+# def makeJobsInDir( inputDir, outputDir, jobScriptsFolder, isData, era, isRun3,  sumProToSkip=[]):
+def makeJobsInDir( inputDir, outputDir, jobScriptsFolder, isData, era, isRun3, sumProToSkip=[], TES=0, eleScale=0, JESSys=0, JERSys=0, METSys=0, if1tau2l=0):
     allProcesses = os.listdir( inputDir )
     if isData:
         outputDir = outputDir + 'data/'
@@ -219,7 +239,7 @@ def makeJobsInDir( inputDir, outputDir, jobScriptsFolder, isData, era, isRun3,  
 
                 iSmallJobName = 'OS_'+ era + sample_k + '_' + f"{_idx_tmp}" + ".sh"
                 smallFilejob = jobScriptsFolder +sample_k + "/" + iSmallJobName   
-                prepareCshJob( sampleDir, koutputDir, smallFilejob, entry)
+                prepareCshJob( sampleDir, koutputDir, smallFilejob, entry, TES, eleScale, JESSys, JERSys, METSys, if1tau2l)
                 _idx_tmp += 1
                 
         if len(sample_file_path)!=0:
@@ -230,8 +250,8 @@ def makeJobsInDir( inputDir, outputDir, jobScriptsFolder, isData, era, isRun3,  
             sub_oneProcess.write( "hep_sub -os CentOS7 " + iSmallJobName_forSub + " -o " + logFile + " -e " + errFile + f" -n {len(sample_file_path)}" + "\n")#!Memory comsuption is too high, need to be understood
             
             #!!!
-            # submit_cmd = "hep_sub -os CentOS7 " + iSmallJobName_forSub + " -o " + logFile + " -e " + errFile + f" -n {len(sample_file_path)}" + "\n"
-            submit_cmd = "hep_sub -os CentOS7 " + kProcessDir + iSmallJobName_forSub + " -o " + logFile + " -e " + errFile + f" -n {len(sample_file_path)}" + "\n"
+            # submit_cmd = "hep_sub -os CentOS7 " + kProcessDir + iSmallJobName_forSub + " -o " + logFile + " -e " + errFile + f" -n {len(sample_file_path)}" + "\n"
+            submit_cmd = "hep_sub " + kProcessDir + iSmallJobName_forSub + " -o " + logFile + " -e " + errFile + f" -n {len(sample_file_path)}" + "\n"
             result = subprocess.run(submit_cmd, shell=True, capture_output=True, text=True)
             cluster_id = None
             if result.returncode == 0:
@@ -258,8 +278,8 @@ def makeJobsInDir( inputDir, outputDir, jobScriptsFolder, isData, era, isRun3,  
 
 
 
-# def prepareCshJob( inputDir, koutputDir, shFile, singleFile, eventSelection):
-def prepareCshJob( inputDir, koutputDir, shFile, singleFile):
+# def prepareCshJob( inputDir, koutputDir, shFile, singleFile):
+def prepareCshJob( inputDir, koutputDir, shFile, singleFile, TES, eleScale, JESSys, JERSys, METSys, if1tau2l):
     subFile = open( shFile, 'w')
     subFile.write( "#!/bin/bash\n" )
     appDir = codePath.rsplit('/', 2)[0]
