aba2c33dca48cee20c0d7f6ec45d4a848324aa5b
huilling110 <huiling2016@outlook.com>
2025-12-14 10:04:36 +0800

    Combine: cleaned old scripts

--- hua/combine/checkWorkSpace.py
+++
@@ -1,4 +0,0 @@
-
-def main():
-    wsName = '/publicfs/cms/user/huahuil/tauOfTTTT_NanoAOD/forMVA/2017/v0baselineAddMoreSys_v58addGenBranches/mc/variableHists_v41tau0lGenTauSys/combine/datacard/workspace/datacard_1tau0lSys.root'
-    
